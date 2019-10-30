b5331740bc79238d56c128ee3f100130df716e89
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-10-30 15:40:21 -0300

    ui: update Sessions view
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- ui/src/views/Sessions.vue
+++ ui/src/views/Sessions.vue
@@ -6,25 +6,46 @@
         </v-app-bar>
         <v-divider></v-divider>
         <v-card-text class="pa-0">
-            <v-data-table :headers="headers" :items="$store.getters['sessions/list']" item-key="uid" hide-default-footer>
-                <template v-slot:items="props">
-                    <td>{{ props.item.device }}</td>
-                    <td>{{ props.item.username }}</td>
-                    <td>{{ props.item.active }}</td>
-                    <td>{{ props.item.ip_address }}</td>
-                    <td>{{ props.item.started_at | moment("ddd D MMM YYYY HH:mm:ss") }}</td>
-                    <td>{{ props.item.last_seen | moment("ddd D MMM YYYY HH:mm:ss") }}</td>
+            <v-data-table :headers="headers" :items="$store.getters['sessions/list']" item-key="uid" :sort-by="['started_at']" :sort-desc="[true]" disable-pagination hide-default-footer>
+                <template v-slot:item.device="{ item }">
+                    <v-chip>
+                        {{ item.device }}
+                        <v-icon small right @click v-clipboard="item.uid" v-clipboard:success="showCopySnack">mdi-content-copy</v-icon>
+                    </v-chip>
+                </template>
+
+                <template v-slot:item.ip_address="{ item }">
+                    <code>{{ item.ip_address }}</code>
+                </template>
+
+                <template v-slot:item.active="{ item }">
+                    <v-icon color="success" v-if="item.active">check_circle</v-icon>
+                    <v-tooltip bottom v-else>
+                        <template #activator="{ on }">
+                            <v-icon v-on="on">check_circle</v-icon>
+                        </template>
+                        <span>active {{ item.last_seen | moment("from", "now") }}</span>
+                    </v-tooltip>
                 </template>
             </v-data-table>
         </v-card-text>
     </v-card>
+    <v-snackbar v-model="copySnack" :timeout=3000>Device UID copied to clipboard</v-snackbar>
 </v-container>
 </template>
 
 <script>
 export default {
+    methods: {
+        showCopySnack() {
+            this.copySnack = true;
+        }
+    },
+
     data() {
         return {
+            copySnack: false,
+
             headers: [{
                     text: "Device",
                     value: "device"
@@ -34,21 +55,13 @@ export default {
                     value: "username"
                 },
                 {
-                    text: "Active",
-                    value: "active"
-                },
-                {
                     text: "IP Address",
                     value: "ip_address"
                 },
                 {
-                    text: "Started At",
-                    value: "started_at"
+                    text: "Active",
+                    value: "active"
                 },
-                {
-                    text: "Last Seen",
-                    value: "last_seen"
-                }
             ]
         };
     },
