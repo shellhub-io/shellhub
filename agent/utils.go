5c2567d0ddfd6f7b38ce3f1bed71d10e2564610b
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-11-01 15:19:43 -0300

    agent: add missing utils file
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

---
+++ agent/utils.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	shellwords "github.com/mattn/go-shellwords"
+)
+
+const etcOsRelease = "/etc/os-release"
+
+func getValueFromOsRelease(key string) (string, error) {
+	osReleaseFile, err := os.Open(etcOsRelease)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return "", fmt.Errorf("Error opening %s: %v", etcOsRelease, err)
+		}
+	}
+	defer osReleaseFile.Close()
+
+	var value string
+	keyWithTrailingEqual := key + "="
+	scanner := bufio.NewScanner(osReleaseFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, keyWithTrailingEqual) {
+			data := strings.SplitN(line, "=", 2)
+			values, err := shellwords.Parse(data[1])
+			if err != nil {
+				return "", fmt.Errorf("%s is invalid: %s", key, err.Error())
+			}
+			if len(values) != 1 {
+				return "", fmt.Errorf("%s needs to be enclosed by quotes if they have spaces: %s", key, data[1])
+			}
+			value = values[0]
+		}
+	}
+
+	return value, nil
+}
