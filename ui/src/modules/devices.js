fc0cff7331950c1eb9b8ba6fd191d705ae00defb
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-02-21 18:14:07 -0300

    Add the page feature devices

--- ui/src/modules/devices.js
+++ ui/src/modules/devices.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
-import { fetchDevices, removeDevice, renameDevice } from '@/api/devices'
+import { fetchDevices, removeDevice, renameDevice, getDevice} from '@/api/devices'
 
 export default {
     namespaced: true,
 
     state: {
-        devices: []
+        devices: [],
+        device: []
     },
 
     getters: {
-        list: state => state.devices
+        list: state => state.devices,
+        get: state => state.device
     },
 
     mutations: {
@@ -24,24 +26,31 @@ export default {
         renameDevice: (state, data) => {
             state.devices = state.devices.map(i => i.uid == data.uid ? { ...i, name: data.name } : i);
         },
+
+        setDevice: (state, data) => {
+            Vue.set(state, 'device', data)
+        }
     },
 
     actions: {
         fetch: async (context) => {
             let res = await fetchDevices()
-
             context.commit('setDevices', res.data)
         },
 
         remove: async (context, uid) => {
             await removeDevice(uid);
-
             context.commit('removeDevice', uid)
         },
 
         rename: async (context, data) => {
             await renameDevice(data);
             context.commit('renameDevice', data)
+        },
+
+        get: async (context,uid)  => {
+            let res = await getDevice(uid)
+            context.commit('setDevice', res.data)
         }
     }
 }
