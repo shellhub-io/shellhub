4da0042549986b424865c0a6234cb6f575d2c5f2
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-02-26 09:01:27 -0300

    Add the page feature devices

---
+++ ui/src/views/FeatureDevice.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    <DeviceFeature/>
+  </div>
+</template>
+
+<script>
+import DeviceFeature from "@/components/DeviceFeature.vue";
+
+export default {
+  components: {
+    DeviceFeature
+  }
+};
+</script>
