fdd9e929ac1394a47215a5610bffb48c147766f0
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-03-05 10:46:00 -0300

    api: initial migrations support

---
+++ api/migrations.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var migrations = []migrate.Migration{
+	// Version 1
+	{
+		Version: 1,
+		Up: func(db *mongo.Database) error {
+			return nil
+		},
+		Down: func(db *mongo.Database) error {
+			return nil
+		},
+	},
+}
+
+func applyMigrations(db *mongo.Database) error {
+	m := migrate.NewMigrate(db, migrations...)
+
+	if err := m.Up(migrate.AllAvailable); err != nil {
+		return err
+	}
+
+	return nil
+}
