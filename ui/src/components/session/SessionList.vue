cd0929e362f89cf8750033177b75c7520e8ecafd
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2020-03-10 16:09:25 -0300

    ui: show authenticated icon in session list page

--- ui/src/components/session/SessionList.vue
+++ ui/src/components/session/SessionList.vue
@@ -35,6 +35,16 @@
           </router-link>
         </template>
 
+        <template v-slot:item.username="{ item }">
+          <v-tooltip bottom v-if="!item.authenticated">
+            <template #activator="{ on }">
+              <v-icon v-on="on" color="error" size="medium">mdi-shield-alert</v-icon>
+            </template>
+            <span>Unauthorized</span>
+          </v-tooltip>
+          {{ item.username }}
+        </template>
+
         <template v-slot:item.ip_address="{ item }">
           <code>{{ item.ip_address }}</code>
         </template>
@@ -51,12 +61,11 @@
           <v-icon class="icons" @click="detailsSession(item)">
             info
           </v-icon>
-          
+
           <v-icon class="icons ml-1" v-if="item.active" @click="closeSession(item)">
             desktop_access_disabled
           </v-icon>
         </template>
-
       </v-data-table>
     </v-card-text>
   <v-snackbar v-model="sessionSnack" :timeout=3000>Closed session conection to the device</v-snackbar>
@@ -114,23 +123,22 @@ export default {
     };
   },
 
-  async mounted(){
+  async mounted() {
     await this.$store.dispatch("sessions/fetch");
-    this.listSessions = this.$store.getters['sessions/list']
+    this.listSessions = this.$store.getters["sessions/list"];
   },
 
   methods: {
     detailsSession(session) {
       this.$router.push("/session/" + session.uid);
     },
-    async closeSession(session){
+    async closeSession(session) {
       this.$store.dispatch("sessions/close", session);
       this.sessionSnack = true;
 
       await this.$store.dispatch("sessions/fetch");
-      this.listSessions = this.$store.getters['sessions/list']
+      this.listSessions = this.$store.getters["sessions/list"];
     }
-
   }
 };
 </script>
