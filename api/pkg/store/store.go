0b25272b20f099684d2a533b683c6f542e63879b
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-03-23 23:15:19 -0300

    api: add pagination to Devices and Sessions list

--- api/pkg/store/store.go
+++ api/pkg/store/store.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Store interface {
-	ListDevices(ctx context.Context) ([]models.Device, error)
+	ListDevices(ctx context.Context, perPage int, page int) ([]models.Device, error)
 	GetDevice(ctx context.Context, uid models.UID) (*models.Device, error)
 	DeleteDevice(ctx context.Context, uid models.UID) error
 	AddDevice(ctx context.Context, d models.Device) error
 	RenameDevice(ctx context.Context, uid models.UID, name string) error
 	LookupDevice(ctx context.Context, namespace, name string) (*models.Device, error)
 	UpdateDeviceStatus(ctx context.Context, uid models.UID, online bool) error
-	ListSessions(ctx context.Context) ([]models.Session, error)
+	ListSessions(ctx context.Context, perPage int, page int) ([]models.Session, error)
 	GetSession(ctx context.Context, uid models.UID) (*models.Session, error)
 	CreateSession(ctx context.Context, session models.Session) (*models.Session, error)
 	SetSessionAuthenticated(ctx context.Context, uid models.UID, authenticated bool) error
@@ -24,4 +24,6 @@ type Store interface {
 	GetUserByTenant(ctx context.Context, tenant string) (*models.User, error)
 	GetDeviceByMac(ctx context.Context, mac string, tenant string) (*models.Device, error)
 	GetDeviceByName(ctx context.Context, name string, tenant string) (*models.Device, error)
+	CountDevices(ctx context.Context) (int64, error)
+	CountSessions(ctx context.Context) (int64, error)
 }
