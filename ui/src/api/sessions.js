21fed165d1f609642609709b5bb5128f1828a7c1
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-05-08 18:01:49 -0300

    ui: add eslint on project
    eslint is used to organize and leave a single programming pattern.

--- ui/src/api/sessions.js
+++ ui/src/api/sessions.js
@@ -1,13 +1,15 @@
-import http from '@/helpers/http'
+import http from '@/helpers/http';
 
 export const
-    fetchSessions = async (per_page, page) => {
-        return http().get('/sessions?per_page='+per_page.toString()+'&page='+page.toString())
-    },
-    getSession = async (uid) => {
-        return http().get(`/sessions/${uid}`)
-    },
-    closeSession = async (session) => {
-        return http().post(`/sessions/${session.uid}/close`, { device: session.device_uid })
-    }
+  fetchSessions = async (perPage, page) => {
+    return http().get(`/sessions?per_page=${perPage}&page=${page}`); 
+  },
+
+  getSession = async (uid) => {
+    return http().get(`/sessions/${uid}`);
+  },
+  
+  closeSession = async (session) => {
+    return http().post(`/sessions/${session.uid}/close`, { device: session.device_uid });
+  };
 
--- ui/src/store.js
+++ ui/src/store.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-import devices from '@/modules/devices'
-import modals from '@/modules/modals'
-import stats from '@/modules/stats'
-import sessions from '@/modules/sessions'
-import auth from '@/modules/auth'
+import devices from '@/modules/devices';
+import modals from '@/modules/modals';
+import stats from '@/modules/stats';
+import sessions from '@/modules/sessions';
+import auth from '@/modules/auth';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: { devices, modals, stats, sessions, auth }
-})
+  modules: { devices, modals, stats, sessions, auth }
+});
