shellhub-io/shellhub

commit d44e0c884dfb39e7209252623bbd99fb0e94c447

Add policy to the only owner for rename and delete device

api/pkg/services/deviceadm/service.go
@@ -8,11 +8,13 @@ import (
 	"github.com/shellhub-io/shellhub/pkg/models"
 )
 
+var UnauthorizedErr = errors.New("unauthorized")
+
 type Service interface {
-	CountDevices(ctx context.Context) (int64, error)	
+	CountDevices(ctx context.Context) (int64, error)
 	ListDevices(ctx context.Context, perPage int, page int) ([]models.Device, error)
 	GetDevice(ctx context.Context, uid models.UID) (*models.Device, error)
-	DeleteDevice(ctx context.Context, uid models.UID) error
+	DeleteDevice(ctx context.Context, uid models.UID, tenant string) error
 	RenameDevice(ctx context.Context, uid models.UID, name string, tenant string) error
 	LookupDevice(ctx context.Context, namespace, name string) (*models.Device, error)
 	UpdateDeviceStatus(ctx context.Context, uid models.UID, online bool) error
@@ -37,17 +39,22 @@ func (s *service) GetDevice(ctx context.Context, uid models.UID) (*models.Device
 	return s.store.GetDevice(ctx, uid)
 }
 
-func (s *service) DeleteDevice(ctx context.Context, uid models.UID) error {
-	return s.store.DeleteDevice(ctx, uid)
+func (s *service) DeleteDevice(ctx context.Context, uid models.UID, tenant string) error {
+	device, _ := s.store.GetDeviceByUid(ctx, uid, tenant)
+	if device != nil {
+		return s.store.DeleteDevice(ctx, uid)
+	}
+	return UnauthorizedErr
 }
 
 func (s *service) RenameDevice(ctx context.Context, uid models.UID, name string, tenant string) error {
-	device, _ := s.store.GetDeviceByName(ctx, name, tenant)
-	if device == nil {
-		return s.store.RenameDevice(ctx, uid, name)
-	} else {
-		return errors.New("rename unauthorized")
+	device, _ := s.store.GetDeviceByUid(ctx, uid, tenant)
+	if device != nil {
+		if device.Name != name {
+			return s.store.RenameDevice(ctx, uid, name)
+		}
 	}
+	return UnauthorizedErr
 }
 
 func (s *service) LookupDevice(ctx context.Context, namespace, name string) (*models.Device, error) {

api/main.go
@@ -177,11 +177,19 @@ func main() {
 	})
 
 	publicAPI.DELETE("/devices/:uid", func(c echo.Context) error {
+		tenant := c.Request().Header.Get("X-Tenant-ID")
 		ctx := c.Get("ctx").(context.Context)
 		store := mongo.NewStore(ctx.Value("db").(*mgo.Database))
 		svc := deviceadm.NewService(store)
 
-		return svc.DeleteDevice(ctx, models.UID(c.Param("uid")))
+		if err := svc.DeleteDevice(ctx, models.UID(c.Param("uid")), tenant); err != nil {
+			if err == deviceadm.UnauthorizedErr {
+				return c.NoContent(http.StatusForbidden)
+			} else {
+				return err
+			}
+		}
+		return nil
 	})
 
 	publicAPI.PATCH("/devices/:uid", func(c echo.Context) error {
@@ -199,7 +207,14 @@ func main() {
 		store := mongo.NewStore(ctx.Value("db").(*mgo.Database))
 		svc := deviceadm.NewService(store)
 
-		return svc.RenameDevice(ctx, models.UID(c.Param("uid")), req.Name, tenant)
+		if err := svc.RenameDevice(ctx, models.UID(c.Param("uid")), req.Name, tenant); err != nil {
+			if err == deviceadm.UnauthorizedErr {
+				return c.NoContent(http.StatusForbidden)
+			} else {
+				return err
+			}
+		}
+		return nil
 	})
 
 	publicAPI.POST("/login", func(c echo.Context) error {

api/pkg/store/mongo/store.go
@@ -548,6 +548,15 @@ func (s *Store) GetDeviceByName(ctx context.Context, name string, tenant string)
 	return device, nil
 }
 
+func (s *Store) GetDeviceByUid(ctx context.Context, uid models.UID, tenant string) (*models.Device, error) {
+	device := new(models.Device)
+	if err := s.db.Collection("devices").FindOne(ctx, bson.M{"tenant_id": tenant, "uid": uid}).Decode(&device); err != nil {
+		return nil, err
+	}
+
+	return device, nil
+}
+
 func EnsureIndexes(db *mongo.Database) error {
 	mod := mongo.IndexModel{
 		Keys:    bson.D{{"uid", 1}},

api/pkg/store/store.go
@@ -26,4 +26,5 @@ type Store interface {
 	GetDeviceByName(ctx context.Context, name string, tenant string) (*models.Device, error)
 	CountDevices(ctx context.Context) (int64, error)
 	CountSessions(ctx context.Context) (int64, error)
+	GetDeviceByUid(ctx context.Context, uid models.UID, tenant string) (*models.Device, error)
 }

commit b30a55fee547abd6bbb95f04c15b6b066e15ac18

api: avoid devices with same name
Restoring getDeviceByName  function to avoid renaming a device
to an already taken name.

Fixes: d44e0c884dfb39e7209252623bbd99fb0e94c447

api/pkg/services/deviceadm/service.go
@@ -68,7 +68,10 @@ func (s *service) RenameDevice(ctx context.Context, uid models.UID, name string,
 		if device.Name != name {
 			device.Name = name
 			if err := validate.Struct(device); err == nil {
-				return s.store.RenameDevice(ctx, uid, name)
+				otherDevice, _ := s.store.GetDeviceByName(ctx, name, tenant)
+				if otherDevice == nil {
+					return s.store.RenameDevice(ctx, uid, name)
+				}
 			}
 		}
 	}