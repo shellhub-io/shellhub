02c3346aad58a9fd1ac1d3890970517118e5e4c1
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2020-05-19 15:09:35 -0300

    agent: add support for auto-updating agent container image

---
+++ agent/updater_docker.go
@@ -0,0 +1,236 @@
+// +build docker
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/semver"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+)
+
+type dockerContainer struct {
+	info *types.ContainerJSON
+}
+
+func (c *dockerContainer) splitImageVersion() (string, string) {
+	parts := strings.SplitN(c.info.Config.Image, ":", 2)
+	image, version := parts[0], ""
+
+	if len(parts) == 2 {
+		version = parts[1]
+	}
+
+	return image, version
+}
+
+type dockerUpdater struct {
+	api client.CommonAPIClient
+}
+
+func (d *dockerUpdater) CurrentVersion() (*semver.Version, error) {
+	container, err := d.currentContainer()
+	if err != nil {
+		return nil, err
+	}
+
+	_, version := container.splitImageVersion()
+
+	return semver.NewVersion(version)
+}
+
+func (d *dockerUpdater) ApplyUpdate(v *semver.Version) error {
+	container, err := d.currentContainer()
+	if err != nil {
+		return err
+	}
+
+	image, _ := container.splitImageVersion()
+	_, err = d.updateContainer(container, fmt.Sprintf("%s:%s", image, v.Original()), "", true)
+	return err
+
+}
+
+func (d *dockerUpdater) CompleteUpdate() error {
+	container, err := d.currentContainer()
+	if err != nil {
+		return err
+	}
+
+	parent, err := d.parentContainer()
+	if err != nil {
+		return err
+	}
+
+	if parent != nil {
+		if err := d.stopContainer(parent); err != nil {
+			return err
+		}
+
+		_, err = d.updateContainer(parent, container.info.Config.Image, parent.info.Name, false)
+		if err != nil {
+			return err
+		}
+
+		os.Exit(0)
+	}
+
+	return nil
+}
+
+func (d *dockerUpdater) getContainer(id string) (*dockerContainer, error) {
+	ctx := context.Background()
+
+	info, err := d.api.ContainerInspect(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dockerContainer{info: &info}, nil
+}
+
+func (d *dockerUpdater) currentContainer() (*dockerContainer, error) {
+	id, err := currentContainerID()
+	if err != nil {
+		return nil, err
+	}
+
+	return d.getContainer(id)
+}
+
+func (d *dockerUpdater) parentContainer() (*dockerContainer, error) {
+	id := os.Getenv("PARENT_CONTAINER")
+	if id == "" {
+		return nil, nil
+	}
+
+	return d.getContainer(id)
+}
+
+func (d *dockerUpdater) stopContainer(container *dockerContainer) error {
+	ctx := context.Background()
+
+	timeout := time.Minute
+	if err := d.api.ContainerStop(ctx, container.info.ID, &timeout); err != nil {
+		return err
+	}
+
+	opts := types.ContainerRemoveOptions{Force: true, RemoveVolumes: true}
+	if err := d.api.ContainerRemove(ctx, container.info.ID, opts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *dockerUpdater) updateContainer(container *dockerContainer, image, name string, parent bool) (*dockerContainer, error) {
+	ctx := context.Background()
+
+	// Clone container container config and update the image name
+	config := container.info.Config
+	config.Image = image
+
+	// Set current container as parent of the new container
+	if parent {
+		config.Env = replaceOrAppendEnvValues(config.Env, []string{fmt.Sprintf("PARENT_CONTAINER=%s", container.info.ID)})
+	}
+
+	netConfig := &network.NetworkingConfig{EndpointsConfig: container.info.NetworkSettings.Networks}
+
+	rd, err := d.api.ImagePull(ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		return nil, err
+	}
+	defer rd.Close()
+
+	// Wait for image to pull
+	_, err = io.Copy(ioutil.Discard, rd)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new container using the cloned container config
+	clone, err := d.api.ContainerCreate(ctx, config, container.info.HostConfig, netConfig, name)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := d.api.ContainerStart(ctx, clone.ID, types.ContainerStartOptions{}); err != nil {
+		return nil, err
+	}
+
+	return d.getContainer(clone.ID)
+
+}
+
+func NewUpdater() (Updater, error) {
+	api, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dockerUpdater{api: api}, nil
+}
+
+func currentContainerID() (string, error) {
+	const idLength = 64
+
+	f, err := os.Open("/proc/self/cgroup")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	if !scanner.Scan() {
+		return "", scanner.Err()
+	}
+
+	line := scanner.Text()
+	return line[len(line)-64:], nil
+}
+
+func replaceOrAppendEnvValues(defaults, overrides []string) []string {
+	cache := make(map[string]int, len(defaults))
+	for i, e := range defaults {
+		index := strings.Index(e, "=")
+		cache[e[:index]] = i
+	}
+
+	for _, value := range overrides {
+		// Values w/o = means they want this env to be removed/unset.
+		index := strings.Index(value, "=")
+		if index < 0 {
+			// no "=" in value
+			if i, exists := cache[value]; exists {
+				defaults[i] = "" // Used to indicate it should be removed
+			}
+			continue
+		}
+
+		if i, exists := cache[value[:index]]; exists {
+			defaults[i] = value
+		} else {
+			defaults = append(defaults, value)
+		}
+	}
+
+	// Now remove all entries that we want to "unset"
+	for i := 0; i < len(defaults); i++ {
+		if defaults[i] == "" {
+			defaults = append(defaults[:i], defaults[i+1:]...)
+			i--
+		}
+	}
+
+	return defaults
+}
