06ad6c27bc9cc316d15fdd06d3aa138f385f3fe9
vagnersaller <vsaller@yahoo.com>
2020-05-19 13:42:51 -0300

    ui: Create a deviceIcon component

---
+++ ui/src/components/device/DeviceIcon.vue
@@ -0,0 +1,42 @@
+<template>
+  <fragment />
+</template>
+
+<script>
+export default {
+  name: 'DeviceIcon',
+  data() {
+    return {
+      deviceIcon: {
+        alpine: 'fl-alpine',
+        arch: 'fl-archlinux',
+        centos: 'fl-centos',
+        coreos: 'fl-coreos',
+        debian: 'fl-debian',
+        devuan: 'fl-devuan',
+        elementary: 'fl-elementary',
+        fedora: 'fl-fedora',
+        freebsd: 'fl-freebsd',
+        gentoo: 'fl-gentoo',
+        linuxmint: 'fl-linuxmint',
+        mageia: 'fl-mageia',
+        manjaro: 'fl-manjaro',
+        mandriva: 'fl-mandriva',
+        nixos: 'fl-nixos',
+        opensuse: 'fl-opensuse',
+        rhel: 'fl-redhat',
+        sabayon: 'fl-sabayon',
+        slackware: 'fl-slackware',
+        ubuntu: 'fl-ubuntu',
+        raspbian: 'fl-raspberry-pi',
+        'ubuntu-core': 'fl-ubuntu',
+        void: 'fl-void',
+      },
+    };
+  },
+  mounted(){
+    this.$emit('expIcons', this.deviceIcon);
+  }
+
+};
+</script>
