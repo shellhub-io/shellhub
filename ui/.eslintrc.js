72e5fd64e9ec17b3b028bb685335cbf515875cdd
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-05-22 17:37:32 -0300

    ui: implementing unit test on Login

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     // this section will be used to determine which APIs are available to us
     // (i.e are we running in a browser environment or a node.js env)
     node: true,
-    browser: true
+    browser: true,
+    jest: true
   },
   parserOptions: {
     parser: 'babel-eslint',
---
+++ ui/tests/unit/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'; 
+import Vuetify from 'vuetify'; 
+
+Vue.config.productionTip = false; 
+Vue.use(Vuetify);
