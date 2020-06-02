c3f60716f7a788c8316d2c1d13f885feb9001254
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-06-02 16:00:22 -0300

    ui: creating basic tests for componente DeviceDetails

---
+++ ui/tests/unit/components/device/DeviceDetails.spec.js
@@ -0,0 +1,64 @@
+import Vuex from 'vuex';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import DeviceDetails from '@/components/device/DeviceDetails.vue';
+
+describe('DeviceDetails', () => {
+  let wrapper;
+
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  const store = new Vuex.Store({
+    namespaced: true,
+    state: {
+      device: {
+        'uid': 'a582b47a42d',
+        'name': '39-5e-2a',
+        'identity': {
+          'mac': '00:00:00:00:00:00'
+        },
+        'info': {
+          'id': 'arch',
+          'pretty_name': 'Linux Mint 19.3',
+          'version': ''
+        },
+        'public_key': '----- PUBLIC KEY -----',
+        'tenant_id': '00000000',
+        'last_seen': '2020-05-20T18:58:53.276Z',
+        'online': false,
+        'namespace': 'user'
+      }
+    },
+    getters: {
+      'devices/get': (state) => state.device,
+    },
+    actions: {
+      'devices/get': () => {
+      }
+    }
+  });
+
+  beforeEach(() => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+
+    wrapper = shallowMount(DeviceDetails, {
+      store,
+      localVue,
+      stubs: ['fragment'],
+      mocks: {
+        $route: {
+          params: {
+            id: 'a582b47a42d'
+          }
+        }
+      }
+    });
+  });
+  it('Is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+  it('Renders the component', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
