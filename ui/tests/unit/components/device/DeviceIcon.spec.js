9685e3da311d7f3297ef9a50a8e491418e78fa7e
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-06-03 10:41:23 -0300

    ui: testing all icons in DeviceIcon

--- ui/tests/unit/components/device/DeviceIcon.spec.js
+++ ui/tests/unit/components/device/DeviceIcon.spec.js
@@ -3,15 +3,40 @@ import { shallowMount, createLocalVue } from '@vue/test-utils';
 import DeviceIcon from '@/components/device/DeviceIcon.vue';
 
 describe('DeviceIcon', () => {
-  let wrapper;
-  let iconName = 'fl-alpine';
-
   const localVue = createLocalVue();
   localVue.use(Vuex);
 
+  let wrapper;
+  let iconName = 'alpine';
+
+  const iconsMap = {
+    alpine: 'fl-alpine',
+    arch: 'fl-archlinux',
+    centos: 'fl-centos',
+    coreos: 'fl-coreos',
+    debian: 'fl-debian',
+    devuan: 'fl-devuan',
+    elementary: 'fl-elementary',
+    fedora: 'fl-fedora',
+    freebsd: 'fl-freebsd',
+    gentoo: 'fl-gentoo',
+    linuxmint: 'fl-linuxmint',
+    mageia: 'fl-mageia',
+    manjaro: 'fl-manjaro',
+    mandriva: 'fl-mandriva',
+    nixos: 'fl-nixos',
+    opensuse: 'fl-opensuse',
+    rhel: 'fl-redhat',
+    sabayon: 'fl-sabayon',
+    slackware: 'fl-slackware',
+    ubuntu: 'fl-ubuntu',
+    raspbian: 'fl-raspberry-pi',
+    'ubuntu-core': 'fl-ubuntu',
+    void: 'fl-void',
+    default: 'fl-tux'
+  };
+
   beforeEach(() => {
-    const localVue = createLocalVue();
-    localVue.use(Vuex);
 
     wrapper = shallowMount(DeviceIcon, {
       localVue,
@@ -26,4 +51,14 @@ describe('DeviceIcon', () => {
   it('Renders the component', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
+  for (iconName in iconsMap) {
+    it(`Has the ${iconName} icon`, () => {
+      wrapper = shallowMount(DeviceIcon, {
+        localVue,
+        stubs: ['fragment'],
+        propsData: { iconName }
+      });
+      expect(wrapper.find('[data-cy="type-icon"]').text()).toBe(iconsMap[iconName]);
+    });
+  }
 });
