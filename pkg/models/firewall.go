6be74a4bca434dee6b2adccda0ff1a4f3379eb8a
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2020-06-05 18:52:53 -0300

    api: firewall rules
    This commits implements a flexible firewall for filtering SSH connections.
    It gives a fine-grained control over which SSH connections reach the devices.
    
    The firewall rules can deny/allow SSH connections from specific IP address
    to a specific or a group of devices using a given username.
    
    Each firewall rule has a priority number which is evaluated in ascending order
    (starting with the lowest). It's makes a lot easier to manage a large number
    of firewall rules.

---
+++ pkg/models/firewall.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"regexp"
+
+	"gopkg.in/go-playground/validator.v9"
+)
+
+type FirewallRuleFields struct {
+	Priority int    `json:"priority"`
+	Action   string `json:"action" validate:"required,oneof=allow deny"`
+	Active   bool   `json:"active"`
+	SourceIP string `json:"source_ip" bson:"source_ip" validate:"required,regexp"`
+	Username string `json:"username" validate:"required,regexp"`
+	Hostname string `json:"hostname" validate:"required,regexp"`
+}
+
+func (f *FirewallRuleFields) Validate() error {
+	v := validator.New()
+
+	_ = v.RegisterValidation("regexp", func(fl validator.FieldLevel) bool {
+		_, err := regexp.Compile(fl.Field().String())
+		return err == nil
+	})
+
+	return v.Struct(f)
+}
+
+type FirewallRule struct {
+	ID                 string `json:"id" bson:"_id"`
+	TenantID           string `json:"tenant_id" bson:"tenant_id"`
+	FirewallRuleFields `bson:",inline"`
+}
+
+type FirewallRuleUpdate struct {
+	FirewallRuleFields `bson:",inline"`
+}
--- ssh/go.mod
+++ ssh/go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/elazarl/goproxy v0.0.0-20200426045556-49ad98f6dac1 // indirect
 	github.com/gliderlabs/ssh v0.3.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.6.6 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/parnurzeal/gorequest v0.2.16
 	github.com/pires/go-proxyproto v0.1.3
 	github.com/pkg/errors v0.9.1 // indirect
@@ -19,6 +21,7 @@ require (
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
 	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	moul.io/http2curl v1.0.0 // indirect
 )
 
