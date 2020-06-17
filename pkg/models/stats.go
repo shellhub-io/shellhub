5fcfafe04caeabf53e6218d2492f0d6577a5b299
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-06-17 17:17:22 -0300

    api: add pending flag to Device
    when adding a new device it will be waiting the user aproval before
    allow connections
    
    created routes to allow a new device and list only pending devices

--- pkg/models/stats.go
+++ pkg/models/stats.go
@@ -4,4 +4,5 @@ type Stats struct {
 	RegisteredDevices int `json:"registered_devices"`
 	OnlineDevices     int `json:"online_devices"`
 	ActiveSessions    int `json:"active_sessions"`
+	PendingDevices    int `json:"pending_devices"`
 }
