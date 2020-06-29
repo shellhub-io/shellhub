1e9ab7e10653b3210c1b0d21d5ea304879a75d46
vagnersaller <vsaller@yahoo.com>
2020-06-29 15:04:27 -0300

    ui: Define vee validate rule for invalid hostname
    The rfc1123 rule was extended to vee-validate, providing real-time
    validation in textfield form. Improvements and adjusts were done as well.

--- ui/src/components/device/DeviceRename.vue
+++ ui/src/components/device/DeviceRename.vue
@@ -20,43 +20,42 @@
         <v-card-title class="headline grey lighten-2 text-center">
           Rename Device
         </v-card-title>
-        <v-card-text class="caption mb-0">
-          <v-text-field
-            v-model="editName"
-            require
-            messages="Examples: (foobar, foo-bar.ba-z.qux, foo.example.com, 127.0.0.1)"
-          />
-        </v-card-text>
-        <v-card-text
-          v-if="invalid"
-          class="red--text"
+        <ValidationObserver
+          ref="obs"
+          v-slot="{ validated, passes }"
         >
-          You entered an invalid hostname,
-          it must follow the
-          <a
-            target="_blank"
-            href="https://tools.ietf.org/html/rfc1123"
-          >
-            RFC1123
-          </a>
-          specifications
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            text
-            @click="cancel"
-          >
-            Close
-          </v-btn>
-          <v-btn
-            color="primary"
-            text
-            @click="check"
-          >
-            Rename
-          </v-btn>
-        </v-card-actions>
+          <v-card-text class="caption mb-0">
+            <ValidationProvider
+              v-slot="{ errors }"
+              name="Hostname"
+              rules="required|rfc1123"
+            >
+              <v-text-field
+                v-model="editName"
+                label="Hostname"
+                :error-messages="errors"
+                require
+                messages="Examples: (foobar, foo-bar.ba-z.qux, foo.example.com, 127.0.0.1)"
+              />
+            </ValidationProvider>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              text
+              @click="cancel"
+            >
+              Close
+            </v-btn>
+            <v-btn
+              color="primary"
+              text
+              @click="passes(edit)"
+            >
+              Rename
+            </v-btn>
+          </v-card-actions>
+        </ValidationObserver>
       </v-card>
     </v-dialog>
   </fragment>
@@ -64,10 +63,18 @@
 
 <script>
 
-import isValidHostname from 'is-valid-hostname';
+import {
+  ValidationObserver,
+  ValidationProvider,
+} from 'vee-validate';
 
 export default {
   name: 'DeviceRename',
+
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
 
   props: {
     name: {
@@ -84,42 +91,44 @@ export default {
     return {
       dialog: false,
       invalid: false,
-      // editName: '',
+      editName: '',
     };
   },
 
+  computed: {
+    device: {
+      get() {
+        return {
+          name: this.name,
+          uid: this.uid,
+        };
+      },
+    },
+  },
+
   created() {
-    this.editName = this.name;
+    this.editName = this.device.name;
   },
 
   updated() {
-    this.editName = this.name;
+    this.editName = this.device.name;
   },
 
   methods: {
-    save() {
-      this.$store.dispatch('devices/rename', {
-        uid: this.uid,
-        name: this.editName,
-      });
-      this.dialog = false;
-    },
-
     cancel() {
       this.dialog = false;
       this.invalid = false;
       this.editName = '';
     },
 
-    check() {
-      if (isValidHostname(this.editName)) {
-        this.save();
-        this.dialog = false;
-        this.$emit('newHostname', this.editName);
-        this.editName = '';
-      } else {
-        this.invalid = true;
-      }
+    edit() {
+      this.$store.dispatch('devices/rename', {
+        uid: this.device.uid,
+        name: this.editName,
+      });
+      this.dialog = false;
+      this.$emit('newHostname', this.editName);
+      this.editName = '';
     },
   },
 };
--- ui/src/vee-validate.js
+++ ui/src/vee-validate.js
@@ -1,5 +1,6 @@
 import { required, integer } from 'vee-validate/dist/rules';
 import { extend } from 'vee-validate';
+import isValidHostname from 'is-valid-hostname';
 
 extend('required', {
   ...required,
@@ -9,4 +10,9 @@ extend('required', {
 extend('integer', {
   ...integer,
   message: 'This value must be a integer number',
+});
+
+extend('rfc1123', {
+  validate: (value) => isValidHostname(value),
+  message: 'You entered an invalid RFC1123 hostname',
 });
