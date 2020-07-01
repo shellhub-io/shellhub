151bf4f44114712e6372b54f52d9a9e09f1e0b16
vagnersaller <vsaller@yahoo.com>
2020-07-01 17:08:17 -0300

    ui: Define slider to SessionPlay
    A vuetify slider was added to the sessionplay
    component, making use of the aquired data
    directly from it.

--- ui/src/components/session/SessionPlay.vue
+++ ui/src/components/session/SessionPlay.vue
@@ -27,6 +27,7 @@
         <v-card>
           <v-card-actions>
             <v-btn
+              v-show="!disable"
               :disabled="disable"
               color="primary"
               class="mt-4"
@@ -34,6 +35,13 @@
             >
               Play
             </v-btn>
+            <v-slider
+              v-model="currentTime"
+              readonly
+              min="0"
+              :max="totalLength"
+              :label="`${now} - ${end}`"
+            />
           </v-card-actions>
         </v-card>
       </v-card>
@@ -45,15 +53,15 @@
 
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
-
+import moment from 'moment';
 import 'xterm/css/xterm.css';
 
 export default {
   name: 'SessionPlay',
 
   props: {
-    logs: {
-      type: Array,
+    uid: {
+      type: String,
       required: true,
     },
   },
@@ -62,7 +70,26 @@ export default {
     return {
       dialog: false,
       disable: false,
+      currentTime: 0,
+      totalLength: 0,
+      timerDisplayEnd: null,
+      timerDisplayNow: null,
     };
+  },
+
+  computed: {
+    length() {
+      return this.logs.length;
+    },
+
+    now() {
+      return this.timerDisplayNow;
+    },
+
+    end() {
+      return this.timerDisplayEnd;
+    },
+
   },
 
   watch: {
@@ -73,10 +100,22 @@ export default {
     },
   },
 
+  updated() {
+    this.timerDisplayNow = this.duration(this.currentTime).display;
+  },
+
+  async created() {
+    await this.$store.dispatch('sessions/getLogSession', this.uid);
+    this.logs = this.$store.getters['sessions/getLogSession'];
+    this.totalLength = this.duration(null).intervalLength;
+    this.timerDisplayEnd = this.duration(null).display;
+    this.timerDisplayNow = this.duration(this.currentTime).display;
+    // eslint-disable-next-line no-console
+    console.log(this.logs);
+  },
+
   methods: {
     openPlay() {
-      // eslint-disable-next-line no-console
-      console.log(this.logs);
       this.dialog = !this.dialog;
       this.xterm = new Terminal({ // instantiate
         cursorBlink: true,
@@ -86,13 +125,41 @@ export default {
       this.xterm.loadAddon(this.fitAddon); // adjust screen in container
     },
 
+    duration(timeMs) {
+      let interval = 0;
+      if (!timeMs) { // not params, will return metrics to max timelengtht
+        const max = new Date(this.logs[this.length - 1].time);
+        const min = new Date(this.logs[0].time);
+        interval = max - min;
+      } else { // it will format to the time argument passed
+        interval = timeMs;
+      }
+      const duration = moment.duration(interval, 'milliseconds');
+      const TimeObject = {
+        seconds: Math.floor(duration.asSeconds()),
+        minutes: Math.floor(duration.asMinutes()),
+        hours: Math.floor(duration.asHours()),
+      };
+      return {
+        display: `${TimeObject.hours}:${TimeObject.minutes}:${TimeObject.seconds}`, // format to slider label
+        intervalLength: interval, // length of slider in Ms
+      };
+    },
+
+    timer() { // Increments the slider
+      if (this.currentTime >= this.totalLength) return;
+      this.currentTime += 100;
+      this.iterativeTimer = setTimeout(this.timer.bind(null), 100);
+    },
+
     connect() {
       this.disable = true;
       this.xterm.open(this.$refs.playterminal);
       this.$nextTick(() => this.fitAddon.fit());
       this.fitAddon.fit();
       this.xterm.focus();
-      this.print(0, this.logs[0]);
+      this.print(0, this.logs);
+      this.timer();
       if (this.xterm.element) { // check already existence
         this.xterm.reset();
       }
@@ -101,6 +168,9 @@ export default {
     close() {
       if (this.xterm) this.xterm.dispose();
       this.disable = false;
+      clearInterval(this.iterativePrinting);
+      clearInterval(this.iterativeTimer);
+      this.currentTime = 0;
     },
 
     print(i, obj) {
