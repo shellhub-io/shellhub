d33449b45b0a19bc5645c1412c13d9d8b94790f2
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-07-07 10:22:15 -0300

    ui: refactor the welcome screen to support accept pending device
    Another tab has been added to the welcome screen, third screen. On this screen,
    the user can see the pending device that he recently added, be able to accept
    the device. After accepting, you are redirected to the fourth screen that
    previously it was the third.

---
+++ ui/src/components/welcome/WelcomeFourthScreen.vue
@@ -0,0 +1,40 @@
+<template>
+  <fragment>
+    <div class="pa-4">
+      <p class="mb-4">
+        Your device has been succesfully added.
+      </p>
+      <p class="mb-4">
+        Now you can take advantage of all the tools made available by ShellHub, viewing the
+        registered devices, the sessions in each of them and getting access through the terminal
+        interface.
+      </p>
+      <p class="mb-2">
+        For more details, consult our <a
+          target="_blank"
+          href="http://docs.shellhub.io/"
+        >
+          documentation
+        </a>
+        and, if you have any questions, contact us through the
+        <a
+          target="_blank"
+          href="https://gitter.im/shellhub-io/community"
+        >
+          Gitter
+        </a>.
+      </p>
+      <p>
+        Thank you for choosing Shellhub.
+      </p>
+    </div>
+  </fragment>
+</template>
+
+<script>
+
+export default {
+  name: 'WelcomeFourthScreen',
+};
+
+</script>
