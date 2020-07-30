875b8b7b9c1bc373af3e5690fb77e3661d7bd747
vagnersaller <vsaller@yahoo.com>
2020-07-30 14:53:27 -0300

    ui: add unit tests for settings

---
+++ ui/tests/unit/views/Settings.spec.js
@@ -0,0 +1,26 @@
+import Vuex from 'vuex';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import VueRouter from 'vue-router';
+import Settings from '@/views/Settings';
+
+describe('Settings', () => {
+  const localVue = createLocalVue();
+  localVue.use(VueRouter);
+  localVue.use(Vuex);
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Settings, {
+      localVue,
+      stubs: ['fragment'],
+    });
+  });
+
+  it('Is a Vue instance', () => {
+    expect(wrapper).toBeTruthy();
+  });
+  it('Renders the component', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
