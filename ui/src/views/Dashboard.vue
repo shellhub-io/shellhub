3f81012947b01108ddb2ab02bbbdc11af0ac2833
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-08-12 15:23:52 -0300

    ui: Refactor dashboard to use for to list cards
    Use the v-for on list cards, it is possible to reuse the same code.
    And to add new items, you must have items in the list data.

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -13,6 +13,8 @@
       class="mt-4"
     >
       <v-col
+        v-for="(item, index) in items"
+        :key="index"
         cols="12"
         md="4"
         class="pt-0"
@@ -21,13 +23,13 @@
           <v-list-item three-line>
             <v-list-item-content>
               <div class="overline mb-4">
-                Registered Devices
+                {{ item.title }}
               </div>
               <v-list-item-title class="headline mb-1">
-                {{ stats.registered_devices }}
+                {{ stats[item.fieldObject] }}
               </v-list-item-title>
               <v-list-item-subtitle>
-                Registered devices into the tenancy account
+                {{ item.content }}
               </v-list-item-subtitle>
             </v-list-item-content>
 
@@ -36,112 +38,46 @@
               size="80"
             >
               <v-icon x-large>
-                devices
-              </v-icon>
-            </v-list-item-avatar>
-          </v-list-item>
-
-          <v-card-actions v-if="show">
-            <Welcome
-              :dialog="true"
-              :curl="curl"
-              @finishedEvent="receiveFinish"
-            />
-          </v-card-actions>
-
-          <v-card-actions>
-            <DeviceAdd />
-            <v-btn
-              text
-              data-cy="addDevice-btn"
-              @click="$store.dispatch('modals/showAddDevice', true)"
-            >
-              Add Device
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-
-      <v-col
-        cols="12"
-        md="4"
-        class="pt-0"
-      >
-        <v-card class="pa-2">
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="overline mb-4">
-                Online Devices
-              </div>
-              <v-list-item-title class="headline mb-1">
-                {{ stats.online_devices }}
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                Devices are online and ready for connecting
-              </v-list-item-subtitle>
-            </v-list-item-content>
-
-            <v-list-item-avatar
-              tile
-              size="80"
-            >
-              <v-icon x-large>
-                devices
+                {{ item.icon }}
               </v-icon>
             </v-list-item-avatar>
           </v-list-item>
 
           <v-card-actions>
-            <v-btn
-              to="/devices"
-              text
-              data-cy="viewDevices-btn"
-            >
-              View all Devices
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
+            <div v-if="item.pathName == 'addDevice'">
+              <DeviceAdd />
+              <v-btn
+                class="v-btn--active"
+                text
+                :data-cy="item.nameUseTest"
+                @click="$store.dispatch('modals/showAddDevice', true)"
+              >
+                {{ item.buttonName }}
+              </v-btn>
+            </div>
 
-      <v-col
-        cols="12"
-        md="4"
-        class="pt-0"
-      >
-        <v-card class="pa-2">
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="overline mb-4">
-                Active Sessions
-              </div>
-              <v-list-item-title class="headline mb-1">
-                {{ stats.active_sessions }}
-              </v-list-item-title>
-              <v-list-item-subtitle>Active SSH Sessions opened by users</v-list-item-subtitle>
-            </v-list-item-content>
-
-            <v-list-item-avatar
-              tile
-              size="80"
-            >
-              <v-icon x-large>
-                history
-              </v-icon>
-            </v-list-item-avatar>
-          </v-list-item>
-
-          <v-card-actions>
-            <v-btn
-              to="/sessions"
-              text
-              data-cy="viewSessions-btn"
-            >
-              View all Sessions
-            </v-btn>
+            <div v-else>
+              <v-btn
+                class="v-btn--active"
+                :to="{ name: item.pathName }"
+                text
+                :data-cy="item.nameUseTest"
+              >
+                {{ item.buttonName }}
+              </v-btn>
+            </div>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
+
+    <Welcome
+      v-if="show"
+      :dialog="true"
+      :curl="curl"
+      @finishedEvent="receiveFinish"
+    />
+
     <SnackbarError
       :error="error"
     />
@@ -171,6 +107,35 @@ export default {
       hasDevicesRegistered: false,
       show: false,
       error: false,
+      items: [
+        {
+          title: 'Registered Devices',
+          fieldObject: 'registered_devices',
+          content: 'Registered devices into the tenancy account',
+          icon: 'devices',
+          buttonName: 'Add Device',
+          pathName: 'addDevice',
+          nameUseTest: 'addDevice-btn',
+        },
+        {
+          title: 'Online Devices',
+          fieldObject: 'online_devices',
+          content: 'Devices are online and ready for connecting',
+          icon: 'devices',
+          buttonName: 'View all Devices',
+          pathName: 'devices',
+          nameUseTest: 'viewDevices-btn',
+        },
+        {
+          title: 'Active Sessions',
+          fieldObject: 'active_sessions',
+          content: 'Active SSH Sessions opened by users',
+          icon: 'devices',
+          buttonName: 'View all Sessions',
+          pathName: 'sessions',
+          nameUseTest: 'viewSessions-btn',
+        },
+      ],
     };
   },
 
