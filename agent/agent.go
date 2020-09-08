f4520828a1a82392a317a11f665b46a6d76446af
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2020-09-08 09:08:31 -0300

    agent: refactor to cleanup main.go

--- agent/agent.go
+++ agent/agent.go
@@ -2,45 +2,81 @@ package main
 
 import (
 	"crypto/rsa"
+	"net/url"
 	"os"
 
+	"github.com/Masterminds/semver"
+	"github.com/pkg/errors"
 	"github.com/shellhub-io/shellhub/agent/pkg/keygen"
 	"github.com/shellhub-io/shellhub/agent/pkg/sysinfo"
+	"github.com/shellhub-io/shellhub/pkg/api/client"
 	"github.com/shellhub-io/shellhub/pkg/models"
-	"github.com/sirupsen/logrus"
+	"github.com/shellhub-io/shellhub/pkg/revdial"
 )
 
 type Agent struct {
-	opts     *ConfigOptions
-	pubKey   *rsa.PublicKey
-	Identity *models.DeviceIdentity
-	Info     *models.DeviceInfo
+	opts          *ConfigOptions
+	pubKey        *rsa.PublicKey
+	Identity      *models.DeviceIdentity
+	Info          *models.DeviceInfo
+	authData      *models.DeviceAuthResponse
+	cli           client.Client
+	serverInfo    *models.Info
+	serverAddress *url.URL
+	sessions      []string
 }
 
-func NewAgent() (*Agent, error) {
-	identity, err := getDeviceIdentity()
+func NewAgent(opts *ConfigOptions) (*Agent, error) {
+	a := &Agent{}
+
+	serverAddress, err := url.Parse(opts.ServerAddress)
 	if err != nil {
 		return nil, err
 	}
 
-	info, err := getDeviceInfo()
-	if err != nil {
-		return nil, err
+	a.serverAddress = serverAddress
+
+	return &Agent{
+		opts: opts,
+		cli:  client.NewClient(client.WithURL(serverAddress)),
+	}, nil
+}
+
+// initialize initializes agent
+func (a *Agent) initialize() error {
+	if err := a.generateDeviceIdentity(); err != nil {
+		return errors.Wrap(err, "failed to generate device identity")
 	}
 
-	return &Agent{Identity: identity, Info: info}, nil
+	if err := a.loadDeviceInfo(); err != nil {
+		return errors.Wrap(err, "failed to load device info")
+	}
+
+	if err := a.generatePrivateKey(); err != nil {
+		return errors.Wrap(err, "failed to generate private key")
+	}
+
+	if err := a.readPublicKey(); err != nil {
+		return errors.Wrap(err, "failed to read public key")
+	}
+
+	if err := a.probeServerInfo(); err != nil {
+		return errors.Wrap(err, "failed to probe server info")
+	}
+
+	if err := a.authorize(); err != nil {
+		return errors.Wrap(err, "failed to authorize device")
+	}
+
+	return nil
 }
 
 func (a *Agent) generatePrivateKey() error {
 	if _, err := os.Stat(a.opts.PrivateKey); os.IsNotExist(err) {
-		logrus.Info("Generating private key prior start communication...")
-
 		err := keygen.GeneratePrivateKey(a.opts.PrivateKey)
 		if err != nil {
 			return err
 		}
-
-		logrus.Info("Private key generated successfully.")
 	}
 
 	return nil
@@ -52,24 +88,71 @@ func (a *Agent) readPublicKey() error {
 	return err
 }
 
-func getDeviceIdentity() (*models.DeviceIdentity, error) {
-	d := &models.DeviceIdentity{}
-
+// generateDeviceIdentity generates device identity
+func (a *Agent) generateDeviceIdentity() error {
 	iface, err := sysinfo.PrimaryInterface()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	d.MAC = iface.HardwareAddr.String()
+	a.Identity = &models.DeviceIdentity{
+		MAC: iface.HardwareAddr.String(),
+	}
 
-	return d, nil
+	return nil
 }
 
-func getDeviceInfo() (*models.DeviceInfo, error) {
+// loadDeviceInfo load some device information
+func (a *Agent) loadDeviceInfo() error {
 	osrelease, err := sysinfo.GetOSRelease()
+	if err != nil {
+		return nil
+	}
+
+	a.Info = &models.DeviceInfo{
+		ID:         osrelease.ID,
+		PrettyName: osrelease.Name,
+		Version:    AgentVersion,
+	}
+
+	return nil
+}
+
+// checkUpdate check for agent updates
+func (a *Agent) checkUpdate() (*semver.Version, error) {
+	info, err := a.cli.GetInfo()
 	if err != nil {
 		return nil, err
 	}
 
-	return &models.DeviceInfo{ID: osrelease.ID, PrettyName: osrelease.Name}, nil
+	return semver.NewVersion(info.Version)
+}
+
+// probeServerInfo probe server information
+func (a *Agent) probeServerInfo() error {
+	info, err := a.cli.GetInfo()
+	a.serverInfo = info
+	return err
+}
+
+// authorize send auth request to the server
+func (a *Agent) authorize() error {
+	authData, err := a.cli.AuthDevice(&models.DeviceAuthRequest{
+		Info:     a.Info,
+		Sessions: a.sessions,
+		DeviceAuth: &models.DeviceAuth{
+			Hostname:  a.opts.PreferredHostname,
+			Identity:  a.Identity,
+			TenantID:  a.opts.TenantID,
+			PublicKey: string(keygen.EncodePublicKeyToPem(a.pubKey)),
+		},
+	})
+
+	a.authData = authData
+
+	return err
+}
+
+func (a *Agent) newReverseListener() (*revdial.Listener, error) {
+	return a.cli.NewReverseListener(a.authData.Token)
 }
--- pkg/api/client/client_public.go
+++ pkg/api/client/client_public.go
@@ -3,7 +3,16 @@
 package client
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/gorilla/websocket"
 	"github.com/shellhub-io/shellhub/pkg/models"
+	"github.com/shellhub-io/shellhub/pkg/revdial"
+	"github.com/shellhub-io/shellhub/pkg/wsconnadapter"
 )
 
 const (
@@ -20,6 +29,7 @@ type publicAPI interface {
 	GetInfo() (*models.Info, error)
 	Endpoints() (*models.Endpoints, error)
 	AuthDevice(req *models.DeviceAuthRequest) (*models.DeviceAuthResponse, error)
+	NewReverseListener(token string) (*revdial.Listener, error)
 }
 
 func (c *client) GetInfo() (*models.Info, error) {
@@ -50,4 +60,27 @@ func (c *client) Endpoints() (*models.Endpoints, error) {
 	}
 
 	return endpoints, nil
+}
+
+func (c *client) NewReverseListener(token string) (*revdial.Listener, error) {
+	req, _ := http.NewRequest("GET", "", nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	url := regexp.MustCompile(`^https?`).ReplaceAllString(buildURL(c, "/ssh/connection"), "ws")
+	conn, _, err := websocket.DefaultDialer.Dial(url, req.Header)
+	if err != nil {
+		return nil, err
+	}
+
+	listener := revdial.NewListener(wsconnadapter.New(conn),
+		func(ctx context.Context, path string) (*websocket.Conn, *http.Response, error) {
+			return tunnelDial(ctx, strings.Replace(c.scheme, "http", "ws", 1), c.host, path)
+		},
+	)
+
+	return listener, nil
+}
+
+func tunnelDial(ctx context.Context, protocol, address, path string) (*websocket.Conn, *http.Response, error) {
+	return websocket.DefaultDialer.DialContext(ctx, strings.Join([]string{fmt.Sprintf("%s://%s", protocol, address), path}, ""), nil)
 }
