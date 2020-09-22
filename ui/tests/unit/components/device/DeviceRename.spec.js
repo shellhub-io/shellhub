a24cc76e937d4f460216736faa4516cc370c81a4
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-09-22 11:28:12 -0300

    ui: Update unit test in deviceRenama component, verify the variables
    Verify the variables and add another's actions use on the component.
    And add a blank line to separate the wrapper from using variables.
    
    This closes #423.

--- ui/tests/unit/components/device/DeviceRename.spec.js
+++ ui/tests/unit/components/device/DeviceRename.spec.js
@@ -7,13 +7,23 @@ describe('DeviceRename', () => {
   localVue.use(Vuex);
 
   let wrapper;
+
   const uid = 'a582b47a42d';
   const name = '39-5e-2a';
 
   const store = new Vuex.Store({
     namespaced: true,
+    state: {
+    },
+    getters: {
+      'devices/get': (state) => state.device,
+    },
     actions: {
       'devices/rename': () => {
+      },
+      'modals/showSnackbarSuccessAction': () => {
+      },
+      'modals/showSnackbarErrorAction': () => {
       },
     },
   });
@@ -36,5 +46,10 @@ describe('DeviceRename', () => {
   it('Receive data in props', () => {
     expect(wrapper.vm.name).toEqual(name);
     expect(wrapper.vm.uid).toEqual(uid);
+  });
+  it('Compare data with default value', () => {
+    expect(wrapper.vm.dialog).toEqual(false);
+    expect(wrapper.vm.invalid).toEqual(false);
+    expect(wrapper.vm.editName).toEqual('39-5e-2a');
   });
 });
