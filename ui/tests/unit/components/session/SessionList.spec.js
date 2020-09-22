c2dbe5fc852429934b73230fa2fddc1e363e093b
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-09-22 15:39:06 -0300

    ui: standard tests in the session directory
    This remove variable declare inside store to scope, add blank line,
    and organizes just like #414.

--- ui/tests/unit/components/session/SessionList.spec.js
+++ ui/tests/unit/components/session/SessionList.spec.js
@@ -8,68 +8,71 @@ describe('DeviceAdd', () => {
 
   let wrapper;
 
+  const numberSessions = 2;
+  const sessions = [
+    {
+      uid: '8c354a00f50',
+      device_uid: 'a582b47a42d',
+      device: {
+        uid: 'a582b47a42d',
+        name: '39-5e-2a',
+        identity: {
+          mac: '00:00:00:00:00:00',
+        },
+        info: {
+          id: 'debian',
+          pretty_name: 'Debian GNU/Linux 10 (buster)',
+          version: 'v0.2.5',
+        },
+        public_key: '----- PUBLIC KEY -----',
+        tenant_id: '00000000',
+        last_seen: '2020-05-18T13:27:02.498Z',
+        online: false,
+        namespace: 'user',
+      },
+      tenant_id: '00000000',
+      username: 'user',
+      ip_address: '000.000.000.000',
+      started_at: '2020-05-18T12:30:28.824Z',
+      last_seen: '2020-05-18T12:30:30.205Z',
+      active: false,
+      authenticated: false,
+    },
+    {
+      uid: '8c354a00f51',
+      device_uid: 'a582b47a42d',
+      device: {
+        uid: 'a582b47a42d',
+        name: 'b4-2e-99',
+        identity: {
+          mac: '00:00:00:00:00:00',
+        },
+        info: {
+          id: 'debian',
+          pretty_name: 'Debian GNU/Linux 10 (buster)',
+          version: 'v0.2.5',
+        },
+        public_key: '----- PUBLIC KEY -----',
+        tenant_id: '00000000',
+        last_seen: '2020-05-18T13:27:02.498Z',
+        online: false,
+        namespace: 'user',
+      },
+      tenant_id: '00000000',
+      username: 'user',
+      ip_address: '000.000.000.000',
+      started_at: '2020-05-18T12:30:28.824Z',
+      last_seen: '2020-05-18T12:30:30.205Z',
+      active: false,
+      authenticated: false,
+    },
+  ];
+
   const store = new Vuex.Store({
     namespaced: true,
     state: {
-      sessions: [
-        {
-          uid: '8c354a00f50',
-          device_uid: 'a582b47a42d',
-          device: {
-            uid: 'a582b47a42d',
-            name: '39-5e-2a',
-            identity: {
-              mac: '00:00:00:00:00:00',
-            },
-            info: {
-              id: 'debian',
-              pretty_name: 'Debian GNU/Linux 10 (buster)',
-              version: 'v0.2.5',
-            },
-            public_key: '----- PUBLIC KEY -----',
-            tenant_id: '00000000',
-            last_seen: '2020-05-18T13:27:02.498Z',
-            online: false,
-            namespace: 'user',
-          },
-          tenant_id: '00000000',
-          username: 'user',
-          ip_address: '000.000.000.000',
-          started_at: '2020-05-18T12:30:28.824Z',
-          last_seen: '2020-05-18T12:30:30.205Z',
-          active: false,
-          authenticated: false,
-        },
-        {
-          uid: '8c354a00f50',
-          device_uid: 'a582b47a42d',
-          device: {
-            uid: 'a582b47a42d',
-            name: 'b4-2e-99',
-            identity: {
-              mac: '00:00:00:00:00:00',
-            },
-            info: {
-              id: 'debian',
-              pretty_name: 'Debian GNU/Linux 10 (buster)',
-              version: 'v0.2.5',
-            },
-            public_key: '----- PUBLIC KEY -----',
-            tenant_id: '00000000',
-            last_seen: '2020-05-18T13:27:02.498Z',
-            online: false,
-            namespace: 'user',
-          },
-          tenant_id: '00000000',
-          username: 'user',
-          ip_address: '000.000.000.000',
-          started_at: '2020-05-18T12:30:28.824Z',
-          last_seen: '2020-05-18T12:30:30.205Z',
-          active: false,
-          authenticated: false,
-        },
-      ],
-      numberSessions: 2,
+      sessions,
+      numberSessions,
     },
     getters: {
       'sessions/list': (state) => state.sessions,
