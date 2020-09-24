c90d04e1798e0c9b9a688095304ecf2f903f5f60
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-09-24 13:59:10 -0300

    ui: Update test on the settings components

--- ui/tests/unit/components/setting/SettingProfile.spec.js
+++ ui/tests/unit/components/setting/SettingProfile.spec.js
@@ -8,8 +8,22 @@ describe('SettingProfile', () => {
 
   let wrapper;
 
+  const user = 'ShellHub';
+  const email = 'shellhub@shellhub.com';
+  const tenant = '';
+
   const store = new Vuex.Store({
     namespaced: true,
+    state: {
+      user,
+      email,
+      tenant,
+    },
+    getters: {
+      'auth/currentUser': (state) => state.user,
+      'auth/email': (state) => state.email,
+      'auth/tenant': (state) => state.tenant,
+    },
     actions: {
       'users/put': () => {
       },
@@ -29,5 +43,18 @@ describe('SettingProfile', () => {
   });
   it('Renders the component', () => {
     expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('Process data in the computed', () => {
+    expect(wrapper.vm.tenant).toEqual(tenant);
+  });
+  it('Compare data with default value', () => {
+    expect(wrapper.vm.username).toEqual(user);
+    expect(wrapper.vm.email).toEqual(email);
+    expect(wrapper.vm.currentPassword).toEqual('');
+    expect(wrapper.vm.newPassword).toEqual('');
+    expect(wrapper.vm.newPasswordConfirm).toEqual('');
+    expect(wrapper.vm.editDataStatus).toEqual(false);
+    expect(wrapper.vm.editPasswordStatus).toEqual(false);
+    expect(wrapper.vm.show).toEqual(false);
   });
 });
