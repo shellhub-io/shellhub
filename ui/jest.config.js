3ec5dd0190f3addd5e45055040df040e8175c57d
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-09-24 14:15:12 -0300

    ui: Update unit test on the settings, renders the template with data
    This test uses the mount, and necessary to mock the rules of the vee-validade.
    
    This closes #448.

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -6,4 +6,10 @@ module.exports = {
     '\\.(css|less|sass|scss)$': '<rootDir>/tests/__mocks__/styleMock.js',
     '\\.(gif|ttf|eot|svg)$': '<rootDir>/tests/__mocks__/fileMock.js',
   },
+  transform: {
+    'vee-validate/dist/rules': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!vee-validate/dist/rules)',
+  ],
 };
