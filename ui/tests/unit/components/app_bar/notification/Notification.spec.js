cb4b4b2dd6b5da08fa5403f527b0c18af77852ad
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-10-01 15:39:21 -0300

    ui: Update test in notification, verify the render of the items

--- ui/tests/unit/components/app_bar/notification/Notification.spec.js
+++ ui/tests/unit/components/app_bar/notification/Notification.spec.js
@@ -8,11 +8,51 @@ describe('Notification', () => {
 
   let wrapper;
 
+  const numberNotifications = 2;
+  const notifications = [
+    {
+      uid: 'a582b47a42d',
+      name: '39-5e-2a',
+      identity: {
+        mac: '00:00:00:00:00:00',
+      },
+      info: {
+        id: 'linuxmint',
+        pretty_name: 'Linux Mint 19.3',
+        version: '',
+      },
+      public_key: '----- PUBLIC KEY -----',
+      tenant_id: '00000000',
+      last_seen: '2020-05-20T18:58:53.276Z',
+      online: false,
+      namespace: 'user',
+      status: 'pending',
+    },
+    {
+      uid: 'a582b47a42e',
+      name: '39-5e-2b',
+      identity: {
+        mac: '00:00:00:00:00:00',
+      },
+      info: {
+        id: 'linuxmint',
+        pretty_name: 'Linux Mint 19.3',
+        version: '',
+      },
+      public_key: '----- PUBLIC KEY -----',
+      tenant_id: '00000001',
+      last_seen: '2020-05-20T19:58:53.276Z',
+      online: true,
+      namespace: 'user',
+      status: 'pending',
+    },
+  ];
+
   const store = new Vuex.Store({
     namespaced: true,
     state: {
-      notifications: [],
-      numberNotifications: 0,
+      notifications,
+      numberNotifications,
     },
     getters: {
       'notifications/list': (state) => state.notifications,
@@ -28,7 +68,7 @@ describe('Notification', () => {
     wrapper = shallowMount(Notification, {
       store,
       localVue,
-      stubs: ['fragment'],
+      stubs: ['fragment', 'router-link'],
     });
   });
 
@@ -37,5 +77,11 @@ describe('Notification', () => {
   });
   it('Renders the component', () => {
     expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('Renders the template with data', () => {
+    Object.keys(notifications).forEach((field) => {
+      expect(wrapper.find(`[data-test="${notifications[field].uid}-field"]`).text()).toEqual(notifications[field].name);
+      expect(wrapper.find(`[data-test="${notifications[field].uid}-btn"]`).exists()).toEqual(true);
+    });
   });
 });
