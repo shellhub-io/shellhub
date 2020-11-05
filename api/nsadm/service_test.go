27d299936cc4c2630c489c655802d84008c82861
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-11-04 23:17:24 -0300

    api: update tests

--- api/nsadm/service_test.go
+++ api/nsadm/service_test.go
@@ -107,3 +107,51 @@ func TestDeleteNamespace(t *testing.T) {
 
 	mock.AssertExpectations(t)
 }
+
+func TestAddNamespaceUser(t *testing.T) {
+	mock := &mocks.Store{}
+	s := NewService(store.Store(mock))
+	ctx := context.TODO()
+	user := &models.User{Name: "user1", Username: "username1", ID: "hash1"}
+	member := &models.User{Name: "user2", Username: "username2", ID: "hash2"}
+	namespace := &models.Namespace{Name: "group1", Owner: "hash1", TenantID: "a736a52b-5777-4f92-b0b8-e359bf484713", Members: []string{"hash1"}}
+	namespace2 := &models.Namespace{Name: "group1", Owner: "hash1", TenantID: "a736a52b-5777-4f92-b0b8-e359bf484713", Members: []string{"hash1", "hash2"}}
+
+	mock.On("GetNamespace", ctx, namespace.TenantID).Return(namespace, nil).Once()
+	mock.On("GetUserByUsername", ctx, user.Username).Return(user, nil).Once()
+	mock.On("GetUserByUsername", ctx, member.Username).Return(member, nil).Once()
+	mock.On("AddNamespaceUser", ctx, namespace.TenantID, member.ID).Return(nil).Once()
+
+	err := s.AddNamespaceUser(ctx, namespace.TenantID, member.Username, user.Username)
+	assert.NoError(t, err)
+
+	mock.On("GetNamespace", ctx, namespace.TenantID).Return(namespace2, nil).Once()
+
+	returnedNamespace, err := s.GetNamespace(ctx, namespace.TenantID)
+	assert.NoError(t, err)
+	assert.Equal(t, namespace2, returnedNamespace)
+}
+
+func TestRemoveNamespaceUser(t *testing.T) {
+	mock := &mocks.Store{}
+	s := NewService(store.Store(mock))
+	ctx := context.TODO()
+	user := &models.User{Name: "user1", Username: "username1", ID: "hash1"}
+	member := &models.User{Name: "user2", Username: "username2", ID: "hash2"}
+	namespace := &models.Namespace{Name: "group1", Owner: "hash1", TenantID: "a736a52b-5777-4f92-b0b8-e359bf484713", Members: []string{"hash1", "hash2"}}
+	namespace2 := &models.Namespace{Name: "group1", Owner: "hash1", TenantID: "a736a52b-5777-4f92-b0b8-e359bf484713", Members: []string{"hash1"}}
+
+	mock.On("GetNamespace", ctx, namespace.TenantID).Return(namespace, nil).Once()
+	mock.On("GetUserByUsername", ctx, user.Username).Return(user, nil).Once()
+	mock.On("GetUserByUsername", ctx, member.Username).Return(member, nil).Once()
+	mock.On("RemoveNamespaceUser", ctx, namespace.TenantID, member.ID).Return(nil).Once()
+
+	err := s.RemoveNamespaceUser(ctx, namespace.TenantID, member.Username, user.Username)
+	assert.NoError(t, err)
+
+	mock.On("GetNamespace", ctx, namespace.TenantID).Return(namespace2, nil).Once()
+
+	returnedNamespace, err := s.GetNamespace(ctx, namespace.TenantID)
+	assert.NoError(t, err)
+	assert.Equal(t, namespace2, returnedNamespace)
+}
