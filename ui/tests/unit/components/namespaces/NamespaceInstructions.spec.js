00af8b94cfd1a4fe5e989ebc3191aedcd1a3f483
vagnersaller <vsaller@yahoo.com>
2020-11-19 09:12:02 -0300

    ui: add unit test to namespace instructions

---
+++ ui/tests/unit/components/namespaces/NamespaceInstructions.spec.js
@@ -0,0 +1,35 @@
+import Vuex from 'vuex';
+import { shallowMount, createLocalVue, config } from '@vue/test-utils';
+import NamespaceInstructions from '@/components/namespace/NamespaceInstructions';
+
+config.mocks = {
+  $env: {
+    isHosted: true,
+  },
+};
+
+describe('NamespaceInstructions', () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  let wrapper;
+  const show = true;
+
+  beforeEach(() => {
+    wrapper = shallowMount(NamespaceInstructions, {
+      localVue,
+      stubs: ['fragment'],
+      propsData: { show },
+    });
+  });
+
+  it('Is a Vue instance', () => {
+    expect(wrapper).toBeTruthy();
+  });
+  it('Renders the component', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('Receives data in props', () => {
+    expect(wrapper.vm.show).toEqual(show);
+  });
+});
