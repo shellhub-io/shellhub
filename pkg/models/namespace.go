f59109c789be5eaee2dd2d24c522e88ca594d419
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-11-19 11:24:11 -0300

    pkg: update models to allow namespace support

---
+++ pkg/models/namespace.go
@@ -0,0 +1,20 @@
+package models
+
+type Namespace struct {
+	Name     string             `json:"name"  validate:"required,hostname_rfc1123"`
+	Owner    string             `json:"owner"`
+	TenantID string             `json:"tenant_id" bson:"tenant_id,omitempty"`
+	Members  interface{}        `json:"members" bson:"members"`
+	Settings *NamespaceSettings `json:"settings"`
+	Devices  int                `json:"devices" bson:",omitempty"`
+	Sessions int                `json:"sessions" bson:",omitempty"`
+}
+
+type NamespaceSettings struct {
+	SessionRecord bool `json:"session_record" bson:"session_record,omitempty"`
+}
+
+type Member struct {
+	ID   string `json:"id" bson:"id"`
+	Name string `json:"name,omitempty" bson:"-"`
+}
--- pkg/models/user.go
+++ pkg/models/user.go
@@ -5,11 +5,12 @@ import (
 )
 
 type User struct {
+	ID            string `json:"id,omitempty" bson:"_id,omitempty"`
 	Name          string `json:"name"`
 	Email         string `json:"email" bson:",omitempty" validate:"email"`
 	Username      string `json:"username" bson:",omitempty"`
 	Password      string `json:"password" bson:",omitempty"`
-	TenantID      string `json:"tenant_id" bson:"tenant_id"`
+	TenantID      string `json:"tenant_id" bson:"-"`
 	Devices       int    `json:"devices" bson:",omitempty"`
 	Sessions      int    `json:"sessions" bson:",omitempty"`
 	SessionRecord bool   `json:"session_record" bson:"session_record,omitempty"`
@@ -24,6 +25,7 @@ type UserAuthResponse struct {
 	Token  string `json:"token"`
 	User   string `json:"user"`
 	Name   string `json:"name"`
+	ID     string `json:"id"`
 	Tenant string `json:"tenant"`
 	Email  string `json:"email"`
 }
@@ -32,6 +34,7 @@ type UserAuthClaims struct {
 	Username string `json:"name"`
 	Admin    bool   `json:"admin"`
 	Tenant   string `json:"tenant"`
+	ID       string `json:"id"`
 
 	AuthClaims         `json:",squash"`
 	jwt.StandardClaims `json:",squash"`
