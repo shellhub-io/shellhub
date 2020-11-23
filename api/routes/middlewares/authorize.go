d53cec14e1505de9cf69d438bceb836797064f32
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2020-11-23 10:47:18 -0300

    api: add namespace to check for active namespace in auth token

---
+++ api/routes/middlewares/authorize.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/shellhub-io/shellhub/api/apicontext"
+)
+
+func Authorize(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := context.WithValue(c.Request().Context(), "ctx", c.(*apicontext.Context))
+
+		id := apicontext.IDFromContext(ctx)
+		tenant := apicontext.TenantFromContext(ctx)
+		if id != nil && tenant == nil {
+			return c.NoContent(http.StatusForbidden)
+		}
+
+		return next(c)
+	}
+}
