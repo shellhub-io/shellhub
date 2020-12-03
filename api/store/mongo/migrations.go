afb302720f7245ec8f6a3626019a5ecc3f28be7b
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-12-03 17:26:21 -0300

    api: update migrations to remove tenant from users

--- api/store/mongo/migrations.go
+++ api/store/mongo/migrations.go
@@ -328,6 +328,74 @@ var migrations = []migrate.Migration{
 			return err
 		},
 	},
+	{
+		Version: 14,
+		Up: func(db *mongo.Database) error {
+			type user struct {
+				Username      string `json:"username" bson:",omitempty"`
+				TenantID      string `json:"tenant_id" bson:"tenant_id"`
+				ID            string `json:"id,omitempty" bson:"_id,omitempty"`
+				SessionRecord bool   `json:"session_record" bson:"session_record,omitempty"`
+			}
+			if _, err := db.Collection("users").Indexes().DropOne(context.TODO(), "tenant_id"); err != nil {
+				return err
+			}
+			if _, err := db.Collection("users").Indexes().DropOne(context.TODO(), "session_record"); err != nil {
+				return err
+			}
+
+			cursor, err := db.Collection("users").Find(context.TODO(), bson.D{})
+			if err != nil {
+				return err
+			}
+			defer cursor.Close(context.TODO())
+			for cursor.Next(context.TODO()) {
+				user := new(user)
+				err := cursor.Decode(&user)
+				if err != nil {
+					return err
+				}
+				settings := &models.NamespaceSettings{SessionRecord: true}
+				namespace := &models.Namespace{
+					Owner:    user.ID,
+					Members:  []string{user.ID},
+					TenantID: user.TenantID,
+					Name:     user.Username,
+					Settings: settings,
+				}
+				_, err = db.Collection("namespaces").InsertOne(context.TODO(), &namespace)
+				if err != nil {
+					return nil
+				}
+
+				if _, err := db.Collection("users").UpdateOne(context.TODO(), bson.M{"tenant_id": user.TenantID}, bson.M{"$unset": bson.M{"tenant_id": ""}}); err != nil {
+					return err
+				}
+				if _, err := db.Collection("users").UpdateOne(context.TODO(), bson.M{"tenant_id": user.TenantID}, bson.M{"$unset": bson.M{"session_record": ""}}); err != nil {
+					return err
+				}
+			}
+
+			return cursor.Err()
+		},
+		Down: func(db *mongo.Database) error {
+			cursor, err := db.Collection("namespaces").Find(context.TODO(), bson.D{})
+			if err != nil {
+				return err
+			}
+			defer cursor.Close(context.TODO())
+			for cursor.Next(context.TODO()) {
+				namespace := new(models.Namespace)
+				err := cursor.Decode(&namespace)
+				if err != nil {
+					return err
+				}
+				_, err = db.Collection("users").UpdateOne(context.TODO(), bson.M{"_id": namespace.Owner}, bson.M{"$set": bson.M{"tenant": namespace.TenantID}})
+			}
+
+			return err
+		},
+	},
 }
 
 func ApplyMigrations(db *mongo.Database) error {
