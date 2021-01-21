77014aa91e65165b001f18cea078c7d7783e57b9
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2021-01-21 10:48:57 -0300

    agent: increase version to v0.5.0 in docker updater

--- agent/selfupdater/updater_docker.go
+++ agent/selfupdater/updater_docker.go
@@ -92,24 +92,24 @@ func (d *dockerUpdater) CompleteUpdate() error {
 			}
 		}
 
-                // Append /var/run and /var/log to mount if old container
-                // version is less than v0.4.2 since utmp and wtmp are
-                // required inside container to record login sessions
-                v0_4_2, _ := semver.NewVersion("v0.4.2")
-                if v.LessThan(v0_4_2) {
-                        parent.info.HostConfig.Mounts = []mount.Mount{
-                                mount.Mount{
-                                        Type:   mount.TypeBind,
-                                        Source: "/var/run",
-                                        Target: "/var/run",
-                                },
-                                mount.Mount{
-                                        Type:   mount.TypeBind,
-                                        Source: "/var/log",
-                                        Target: "/var/log",
-                                },
-                        }
-                }
+		// Append /var/run and /var/log to mount if old container
+		// version is less than v0.5.0 since utmp and wtmp are
+		// required inside container to record login sessions
+		v0_5_0, _ := semver.NewVersion("v0.5.0")
+		if v.LessThan(v0_5_0) {
+			parent.info.HostConfig.Mounts = []mount.Mount{
+				mount.Mount{
+					Type:   mount.TypeBind,
+					Source: "/var/run",
+					Target: "/var/run",
+				},
+				mount.Mount{
+					Type:   mount.TypeBind,
+					Source: "/var/log",
+					Target: "/var/log",
+				},
+			}
+		}
 
 		_, err = d.updateContainer(parent, container.info.Config.Image, parent.info.Name, false)
 		if err != nil {
