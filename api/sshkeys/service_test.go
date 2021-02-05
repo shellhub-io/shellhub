b4e4fe7104176b17ae153bc411c813be23802034
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2021-02-05 10:47:40 -0300

    api: update sshkeys service tests

---
+++ api/sshkeys/service_test.go
@@ -0,0 +1,130 @@
+package sshkeys
+
+import (
+	"context"
+
+	"testing"
+	"time"
+
+	//"fmt"
+
+	"github.com/shellhub-io/shellhub/api/store"
+	"github.com/shellhub-io/shellhub/api/store/mocks"
+	"github.com/shellhub-io/shellhub/pkg/api/paginator"
+	"github.com/shellhub-io/shellhub/pkg/models"
+	"github.com/stretchr/testify/assert"
+	//"golang.org/x/crypto/ssh"
+)
+
+/*type Service interface {
+	ListPublicKeys(ctx context.Context, pagination paginator.Query) ([]models.PublicKey, int, error)
+	GetPublicKey(ctx context.Context, fingerprint, tenant string) (*models.PublicKey, error)
+	CreatePublicKey(ctx context.Context, key *models.PublicKey) error
+	UpdatePublicKey(ctx context.Context, fingerprint, tenant string, key *models.PublicKeyUpdate) (*models.PublicKey, error)
+	DeletePublicKey(ctx context.Context, fingerprint, tenant string) error
+	CreatePrivateKey(ctx context.Context) (*models.PrivateKey, error)
+}*/
+
+func TestListPublicKeys(t *testing.T) {
+	mock := &mocks.Store{}
+	s := NewService(store.Store(mock))
+
+	ctx := context.TODO()
+	keys := []models.PublicKey{
+		{Data: []byte("teste"), Fingerprint: "fingerprint", CreatedAt: time.Now(), TenantID: "tenant1", PublicKeyFields: models.PublicKeyFields{Name: "teste"}},
+		{Data: []byte("teste2"), Fingerprint: "fingerprint2", CreatedAt: time.Now(), TenantID: "tenant2", PublicKeyFields: models.PublicKeyFields{Name: "teste2"}},
+	}
+
+	query := paginator.Query{Page: 1, PerPage: 10}
+
+	mock.On("ListPublicKeys", ctx, query).Return(keys, len(keys), nil).Once()
+	returnedKeys, count, err := s.ListPublicKeys(ctx, query)
+	assert.NoError(t, err)
+	assert.Equal(t, keys, returnedKeys)
+	assert.Equal(t, count, len(keys))
+	mock.AssertExpectations(t)
+
+}
+
+func TestGetPublicKeys(t *testing.T) {
+	mock := &mocks.Store{}
+	s := NewService(store.Store(mock))
+
+	ctx := context.TODO()
+	key := &models.PublicKey{
+		Data: []byte("teste"), Fingerprint: "fingerprint", CreatedAt: time.Now(), TenantID: "tenant1", PublicKeyFields: models.PublicKeyFields{Name: "teste"},
+	}
+
+	mock.On("GetPublicKey", ctx, key.Fingerprint, key.TenantID).Return(key, nil).Once()
+	returnedKey, err := s.GetPublicKey(ctx, key.Fingerprint, key.TenantID)
+	assert.NoError(t, err)
+	assert.Equal(t, key, returnedKey)
+	mock.AssertExpectations(t)
+
+}
+
+func TestUpdatePublicKeys(t *testing.T) {
+	mock := &mocks.Store{}
+	s := NewService(store.Store(mock))
+
+	ctx := context.TODO()
+	key := &models.PublicKey{
+		Data: []byte("teste"), Fingerprint: "fingerprint", CreatedAt: time.Now(), TenantID: "tenant1", PublicKeyFields: models.PublicKeyFields{Name: "teste"},
+	}
+	keyUpdate := &models.PublicKeyUpdate{
+		PublicKeyFields: models.PublicKeyFields{Name: "teste"},
+	}
+
+	newKey := &models.PublicKey{
+		Data: []byte("teste"), Fingerprint: "fingerprint", CreatedAt: time.Now(), TenantID: "tenant1", PublicKeyFields: models.PublicKeyFields{Name: "teste2"},
+	}
+
+	mock.On("UpdatePublicKey", ctx, key.Fingerprint, key.TenantID, keyUpdate).Return(newKey, nil).Once()
+	returnedKey, err := s.UpdatePublicKey(ctx, key.Fingerprint, key.TenantID, keyUpdate)
+	assert.NoError(t, err)
+	assert.Equal(t, newKey, returnedKey)
+	mock.AssertExpectations(t)
+
+}
+
+func TestDeletePublicKeys(t *testing.T) {
+	mock := &mocks.Store{}
+	s := NewService(store.Store(mock))
+
+	ctx := context.TODO()
+	key := &models.PublicKey{
+		Data: []byte("teste"), Fingerprint: "fingerprint", CreatedAt: time.Now(), TenantID: "tenant1", PublicKeyFields: models.PublicKeyFields{Name: "teste"},
+	}
+
+	mock.On("DeletePublicKey", ctx, key.Fingerprint, key.TenantID).Return(nil).Once()
+	err := s.DeletePublicKey(ctx, key.Fingerprint, key.TenantID)
+	assert.NoError(t, err)
+	mock.AssertExpectations(t)
+
+}
+
+func TestCreatePublicKey(t *testing.T) {
+	mock := &mocks.Store{}
+	s := NewService(store.Store(mock))
+
+	ctx := context.TODO()
+	sshRsa := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDhZgLqiZYDKCWhyi2gUXIRwIPyMSyXZ6yrwsm3PYfIvFB60kVlNgqDpPVhWoH6eRfaQ1y/xbg4nClZmHEDTvLbTQ1ZoQzzjZ7zvM6aQ4nADmKcCYswEuU94axouVjsHNyMLfOkPXuGec0fChwQ2JDh/B9LCiSDxyhCOgHvETXGXsyBMKjn498iPjJ6snzk35dy5wPZRz41g3dLaygF+wYAT791u/JchHQL7OP7RoNgby+RM16SYZs1tgQVkfU//o+AyTarWYLVDpFU6HPPenE4xEXhbgqd7x3wSNPBsMvY8Zjcu3kdHtboJidyMtKeD8ghV/T24kME58TW15T8Eg8R"
+	//pubKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(sshRsa))
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+
+	//fingerprint := ssh.FingerprintLegacyMD5(pubKey)
+	key := &models.PublicKey{
+		Data: []byte(sshRsa), TenantID: "tenant1", PublicKeyFields: models.PublicKeyFields{Name: "teste"},
+	}
+
+	//key2 := &models.PublicKey{
+	//	Data: []byte(sshRsa), Fingerprint: fingerprint, TenantID: "tenant1", PublicKeyFields: models.PublicKeyFields{Name: "teste"},
+	//}
+	mock.On("CreatePublicKey", ctx, key).Return(nil).Once()
+	err := s.CreatePublicKey(ctx, key)
+	assert.NoError(t, err)
+	mock.AssertExpectations(t)
+
+}
