751aeb82a5935bc34f888d08fe80c6ca9a45e455
LeonardoJoao <leonardorsjoao@gmail.com>
2021-03-04 10:58:35 -0300

    ui: Add billing page

--- ui/tests/unit/components/setting/SettingNamespace.spec.js
+++ ui/tests/unit/components/setting/SettingNamespace.spec.js
@@ -21,6 +21,22 @@ describe('SettingNamespace', () => {
     members: [{ id: '6', name: 'user6' }, { id: '7', name: 'user7' }, { id: '8', name: 'user8' }],
     owner: '6',
     tenant_id: 'e359bf484715',
+    devices_count: 1,
+    max_devices: 3,
+  };
+
+  const openNamespace = {
+    name: 'namespace3',
+    members: [{ id: '6', name: 'user6' }, { id: '7', name: 'user7' }, { id: '8', name: 'user8' }],
+    owner: '6',
+    tenant_id: 'e359bf484715',
+    devices_count: 1,
+    max_devices: -1,
+  };
+
+  const countDevicesHasNamespacePercent = {
+    maxDevices: 0,
+    percent: 0,
   };
 
   const idOwner = '6';
@@ -80,6 +96,32 @@ describe('SettingNamespace', () => {
     },
   });
 
+  const storeOwnerOpen = new Vuex.Store({
+    namespaced: true,
+    state: {
+      namespace: openNamespace,
+      id: idOwner,
+      owner,
+    },
+    getters: {
+      'namespaces/get': (state) => state.namespace,
+      'auth/id': (state) => state.id,
+      'namespaces/owner': (state) => state.owner,
+    },
+    actions: {
+      'namespaces/put': () => {
+      },
+      'namespaces/get': () => {
+      },
+      'namespaces/removeUser': () => {
+      },
+      'snackbar/showSnackbarSuccessAction': () => {
+      },
+      'snackbar/showSnackbarErrorAction': () => {
+      },
+    },
+  });
+
   beforeEach(() => {
     jest.spyOn(Storage.prototype, 'getItem').mockReturnValue('e359bf484715');
 
@@ -97,7 +139,7 @@ describe('SettingNamespace', () => {
     });
     wrapper3 = shallowMount(SettingNamespace, {
       localVue,
-      store: storeOwner,
+      store: storeOwnerOpen,
       stubs: ['fragment'],
       mocks: {
         $env: {
@@ -120,6 +162,8 @@ describe('SettingNamespace', () => {
     expect(wrapper.vm.namespace).toEqual(namespace);
     expect(wrapper.vm.tenant).toEqual(namespace.tenant_id);
     expect(wrapper.vm.isEnterpriseOwner).toEqual(true);
+    expect(wrapper.vm.isEnterprise).toEqual(true);
+    expect(wrapper3.vm.isEnterprise).toEqual(false);
   });
   it('Loads name when component is created', () => {
     wrapper.vm.$nextTick(() => {
@@ -134,9 +178,32 @@ describe('SettingNamespace', () => {
       expect(wrapper.find(`[data-test=${member.name}]`).text()).toEqual(member.name);
     });
   });
+  it('Process data in methods', () => {
+    expect(wrapper.vm.countDevicesHasNamespace()).toEqual(namespace.devices_count);
+    expect(wrapper3.vm.countDevicesHasNamespace()).toEqual(openNamespace.devices_count);
+
+    // hosted version tests
+    let percent = 0;
+    if (namespace.max_devices >= 0) {
+      percent = (wrapper.vm.countDevicesHasNamespace() / namespace.max_devices) * 100;
+    }
+    countDevicesHasNamespacePercent.maxDevices = namespace.max_devices;
+    countDevicesHasNamespacePercent.percent = percent;
+    expect(wrapper.vm.countDevicesHasNamespacePercent()).toEqual(countDevicesHasNamespacePercent);
+
+    // open version tests
+    percent = 0;
+    if (openNamespace.max_devices >= 0) {
+      percent = (wrapper.vm.countDevicesHasNamespace() / openNamespace.max_devices) * 100;
+    }
+    countDevicesHasNamespacePercent.maxDevices = openNamespace.max_devices;
+    countDevicesHasNamespacePercent.percent = percent;
+    expect(wrapper3.vm.countDevicesHasNamespacePercent()).toEqual(countDevicesHasNamespacePercent);
+  });
   // hosted version tests
   it('Check owner fields rendering in hosted version of the template', () => {
     expect(wrapper.find('[data-test=editOperation]').exists()).toEqual(true);
+    expect(wrapper.find('[data-test=usageOverview]').exists()).toEqual(true);
     expect(wrapper.find('[data-test=userOperation]').exists()).toEqual(true);
     expect(wrapper.find('[data-test=deleteOperation]').exists()).toEqual(true);
     expect(wrapper.find('[data-test=securityOperation]').exists()).toEqual(true);
@@ -150,6 +217,7 @@ describe('SettingNamespace', () => {
     const namespaceOwnerMessage = `Contact ${namespace.members[0].name} user for more information.`;
 
     expect(wrapper2.find('[data-test=editOperation]').exists()).toEqual(false);
+    expect(wrapper2.find('[data-test=usageOverview]').exists()).toEqual(true);
     expect(wrapper2.find('[data-test=userOperation]').exists()).toEqual(false);
     expect(wrapper2.find('[data-test=deleteOperation]').exists()).toEqual(false);
     expect(wrapper2.find('[data-test=securityOperation]').exists()).toEqual(false);
@@ -160,6 +228,7 @@ describe('SettingNamespace', () => {
   // open version tests
   it('Check owner fields rendering in open version of the template.', () => {
     expect(wrapper3.find('[data-test=editOperation]').exists()).toEqual(true);
+    expect(wrapper3.find('[data-test=usageOverview]').exists()).toEqual(false);
     expect(wrapper3.find('[data-test=userOperation]').exists()).toEqual(true);
     expect(wrapper3.find('[data-test=deleteOperation]').exists()).toEqual(true);
     expect(wrapper3.find('[data-test=securityOperation]').exists()).toEqual(false);
