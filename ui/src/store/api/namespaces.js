b6a60ef4960ef4b5083aafa6bfd396d3018db11b
LeonardoJoao <leonardorsjoao@gmail.com>
2021-03-08 17:00:10 -0300

    ui: Pattern namespace route, add S to the name

--- ui/src/store/api/namespaces.js
+++ ui/src/store/api/namespaces.js
@@ -1,24 +1,24 @@
 import http from '@/store/helpers/http';
 
-export const postNamespace = async (data) => http().post('/namespace', {
+export const postNamespace = async (data) => http().post('/namespaces', {
   name: data.name,
 });
 
-export const fetchNamespaces = async () => http().get('/namespace');
+export const fetchNamespaces = async () => http().get('/namespaces');
 
-export const getNamespace = async (id) => http().get(`/namespace/${id}`);
+export const getNamespace = async (id) => http().get(`/namespaces/${id}`);
 
-export const removeNamespace = async (id) => http().delete(`/namespace/${id}`);
+export const removeNamespace = async (id) => http().delete(`/namespaces/${id}`);
 
-export const putNamespace = async (data) => http().put(`/namespace/${data.id}`, {
+export const putNamespace = async (data) => http().put(`/namespaces/${data.id}`, {
   name: data.name,
 });
 
-export const addUserToNamespace = async (data) => http().patch(`/namespace/${data.tenant_id}/add`, {
+export const addUserToNamespace = async (data) => http().patch(`/namespaces/${data.tenant_id}/add`, {
   username: data.username,
 });
 
-export const removeUserFromNamespace = async (data) => http().patch(`/namespace/${data.tenant_id}/del`, {
+export const removeUserFromNamespace = async (data) => http().patch(`/namespaces/${data.tenant_id}/del`, {
   username: data.username,
 });
 
