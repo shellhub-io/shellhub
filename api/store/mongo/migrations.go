5ccd0351090f133f458e163a3d61ffde576feef6
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2021-04-23 13:39:05 -0300

    api: Fix migrations to remove orphan sessions and records

--- api/store/mongo/migrations.go
+++ api/store/mongo/migrations.go
@@ -560,29 +560,6 @@ var migrations = []migrate.Migration{
 			}
 
 			cursor.Close(context.TODO())
-
-			for cursor.Next(context.TODO()) {
-				record := new(models.RecordedSession)
-				err = cursor.Decode(&record)
-				if err != nil {
-					return err
-				}
-
-				namespace := new(models.Namespace)
-				if err := db.Collection("namespaces").FindOne(context.TODO(), bson.M{"tenant_id": record.TenantID}).Decode(&namespace); err != nil {
-					if err != mongo.ErrNoDocuments {
-						return err
-					}
-					if _, err := db.Collection("recorded_sessions").DeleteOne(context.TODO(), bson.M{"tenant_id": record.TenantID}); err != nil {
-						return err
-					}
-				}
-			}
-			if err := cursor.Err(); err != nil {
-				return err
-			}
-
-			cursor.Close(context.TODO())
 			return err
 		},
 		Down: func(db *mongo.Database) error {
@@ -661,6 +638,86 @@ var migrations = []migrate.Migration{
 			return nil
 		},
 
+		Down: func(db *mongo.Database) error {
+			return nil
+		},
+	},
+	{
+		Version: 21,
+		Up: func(db *mongo.Database) error {
+			cursor, err := db.Collection("sessions").Find(context.TODO(), bson.D{})
+			if err != nil {
+				return err
+			}
+
+			for cursor.Next(context.TODO()) {
+				session := new(models.Session)
+				err = cursor.Decode(&session)
+				if err != nil {
+					return err
+				}
+
+				device := new(models.Device)
+				if err := db.Collection("devices").FindOne(context.TODO(), bson.M{"uid": session.DeviceUID}).Decode(&device); err != nil {
+					if err != mongo.ErrNoDocuments {
+						return err
+					}
+
+					if _, err := db.Collection("sessions").DeleteMany(context.TODO(), bson.M{"device_uid": session.DeviceUID}); err != nil {
+						return err
+					}
+				}
+			}
+
+			if err := cursor.Err(); err != nil {
+				return err
+			}
+
+			cursor.Close(context.TODO())
+
+			cursor, err = db.Collection("recorded_sessions").Find(context.TODO(), bson.D{})
+			if err != nil {
+				return err
+			}
+
+			for cursor.Next(context.TODO()) {
+				record := new(models.RecordedSession)
+				err = cursor.Decode(&record)
+				if err != nil {
+					return err
+				}
+
+				namespace := new(models.Namespace)
+				if err := db.Collection("namespaces").FindOne(context.TODO(), bson.M{"tenant_id": record.TenantID}).Decode(&namespace); err != nil {
+					if err != mongo.ErrNoDocuments {
+						return err
+					}
+
+					if _, err := db.Collection("recorded_sessions").DeleteMany(context.TODO(), bson.M{"tenant_id": record.TenantID}); err != nil {
+						return err
+					}
+				}
+				session := new(models.Session)
+				if err := db.Collection("sessions").FindOne(context.TODO(), bson.M{"uid": record.UID}).Decode(&session); err != nil {
+					if err != mongo.ErrNoDocuments {
+						return err
+					}
+
+					if _, err := db.Collection("recorded_sessions").DeleteMany(context.TODO(), bson.M{"uid": record.UID}); err != nil {
+						return err
+					}
+				}
+
+			}
+			if err := cursor.Err(); err != nil {
+				return err
+			}
+
+			cursor.Close(context.TODO())
+
+			return nil
+
+		},
 		Down: func(db *mongo.Database) error {
 			return nil
 		},
