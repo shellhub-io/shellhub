a468e9c83a2e7fd67dac115d3e963792c3139383
vagnernornberg <vsaller@yahoo.com>
2021-05-13 13:38:09 -0300

    cli: modify store call in updateuser

--- cli/service_test.go
+++ cli/service_test.go
@@ -75,7 +75,7 @@ func TestResetUserPassword(t *testing.T) {
 
 	hash := sha256.Sum256([]byte("testService2"))
 
-	data.Mock.On("UserUpdate", context.TODO(), data.User.Username, data.User.Username, data.User.Email, data.User.Password, hex.EncodeToString(hash[:]), data.User.ID).Return(nil).Once()
+	data.Mock.On("UserPasswordUpdate", context.TODO(), hex.EncodeToString(hash[:]), data.User.ID).Return(nil).Once()
 
 	err := data.Service.UserUpdate(data.Parameters)
 	assert.NoError(t, err)
