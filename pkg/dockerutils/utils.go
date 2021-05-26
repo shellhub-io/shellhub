61348784f615d5f971d7393312a3af62f1234f6a
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2021-05-26 09:55:07 -0300

    pkg: fix regexp to match only containerIDs in correct path
    the space is needed to match only paths started in /var

--- pkg/dockerutils/utils.go
+++ pkg/dockerutils/utils.go
@@ -27,7 +27,7 @@ func CurrentContainerID() (string, error) {
 		return "", err
 	}
 
-	re := regexp.MustCompilePOSIX(`([0-9]+:[a-z_,=]+.*docker[/-]|/var/lib/docker/containers/)([0-9a-f]{64})`)
+	re := regexp.MustCompilePOSIX(`([0-9]+:[a-z_,=]+.*docker[/-]| /var/lib/docker/containers/)([0-9a-f]{64})`)
 	match := re.FindSubmatch(content)
 	if match == nil || len(match) != 3 {
 		return "", nil
