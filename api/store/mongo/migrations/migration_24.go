d8a8393e4a47480e4f203e8c1315f5191b48f3a8
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2021-05-28 10:39:36 -0300

    api: add migration to convert usernames and emails to lowercase

---
+++ api/store/mongo/migrations/migration_24.go
@@ -0,0 +1,38 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var migration_24 = migrate.Migration{
+	Version:     24,
+	Description: "convert names and emails to lowercase",
+	Up: func(db *mongo.Database) error {
+		logrus.Info("Applying migration 24 - Up")
+		if _, err := db.Collection("users").UpdateMany(context.TODO(), bson.D{}, []bson.M{
+			{
+				"$set": bson.M{"username": bson.M{"$toLower": "$username"},
+					"email": bson.M{"$toLower": "$email"}},
+			},
+		}); err != nil {
+			return err
+		}
+
+		_, err := db.Collection("namespaces").UpdateMany(context.TODO(), bson.D{}, []bson.M{
+			{
+				"$set": bson.M{"name": bson.M{"$toLower": "$name"}},
+			},
+		})
+
+		return err
+	},
+	Down: func(db *mongo.Database) error {
+		logrus.Info("Applying migration 24 - Down")
+		return nil
+	},
+}
