d644ec03bb51683bf5126a8520ca5e403c1ae477
vagnernornberg <vsaller@yahoo.com>
2021-06-02 18:01:09 -0300

    api: add shellhub envs package

--- api/store/mongo/migrations/migration_18.go
+++ api/store/mongo/migrations/migration_18.go
@@ -2,8 +2,8 @@ package migrations
 
 import (
 	"context"
-	"os"
 
+	"github.com/shellhub-io/shellhub/pkg/envs"
 	"github.com/sirupsen/logrus"
 	migrate "github.com/xakep666/mongo-migrate"
 	"go.mongodb.org/mongo-driver/bson"
@@ -14,7 +14,7 @@ var migration_18 = migrate.Migration{
 	Version: 18,
 	Up: func(db *mongo.Database) error {
 		logrus.Info("Applying migration 18 - Up")
-		if os.Getenv("SHELLHUB_ENTERPRISE") == "true" {
+		if envs.IsEnterprise() {
 			_, err := db.Collection("namespaces").UpdateMany(context.TODO(), bson.M{}, bson.M{"$set": bson.M{"max_devices": 3}})
 			return err
 		}
