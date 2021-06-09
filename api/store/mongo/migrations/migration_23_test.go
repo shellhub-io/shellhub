be97ea824c2bdea6618a403bef993088f9d1b015
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2021-06-09 20:06:51 -0300

    api: refactor migration 23 test to use assert package

--- api/store/mongo/migrations/migration_23_test.go
+++ api/store/mongo/migrations/migration_23_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/shellhub-io/shellhub/api/pkg/dbtest"
 	"github.com/shellhub-io/shellhub/pkg/models"
+	"github.com/stretchr/testify/assert"
 	migrate "github.com/xakep666/mongo-migrate"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,41 +20,28 @@ func TestMigration23(t *testing.T) {
 	migrations := GenerateMigrations()[:22]
 
 	migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
-	if err := migrates.Up(migrate.AllAvailable); err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
+	err := migrates.Up(migrate.AllAvailable)
+	assert.NoError(t, err)
 
-	version, description, err := migrates.Version()
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
-
-	if version != 22 || description != "" {
-		t.Errorf("Unexpected version/description %v %v", version, description)
-		return
-	}
+	version, _, err := migrates.Version()
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(22), version)
 
 	namespace := models.Namespace{
 		Name:     "namespace.test",
 		Owner:    "owner",
 		TenantID: "tenant",
 	}
-	if _, err := db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace); err != nil {
-		t.Errorf("failed to insert document")
-		return
-	}
+	_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace)
+	assert.NoError(t, err)
 
 	namespace = models.Namespace{
 		Name:     "namespacetest",
 		Owner:    "owner",
 		TenantID: "tenant2",
 	}
-	if _, err := db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace); err != nil {
-		t.Errorf("failed to insert document")
-		return
-	}
+	_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace)
+	assert.NoError(t, err)
 
 	device := models.Device{
 		Name:     "device.test",
@@ -62,10 +50,8 @@ func TestMigration23(t *testing.T) {
 		TenantID: "tenant",
 		LastSeen: time.Now(),
 	}
-	if _, err := db.Client().Database("test").Collection("devices").InsertOne(context.TODO(), device); err != nil {
-		t.Errorf("failed to insert document")
-		return
-	}
+	_, err = db.Client().Database("test").Collection("devices").InsertOne(context.TODO(), device)
+	assert.NoError(t, err)
 
 	device = models.Device{
 		Name:     "devicetest",
@@ -74,73 +60,38 @@ func TestMigration23(t *testing.T) {
 		TenantID: "tenant2",
 		LastSeen: time.Now(),
 	}
-	if _, err := db.Client().Database("test").Collection("devices").InsertOne(context.TODO(), device); err != nil {
-		t.Errorf("failed to insert document")
-		return
-	}
+	_, err = db.Client().Database("test").Collection("devices").InsertOne(context.TODO(), device)
+	assert.NoError(t, err)
 
 	migration := GenerateMigrations()[22]
 
 	migrates = migrate.NewMigrate(db.Client().Database("test"), migration)
-	if err := migrates.Up(migrate.AllAvailable); err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
+	err = migrates.Up(migrate.AllAvailable)
+	assert.NoError(t, err)
 
-	version, description, err = migrates.Version()
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
-
-	if version != 23 || description != "change dot in namespace name and hostname to -" {
-		t.Errorf("Unexpected version/description %v %v", version, description)
-		return
-	}
+	version, _, err = migrates.Version()
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(23), version)
 
 	var migratedNamespace *models.Namespace
-	if err := db.Client().Database("test").Collection("namespaces").FindOne(context.TODO(), bson.M{"tenant_id": "tenant"}).Decode(&migratedNamespace); err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
+	err = db.Client().Database("test").Collection("namespaces").FindOne(context.TODO(), bson.M{"tenant_id": "tenant"}).Decode(&migratedNamespace)
+	assert.NoError(t, err)
+	assert.Equal(t, "namespace-test", migratedNamespace.Name)
 
-	if migratedNamespace.Name != "namespace-test" {
-		t.Errorf("Unexpected data on namespace collection %v", migratedNamespace.Name)
-		return
-	}
-
-	if err := db.Client().Database("test").Collection("namespaces").FindOne(context.TODO(), bson.M{"tenant_id": "tenant2"}).Decode(&migratedNamespace); err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
-
-	if migratedNamespace.Name != "namespacetest" {
-		t.Errorf("Unexpected data on namespace collection %v", migratedNamespace.Name)
-		return
-	}
+	err = db.Client().Database("test").Collection("namespaces").FindOne(context.TODO(), bson.M{"tenant_id": "tenant2"}).Decode(&migratedNamespace)
+	assert.NoError(t, err)
+	assert.Equal(t, "namespacetest", migratedNamespace.Name)
 
 	var migratedDevice *models.Device
-	if err := db.Client().Database("test").Collection("devices").FindOne(context.TODO(), bson.M{"tenant_id": "tenant"}).Decode(&migratedDevice); err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
+	err = db.Client().Database("test").Collection("devices").FindOne(context.TODO(), bson.M{"tenant_id": "tenant"}).Decode(&migratedDevice)
+	assert.NoError(t, err)
+	assert.Equal(t, "device-test", migratedDevice.Name)
 
-	if migratedDevice.Name != "device-test" {
-		t.Errorf("Unexpected data on namespace collection %v", migratedDevice.Name)
-		return
-	}
+	err = db.Client().Database("test").Collection("devices").FindOne(context.TODO(), bson.M{"tenant_id": "tenant2"}).Decode(&migratedDevice)
+	assert.NoError(t, err)
+	assert.Equal(t, "devicetest", migratedDevice.Name)
 
-	if err := db.Client().Database("test").Collection("devices").FindOne(context.TODO(), bson.M{"tenant_id": "tenant2"}).Decode(&migratedDevice); err != nil {
-		t.Errorf("Unexpected error: %v", err)
-		return
-	}
+	err = db.Client().Database("test").Collection("namespaces").FindOne(context.TODO(), bson.M{"name": "name.test"}).Decode(&models.Namespace{})
+	assert.EqualError(t, mongo.ErrNoDocuments, err.Error())
 
-	if migratedDevice.Name != "devicetest" {
-		t.Errorf("Unexpected data on namespace collection %v", migratedDevice.Name)
-		return
-	}
-	if err := db.Client().Database("test").Collection("namespaces").FindOne(context.TODO(), bson.M{"name": "name.test"}).Decode(&models.Namespace{}); err != mongo.ErrNoDocuments {
-		t.Errorf("Unexpectedly found data from non-applied migration")
-		return
-	}
 }
