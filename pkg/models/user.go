046c30acdad85ac8a7f679fca702e93bb356ca52
LeonardoJoao <leonardorsjoao@gmail.com>
2021-06-11 13:46:26 -0300

    pkg: add user token recover model

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -2,15 +2,17 @@ package models
 
 import (
 	jwt "github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 type User struct {
-	ID         string `json:"id,omitempty" bson:"_id,omitempty"`
-	Name       string `json:"name" validate:"required,min=1"`
-	Email      string `json:"email" bson:",omitempty" validate:"required,email"`
-	Username   string `json:"username" bson:",omitempty" validate:"required,min=3,max=30,alphanum,ascii"`
-	Password   string `json:"password" bson:",omitempty"`
-	Namespaces int    `json:"namespaces" bson:"namespaces,omitempty"`
+	ID            string `json:"id,omitempty" bson:"_id,omitempty"`
+	Name          string `json:"name" validate:"required,min=1"`
+	Email         string `json:"email" bson:",omitempty" validate:"required,email"`
+	Username      string `json:"username" bson:",omitempty" validate:"required,min=3,max=30,alphanum,ascii"`
+	Password      string `json:"password" bson:",omitempty"`
+	Namespaces    int    `json:"namespaces" bson:"namespaces,omitempty"`
+	Authenticated bool   `json:"Authenticated"`
 }
 
 type UserAuthRequest struct {
@@ -35,4 +37,10 @@ type UserAuthClaims struct {
 
 	AuthClaims         `mapstruct:",squash"`
 	jwt.StandardClaims `mapstruct:",squash"`
+}
+
+type UserTokenRecover struct {
+	Token     string    `json:"uid"`
+	User      string    `json:"user_id"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
