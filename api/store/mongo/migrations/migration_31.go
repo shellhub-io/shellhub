67d0f136ef72f2d769442f7473afedee4a2aa570
leonardojoao <leonardorsjoao@gmail.com>
2021-08-13 07:07:39 -0300

    api: fix message name on migration

--- api/store/mongo/migrations/migration_31.go
+++ api/store/mongo/migrations/migration_31.go
@@ -12,7 +12,7 @@ import (
 
 var migration31 = migrate.Migration{
 	Version:     31,
-	Description: "add last_login field to collection users",
+	Description: "add last_login field to collection namespaces",
 	Up: func(db *mongo.Database) error {
 		logrus.Info("Applying migration 31 - Up")
 		if _, err := db.Collection("namespaces").UpdateMany(context.TODO(), bson.M{}, bson.M{"$set": bson.M{"created_at": clock.Now()}}); err != nil {
