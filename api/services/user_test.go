bc2de7afa4a3348231dca0ebfad09f2388f7fd66
asakiz <asakizin@gmail.com>
2021-08-17 14:21:18 -0300

    api: update the tests on user for table driven model
    Signed-off-by: asakiz <asakizin@gmail.com>

--- api/services/user_test.go
+++ api/services/user_test.go
@@ -19,76 +19,172 @@ func TestUpdateDataUser(t *testing.T) {
 
 	ctx := context.TODO()
 
+	Err := errors.New("conflict")
+
 	user1 := &models.User{Name: "name", Email: "user1@email.com", Username: "username1", Password: "hash1", ID: "id1"}
 
 	user2 := &models.User{Name: "name", Email: "user2@email.com", Username: "username2", Password: "hash2", ID: "id2"}
 
 	updateUser1 := &models.User{Name: "name", Email: "user1@email2.com", Username: user2.Username, ID: "id1"}
 
-	Err := errors.New("conflict")
+	updateInvalidUsername := &models.User{Name: "newname", Email: "user1@email2.com", Username: "invalid_name", ID: "id1"}
 
-	// verifies existent username only
-	mock.On("UserGetByID", ctx, updateUser1.ID, false).Return(user1, 0, nil).Once()
-	mock.On("UserGetByUsername", ctx, updateUser1.Username).Return(user2, nil).Once()
-	mock.On("UserGetByEmail", ctx, updateUser1.Email).Return(nil, Err).Once()
-	fields, err := s.UpdateDataUser(ctx, updateUser1, updateUser1.ID)
+	updateInvalidEmail := &models.User{Name: "newname", Email: "invalid.email", Username: "newusername", ID: "id1"}
 
-	assert.Equal(t, err, ErrConflict)
-	assert.Equal(t, fields, []validator.InvalidField{{"username", "conflict", "", ""}})
+	updateInvalidUsernameEmail := &models.User{Name: "newname", Email: "invalid.email", Username: "us", ID: "id1"}
 
-	updateUser1 = &models.User{Name: "name", Email: "user2@email.com", Username: user2.Username, ID: "id1"}
+	updateEmptyUsername := &models.User{Name: "", Email: "new@email.com", Username: "newusername", ID: "id1"}
 
-	// verifies existent email and username only
-	mock.On("UserGetByID", ctx, updateUser1.ID, false).Return(user1, 0, nil).Once()
-	mock.On("UserGetByUsername", ctx, updateUser1.Username).Return(user2, nil).Once()
-	mock.On("UserGetByEmail", ctx, updateUser1.Email).Return(user2, nil).Once()
-	fields, err = s.UpdateDataUser(ctx, updateUser1, updateUser1.ID)
+	updateEmptyEmail := &models.User{Name: "newname", Email: "", Username: "newusername", ID: "id1"}
 
-	assert.Equal(t, err, ErrConflict)
-	assert.Equal(t, fields, []validator.InvalidField{{"username", "conflict", "", ""}, {"email", "conflict", "", ""}})
+	conflictedUsername := []validator.InvalidField{{"username", "conflict", "", ""}}
 
-	// shows invalid errors
+	conflictedUserEmail := []validator.InvalidField{{"username", "conflict", "", ""}, {"email", "conflict", "", ""}}
 
-	// for username
-	updateUser1 = &models.User{Name: "newname", Email: "user1@email2.com", Username: "invalid_name", ID: "id1"}
+	invalidUsername := []validator.InvalidField{{"username", "invalid", "alphanum", ""}}
 
-	mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
-	fields, err = s.UpdateDataUser(ctx, updateUser1, updateUser1.ID)
-	assert.Equal(t, err, ErrBadRequest)
-	assert.Equal(t, fields, []validator.InvalidField{{"username", "invalid", "alphanum", ""}})
+	invalidEmail := []validator.InvalidField{{"email", "invalid", "email", ""}}
 
-	// for email
-	updateUser1 = &models.User{Name: "newname", Email: "invalid.email", Username: "newusername", ID: "id1"}
-	mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
-	fields, err = s.UpdateDataUser(ctx, updateUser1, updateUser1.ID)
-	assert.Equal(t, err, ErrBadRequest)
-	assert.Equal(t, fields, []validator.InvalidField{{"email", "invalid", "email", ""}})
+	invalidUsernameEmail := []validator.InvalidField{{"email", "invalid", "email", ""}, {"username", "invalid", "min", "3"}}
 
-	// both email and username
-	updateUser1 = &models.User{Name: "newname", Email: "invalid.email", Username: "us", ID: "id1"}
-	mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
-	fields, err = s.UpdateDataUser(ctx, updateUser1, updateUser1.ID)
-	assert.Equal(t, err, ErrBadRequest)
-	assert.Equal(t, fields, []validator.InvalidField{{"email", "invalid", "email", ""}, {"username", "invalid", "min", "3"}})
+	emptyUsername := []validator.InvalidField{{"name", "invalid", "required", ""}}
 
-	// for empty name
-	updateUser1 = &models.User{Name: "", Email: "new@email.com", Username: "newusername", ID: "id1"}
-	mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
-	_, err = s.UpdateDataUser(ctx, updateUser1, updateUser1.ID)
+	emptyEmail := []validator.InvalidField{{"email", "invalid", "required", ""}}
 
-	assert.Equal(t, err, ErrBadRequest)
+	type Expected struct {
+		fields []validator.InvalidField
+		err    error
+	}
 
-	// successful update
-	updateUser1 = &models.User{Name: "newname", Email: "user1@email2.com", Username: "newusername", ID: "id1"}
+	tests := []struct {
+		description                string
+		user1                      *models.User
+		user2                      *models.User
+		updateUser                 *models.User
+		updateInvalidUsername      *models.User
+		updateInvalidEmail         *models.User
+		updateInvalidUsernameEmail *models.User
+		updateEmptyUsername        *models.User
+		updateEmptyEmail           *models.User
+		requiredMocks              func()
+		expected                   Expected
+	}{
+		{
+			description: "Fails to find the user by the ID",
+			user1:       user1,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, user1.ID, false).Return(nil, 0, Err).Once()
+			},
+			expected: Expected{nil, Err},
+		},
+		{
+			description: "Fails conflict username",
+			user1:       user1,
+			user2:       user2,
+			updateUser:  updateUser1,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, updateUser1.ID, false).Return(user1, 0, nil).Once()
+				mock.On("UserGetByUsername", ctx, updateUser1.Username).Return(user2, nil).Once()
+				mock.On("UserGetByEmail", ctx, updateUser1.Email).Return(nil, Err).Once()
+			},
+			expected: Expected{conflictedUsername, Err},
+		},
+		{
+			description: "Fails conflict email and username",
+			user1:       user1,
+			user2:       user2,
+			updateUser:  updateUser1,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, updateUser1.ID, false).Return(user1, 0, nil).Once()
+				mock.On("UserGetByUsername", ctx, updateUser1.Username).Return(user2, nil).Once()
+				mock.On("UserGetByEmail", ctx, updateUser1.Email).Return(user2, nil).Once()
+			},
+			expected: Expected{conflictedUserEmail, Err},
+		},
+		{
+			description:           "Fails invalid username",
+			user1:                 user1,
+			updateInvalidUsername: updateInvalidUsername,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
+			},
+			expected: Expected{invalidUsername, ErrBadRequest},
+		},
+		{
+			description:        "Fails invalid email",
+			user1:              user1,
+			updateInvalidEmail: updateInvalidEmail,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
+			},
+			expected: Expected{invalidEmail, ErrBadRequest},
+		},
+		{
+			description:                "Fails invalid email and username",
+			user1:                      user1,
+			updateInvalidUsernameEmail: updateInvalidUsernameEmail,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
+			},
+			expected: Expected{invalidUsernameEmail, ErrBadRequest},
+		},
+		{
+			description:         "Fails empty username",
+			user1:               user1,
+			updateEmptyUsername: updateEmptyUsername,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
+			},
+			expected: Expected{emptyUsername, ErrBadRequest},
+		},
+		{
+			description:      "Fails empty email",
+			user1:            user1,
+			updateEmptyEmail: updateEmptyEmail,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
+			},
+			expected: Expected{emptyEmail, ErrBadRequest},
+		},
+		{
+			description: "Successful update user data",
+			user1:       user1,
+			updateUser:  updateUser1,
+			requiredMocks: func() {
+				mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
+				mock.On("UserGetByUsername", ctx, updateUser1.Username).Return(nil, Err).Once()
+				mock.On("UserGetByEmail", ctx, updateUser1.Email).Return(nil, Err).Once()
+				mock.On("UserUpdateData", ctx, updateUser1, updateUser1.ID).Return(nil).Once()
+			},
+			expected: Expected{nil, nil},
+		},
+	}
 
-	Err = errors.New("no documents")
+	returnedFields := new([]validator.InvalidField)
 
-	mock.On("UserGetByID", ctx, user1.ID, false).Return(user1, 0, nil).Once()
-	mock.On("UserGetByUsername", ctx, updateUser1.Username).Return(nil, Err).Once()
-	mock.On("UserGetByEmail", ctx, updateUser1.Email).Return(nil, Err).Once()
-	mock.On("UserUpdateData", ctx, updateUser1, updateUser1.ID).Return(nil).Once()
-	_, err = s.UpdateDataUser(ctx, updateUser1, updateUser1.ID)
-	assert.Nil(t, err)
+	for _, test := range tests {
+		t.Log("PASS:  ", test.description)
+		test.requiredMocks()
+		switch test.description {
+		case "Fails to find the user by the ID":
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.user1, test.user1.ID)
+		case "Fails invalid username":
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.updateInvalidUsername, test.updateInvalidUsername.ID)
+		case "Fails invalid email":
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.updateInvalidEmail, test.updateInvalidEmail.ID)
+		case "Fails invalid email and username":
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.updateInvalidUsernameEmail, test.updateInvalidUsernameEmail.ID)
+		case "Fails empty username":
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.updateEmptyUsername, test.updateEmptyUsername.ID)
+		case "Fails empty email":
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.updateEmptyEmail, test.updateEmptyEmail.ID)
+		case "Successful update user data":
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.updateUser, test.updateUser.ID)
+		default:
+			*returnedFields, Err = s.UpdateDataUser(ctx, test.updateUser, test.updateUser.ID)
+		}
+		assert.Equal(t, test.expected, Expected{*returnedFields, Err})
+	}
 
 	mock.AssertExpectations(t)
 }
