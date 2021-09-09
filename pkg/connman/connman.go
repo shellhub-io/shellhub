shellhub-io/shellhub

commit 57b4887f8993a9e5dad3d344e4de897733113dff

ssh, api: detect when device goes offline

pkg/connman/connman.go
@@ -9,18 +9,23 @@ import (
 	"github.com/shellhub-io/shellhub/pkg/revdial"
 )
 
-var (
-	ErrNoConnection = errors.New("no connection")
-)
+var ErrNoConnection = errors.New("no connection")
 
 type ConnectionManager struct {
 	dialers map[string]*revdial.Dialer
 	lock    sync.RWMutex
+	status  chan TunnelStatus
+}
+
+type TunnelStatus struct {
+	id     string
+	online bool
 }
 
 func New() *ConnectionManager {
 	return &ConnectionManager{
 		dialers: make(map[string]*revdial.Dialer),
+		status:  make(chan TunnelStatus),
 	}
 }
 
@@ -28,6 +33,11 @@ func (m *ConnectionManager) Set(key string, conn net.Conn) {
 	m.lock.Lock()
 	m.dialers[key] = revdial.NewDialer(conn, "/ssh/revdial")
 	m.lock.Unlock()
+
+	go func() {
+		stats := TunnelStatus{id: key, online: m.dialers[key].IsOnline()}
+		m.status <- stats
+	}()
 }
 
 func (m *ConnectionManager) Dial(ctx context.Context, key string) (net.Conn, error) {
@@ -40,4 +50,11 @@ func (m *ConnectionManager) Dial(ctx context.Context, key string) (net.Conn, err
 	m.lock.RUnlock()
 
 	return dialer.Dial(ctx)
+}
+
+func (m *ConnectionManager) Online() (id string, online bool) {
+	status := <-m.status
+	id = status.id
+	online = status.online
+	return
 }

pkg/httptunnel/httptunnel.go
@@ -35,6 +35,8 @@ type Tunnel struct {
 	DialerPath        string
 	ConnectionHandler func(*http.Request) (string, error)
 	connman           *connman.ConnectionManager
+	id                chan string
+	online            chan bool
 }
 
 func NewTunnel(connectionPath, dialerPath string) *Tunnel {
@@ -45,6 +47,8 @@ func NewTunnel(connectionPath, dialerPath string) *Tunnel {
 			panic("ConnectionHandler not implemented")
 		},
 		connman: connman.New(),
+		id:      make(chan string),
+		online:  make(chan bool),
 	}
 }
 
@@ -105,4 +109,9 @@ func (t *Tunnel) ForwardResponse(resp *http.Response, w http.ResponseWriter) {
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
 	resp.Body.Close()
+}
+
+func (t *Tunnel) Online() (id string, online bool) {
+	id, online = t.connman.Online()
+	return
 }

pkg/revdial/revdial.go
@@ -51,6 +51,7 @@ type Dialer struct {
 	connReady    chan bool
 	donec        chan struct{}
 	closeOnce    sync.Once
+	online       chan bool
 }
 
 var (
@@ -73,6 +74,7 @@ func NewDialer(c net.Conn, connPath string) *Dialer {
 		connReady:    make(chan bool),
 		incomingConn: make(chan net.Conn),
 		pickupFailed: make(chan error),
+		online:       make(chan bool),
 	}
 
 	join := "?"
@@ -110,6 +112,7 @@ func (d *Dialer) Done() <-chan struct{} { return d.donec }
 
 // Close closes the Dialer.
 func (d *Dialer) Close() error {
+	d.online <- false
 	d.closeOnce.Do(d.close)
 	return nil
 }
@@ -404,4 +407,8 @@ func ConnHandler(upgrader websocket.Upgrader) http.Handler {
 
 		d.matchConn(wsconnadapter.New(wsConn))
 	})
+}
+
+func (d *Dialer) IsOnline() bool {
+	return <-d.online
 }

ssh/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/parnurzeal/gorequest"
 	"github.com/shellhub-io/shellhub/pkg/httptunnel"
 )
 
@@ -50,11 +51,19 @@ func main() {
 			http.Error(res, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
 	})
 	go http.ListenAndServe(":8080", router)
 
 	server := NewServer(opts, tunnel)
+
+	go func() {
+		for {
+			id, online := tunnel.Online()
+			if !online {
+				_, _, _ = gorequest.New().Post(fmt.Sprintf("http://api:8080/internal/devices/%s/offline", id)).End()
+			}
+		}
+	}()
 
 	logrus.Fatal(server.ListenAndServe())
 }

api/main.go
@@ -31,7 +31,6 @@ func main() {
 	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
 	// Connect to MongoDB
 	client, err := mgo.Connect(context.TODO(), clientOptions)
-
 	if err != nil {
 		panic(err)
 	}
@@ -84,6 +83,19 @@ func main() {
 
 	publicAPI := e.Group("/api")
 	internalAPI := e.Group("/internal")
+	internalAPI.POST("/devices/:uid/offline", func(c echo.Context) error {
+		ctx := c.Get("ctx").(context.Context)
+		store := mongo.NewStore(ctx.Value("db").(*mgo.Database))
+		svc := deviceadm.NewService(store)
+
+		err := svc.UpdateDeviceStatus(ctx, models.UID(c.Param("uid")), false)
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, nil)
+	})
+
 	publicAPI.POST("/devices/auth", func(c echo.Context) error {
 		var req models.DeviceAuthRequest
 

commit c18cc07af15f57e2c26ab7f7a58bb2a0041f35cb

ssh: use existing channel to detect connection failures
This also fixes segmentation violation introduced in `57b4887f`.

pkg/connman/connman.go
@@ -12,31 +12,28 @@ import (
 var ErrNoConnection = errors.New("no connection")
 
 type ConnectionManager struct {
-	dialers map[string]*revdial.Dialer
-	lock    sync.RWMutex
-	status  chan TunnelStatus
-}
-
-type TunnelStatus struct {
-	id     string
-	online bool
+	dialers            map[string]*revdial.Dialer
+	lock               sync.RWMutex
+	DialerDoneCallback func(string, *revdial.Dialer)
 }
 
 func New() *ConnectionManager {
 	return &ConnectionManager{
 		dialers: make(map[string]*revdial.Dialer),
-		status:  make(chan TunnelStatus),
+		DialerDoneCallback: func(string, *revdial.Dialer) {
+		},
 	}
 }
 
 func (m *ConnectionManager) Set(key string, conn net.Conn) {
 	m.lock.Lock()
-	m.dialers[key] = revdial.NewDialer(conn, "/ssh/revdial")
+	dialer := revdial.NewDialer(conn, "/ssh/revdial")
+	m.dialers[key] = dialer
 	m.lock.Unlock()
 
 	go func() {
-		stats := TunnelStatus{id: key, online: m.dialers[key].IsOnline()}
-		m.status <- stats
+		<-dialer.Done()
+		m.DialerDoneCallback(key, dialer)
 	}()
 }
 
@@ -51,12 +48,4 @@ func (m *ConnectionManager) Dial(ctx context.Context, key string) (net.Conn, err
 	m.lock.RUnlock()
 
 	return dialer.Dial(ctx)
-}
-
-func (m *ConnectionManager) Online() (id string, online bool) {
-	status := <-m.status
-	id = status.id
-	online = status.online
-
-	return
 }

pkg/httptunnel/httptunnel.go
@@ -32,22 +32,31 @@ type Tunnel struct {
 	ConnectionPath    string
 	DialerPath        string
 	ConnectionHandler func(*http.Request) (string, error)
+	CloseHandler      func(string)
 	connman           *connman.ConnectionManager
 	id                chan string
 	online            chan bool
 }
 
 func NewTunnel(connectionPath, dialerPath string) *Tunnel {
-	return &Tunnel{
+	tunnel := &Tunnel{
 		ConnectionPath: connectionPath,
 		DialerPath:     dialerPath,
 		ConnectionHandler: func(r *http.Request) (string, error) {
 			panic("ConnectionHandler not implemented")
 		},
+		CloseHandler: func(string) {
+		},
 		connman: connman.New(),
 		id:      make(chan string),
 		online:  make(chan bool),
 	}
+
+	tunnel.connman.DialerDoneCallback = func(id string, _ *revdial.Dialer) {
+		tunnel.CloseHandler(id)
+	}
+
+	return tunnel
 }
 
 func (t *Tunnel) Router() http.Handler {
@@ -109,10 +118,4 @@ func (t *Tunnel) ForwardResponse(resp *http.Response, w http.ResponseWriter) {
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body) // nolint:errcheck
 	resp.Body.Close()
-}
-
-func (t *Tunnel) Online() (id string, online bool) {
-	id, online = t.connman.Online()
-
-	return
 }

pkg/revdial/revdial.go
@@ -54,7 +54,6 @@ type Dialer struct {
 	connReady    chan bool
 	donec        chan struct{}
 	closeOnce    sync.Once
-	online       chan bool
 }
 
 var (
@@ -77,7 +76,6 @@ func NewDialer(c net.Conn, connPath string) *Dialer {
 		connReady:    make(chan bool),
 		incomingConn: make(chan net.Conn),
 		pickupFailed: make(chan error),
-		online:       make(chan bool),
 	}
 
 	join := "?"
@@ -117,7 +115,6 @@ func (d *Dialer) Done() <-chan struct{} { return d.donec }
 
 // Close closes the Dialer.
 func (d *Dialer) Close() error {
-	d.online <- false
 	d.closeOnce.Do(d.close)
 
 	return nil
@@ -432,8 +429,4 @@ func ConnHandler(upgrader websocket.Upgrader) http.Handler {
 
 		d.matchConn(wsconnadapter.New(wsConn))
 	})
-}
-
-func (d *Dialer) IsOnline() bool {
-	return <-d.online
 }

ssh/main.go
@@ -30,6 +30,11 @@ func main() {
 	tunnel.ConnectionHandler = func(r *http.Request) (string, error) {
 		return r.Header.Get(client.DeviceUIDHeader), nil
 	}
+	tunnel.CloseHandler = func(id string) {
+		if err := client.NewClient().DevicesOffline(id); err != nil {
+			logrus.Error(err)
+		}
+	}
 
 	router := tunnel.Router().(*mux.Router)
 	router.HandleFunc("/sessions/{uid}/close", func(res http.ResponseWriter, req *http.Request) {
@@ -62,16 +67,6 @@ func main() {
 	router.Handle("/ws/ssh", websocket.Handler(HandlerWebsocket))
 
 	go http.ListenAndServe(":8080", router) // nolint:errcheck
-
-	go func() {
-		for {
-			if id, online := tunnel.Online(); !online {
-				if err := client.NewClient().DevicesOffline(id); err != nil {
-					logrus.Info(err)
-				}
-			}
-		}
-	}()
 
 	var err error
 	magicKey, err = rsa.GenerateKey(rand.Reader, 2048)