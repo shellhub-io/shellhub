239192f15e46c7e330abb304ab78a772fb3af4bb
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2021-09-13 17:12:46 -0300

    ssh: wait remote command to exit
    This fixes unexpected behaviour when getting command output
    from `CombinedOutput() (x/crypto/ssh.*Session.CombinedOutput) which
    returns an empty buffer.

--- ssh/session.go
+++ ssh/session.go
@@ -304,6 +304,13 @@ func (s *Session) connect(passwd string, key *rsa.PrivateKey, session sshserver.
 			return nil
 		}
 
+		if err := client.Wait(); err != nil {
+			logrus.WithFields(logrus.Fields{
+				"session": s.UID,
+				"err":     err,
+			}).Error("Failed to wait remote command to exit")
+		}
+
 		<-done
 	}
 
