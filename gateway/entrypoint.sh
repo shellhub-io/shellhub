c9d4b1c7440788bd8d11a088dccfe9d182caf504
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2021-09-28 15:15:31 -0300

    gateway: sets the default value of MAX_WORKER_CONNECTIONS
    Sets the default value (16384) of max number of simultaneous
    connections that can be opened by each worker process.
    
    NOTE: Using 0 in scenarios of high load improves performance at the
    cost of increasing RAM utilization (even on idle).

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -3,20 +3,23 @@
 # Same tunning settings applied by NGINX Ingress Controller
 # https://github.com/kubernetes/ingress-nginx/blob/844a02c276788e293480c080fe09f4d242545c82/internal/ingress/controller/nginx.go#L512
 
-if [ "$WORKER_PROCESSES" == "" ]; then
+WORKER_PROCESSES="${WORKER_PROCESSES:-auto}"
+MAX_WORKER_OPEN_FILES="${MAX_WORKER_OPEN_FILES:-0}"
+MAX_WORKER_CONNECTIONS="${MAX_WORKER_CONNECTIONS:-16384}"
+
+if [ "$WORKER_PROCESSES" == "auto" ]; then
     WORKER_PROCESSES=$(nproc)
 fi
 
-if [ "$MAX_WORKER_OPEN_FILES" == "" ]; then
-    MAX_OPEN_FILES=$(ulimit -n)
-    MAX_WORKER_OPEN_FILES=$((($MAX_OPEN_FILES / $WORKER_PROCESSES) - 1024))
+if [ "$MAX_WORKER_OPEN_FILES" == "0" ]; then
+    MAX_WORKER_OPEN_FILES=$(($(ulimit -Sn) - 1024))
 
     if [ "$MAX_WORKER_OPEN_FILES" -lt "1024" ]; then
 	MAX_WORKER_OPEN_FILES=1024
     fi
 fi
 
-if [ "$MAX_WORKER_CONNECTIONS" == "" ]; then
+if [ "$MAX_WORKER_CONNECTIONS" == "0" ]; then
     MAX_WORKER_CONNECTIONS=$(($MAX_WORKER_OPEN_FILES * 3 / 4))
 fi
 
