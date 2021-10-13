edcbdc62799374bdbb4eaf6cbdf90a7642c49131
Henry Barreto <henry.barreto@ossystems.com.br>
2021-10-13 11:25:43 -0300

    pkg: change validation return

---
+++ pkg/validator/format.go
@@ -0,0 +1,24 @@
+package validator
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strings"
+
+	"github.com/shellhub-io/shellhub/pkg/models"
+)
+
+func HashPassword(password string) string {
+	s := sha256.Sum256([]byte(password))
+
+	return hex.EncodeToString(s[:])
+}
+
+// FormatUser apply some formation rules to a models.User and encrypt the password.
+func FormatUser(user *models.User) {
+	user.Username = strings.ToLower(user.Username)
+	user.Email = strings.ToLower(user.Email)
+	if user.Password != "" {
+		user.Password = HashPassword(user.Password)
+	}
+}
