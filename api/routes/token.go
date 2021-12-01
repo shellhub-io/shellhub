29e15bc150e8ebd27c954b1771502cc0f1d74389
Henry Barreto <contato@henrybarreto.dev>
2021-12-01 17:17:38 -0300

    api: add check to tenant ID and service's output evaluate
    This check will avoid a panic caused by a try to access a nil
    pointer.
    I also add a switch case statement to evaluate service's output.

--- api/routes/token.go
+++ api/routes/token.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/shellhub-io/shellhub/api/apicontext"
-	"github.com/shellhub-io/shellhub/api/apierr"
+	"github.com/shellhub-io/shellhub/api/services"
 	"github.com/shellhub-io/shellhub/pkg/models"
 )
 
@@ -17,49 +17,113 @@ const (
 )
 
 func (h *Handler) ListToken(c apicontext.Context) error {
-	tokens, err := h.service.ListToken(c.Ctx(), c.Tenant().ID)
+	var tenantID string
+	if c.Tenant() != nil {
+		tenantID = c.Tenant().ID
+	}
+
+	tokens, err := h.service.ListToken(c.Ctx(), tenantID)
 	if err != nil {
-		return apierr.HandleError(c, err)
+		switch err {
+		case services.ErrNamespaceNotFound:
+			return c.NoContent(http.StatusNotFound)
+		default:
+			return err
+		}
 	}
 
 	return c.JSON(http.StatusOK, tokens)
 }
 
 func (h *Handler) CreateToken(c apicontext.Context) error {
-	if _, err := h.service.CreateToken(c.Ctx(), c.Tenant().ID); err != nil {
-		return apierr.HandleError(c, err)
+	var tenantID string
+	if c.Tenant() != nil {
+		tenantID = c.Tenant().ID
+	}
+
+	_, err := h.service.CreateToken(c.Ctx(), tenantID)
+	if err != nil {
+		switch err {
+		case services.ErrNamespaceNotFound:
+			return c.NoContent(http.StatusNotFound)
+		default:
+			return err
+		}
 	}
 
 	token, err := h.service.AuthAPIToken(c.Ctx(), &models.APITokenAuthRequest{
-		TenantID: c.Tenant().ID,
+		TenantID: tenantID,
 	})
 	if err != nil {
-		return err
+		switch err {
+		case services.ErrNamespaceNotFound:
+			return c.NoContent(http.StatusNotFound)
+		case services.ErrHashGeneration:
+			return c.NoContent(http.StatusInternalServerError)
+		case services.ErrHashWrite:
+			return c.NoContent(http.StatusInternalServerError)
+		case services.ErrAPITokenSign:
+			return c.NoContent(http.StatusInternalServerError)
+		default:
+			return err
+		}
 	}
 
 	return c.JSON(http.StatusOK, token)
 }
 
 func (h *Handler) GetToken(c apicontext.Context) error {
-	token, err := h.service.GetToken(c.Ctx(), c.Tenant().ID, c.Param("id"))
+	var tenantID string
+	if c.Tenant() != nil {
+		tenantID = c.Tenant().ID
+	}
+
+	token, err := h.service.GetToken(c.Ctx(), tenantID, c.Param("id"))
 	if err != nil {
-		return apierr.HandleError(c, err)
+		switch err {
+		case services.ErrNotFound:
+			return c.NoContent(http.StatusNotFound)
+		default:
+			return err
+		}
 	}
 
 	return c.JSON(http.StatusOK, token)
 }
 
 func (h *Handler) DeleteToken(c apicontext.Context) error {
-	if err := h.service.DeleteToken(c.Ctx(), c.Tenant().ID, c.Param("id")); err != nil {
-		return apierr.HandleError(c, err)
+	var tenantID string
+	if c.Tenant() != nil {
+		tenantID = c.Tenant().ID
+	}
+
+	err := h.service.DeleteToken(c.Ctx(), tenantID, c.Param("id"))
+	if err != nil {
+		switch err {
+		case services.ErrNotFound:
+			return c.NoContent(http.StatusNotFound)
+		default:
+			return err
+		}
 	}
 
 	return c.NoContent(http.StatusOK)
 }
 
 func (h *Handler) UpdateToken(c apicontext.Context) error {
-	if err := h.service.UpdateToken(c.Ctx(), c.Tenant().ID, c.Param("id"), &models.APITokenUpdate{}); err != nil {
-		return apierr.HandleError(c, err)
+	var tenantID string
+	if c.Tenant() != nil {
+		tenantID = c.Tenant().ID
+	}
+
+	err := h.service.UpdateToken(c.Ctx(), tenantID, c.Param("id"), &models.APITokenUpdate{})
+	if err != nil {
+		switch err {
+		case services.ErrNotFound:
+			return c.NoContent(http.StatusNotFound)
+		default:
+			return err
+		}
 	}
 
 	return c.NoContent(http.StatusOK)
