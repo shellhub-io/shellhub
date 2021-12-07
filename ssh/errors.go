e017c7d53695767b355b36baa7fde8e1dd1ccb50
Henry Barreto <contato@henrybarreto.dev>
2021-12-07 16:16:29 -0300

    ssh: improve SSH error code

--- ssh/errors.go
+++ ssh/errors.go
@@ -1,39 +1,22 @@
 package main
 
-import (
-	"errors"
-)
+import "errors"
 
 var (
 	ErrInvalidSessionTarget = errors.New("invalid session target")
-	ErrBillingBlock         = errors.New("reached the device limit, update to premium or choose up to 3 devices")
+	ErrBillingBlock         = errors.New("reached the device limit")
 	ErrFirewallBlock        = errors.New("a firewall rule block this action")
 	ErrFindDevice           = errors.New("cloud not find the device")
 	ErrLookupDevice         = errors.New("could not lookup for device data")
 )
 
-// getExternalErrors returns a map with internal error as key and external error as value.
-// watch out: on each new error, a new external must be created and associate on the returning map.
-func getExternalErrors() map[error]error {
+// getExternalErrors converts an internal error for external one.
+func getExternalError(err error) error {
 	// External errors are intended to be returned for the end user.
-	var (
-		ErrInvalidSessionTargetExternal = errors.New("internal error")
-		ErrBillingBlockExternal         = errors.New("billing block")
-		ErrFirewallBlockExternal        = errors.New("firewall block")
-		ErrFindDeviceExternal           = errors.New("invalid device")
-		ErrLookupDeviceExternal         = errors.New("device data error")
-	)
-
-	return map[error]error{
-		ErrInvalidSessionTarget: ErrInvalidSessionTargetExternal,
-		ErrBillingBlock:         ErrBillingBlockExternal,
-		ErrFirewallBlock:        ErrFirewallBlockExternal,
-		ErrFindDevice:           ErrFindDeviceExternal,
-		ErrLookupDevice:         ErrLookupDeviceExternal,
+	switch err {
+	case ErrBillingBlock:
+		return errors.New("reached the device limit, update to premium or choose up to 3 devices")
+	default:
+		return err
 	}
-}
-
-// ErrorExternal returns a external error from a internal one.
-func ErrorExternal(err error) error {
-	return getExternalErrors()[err]
 }
