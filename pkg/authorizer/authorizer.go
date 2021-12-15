e824718714ed0b04d94ed54281e0123354bc2797
Henry Barreto <henry.barreto@ossystems.com.br>
2021-12-15 16:58:09 -0300

    pkg: remove function to get all roles in favor of a map
    I've removed the function for get all roles and that generated role's
    code through a loop in favor of a hard coded map, thus, when a new
    role is added, less code would need to be changed.

--- pkg/authorizer/authorizer.go
+++ pkg/authorizer/authorizer.go
@@ -8,17 +8,23 @@ const (
 	MemberRoleOwner         = "owner"
 )
 
-// GetAllMemberRoles return a list with all member roles.
-// What out, when you add a new role, you need to add it to this return list.
-func GetAllMemberRoles() []string {
-	// The position at this slice is important to define the role priority.
-	return []string{
-		MemberRoleToken,
-		MemberRoleObserver,
-		MemberRoleOperator,
-		MemberRoleAdministrator,
-		MemberRoleOwner,
-	}
+// RolePermission is mapping role to its permissions.
+var RolePermission = map[string]permissions{
+	MemberRoleToken:         tokenPermission,
+	MemberRoleObserver:      observerPermissions,
+	MemberRoleOperator:      operatorPermissions,
+	MemberRoleAdministrator: adminPermissions,
+	MemberRoleOwner:         ownerPermissions,
+}
+
+// RoleCode is mapping role to is code.
+// 1 has the lowest priority.
+var RoleCode = map[string]int{
+	MemberRoleToken:         1,
+	MemberRoleObserver:      2,
+	MemberRoleOperator:      3,
+	MemberRoleAdministrator: 4,
+	MemberRoleOwner:         5,
 }
 
 func checkPermission(action int, permissions permissions) bool {
@@ -33,11 +39,9 @@ func checkPermission(action int, permissions permissions) bool {
 
 // GetRoleCode converts a member's role to an int.
 func GetRoleCode(role string) int {
-	roles := GetAllMemberRoles()
-	for code, roleSearch := range roles {
-		if role == roleSearch {
-			return code
-		}
+	code := RoleCode[role]
+	if code != 0 {
+		return code
 	}
 
 	// return -1 when member role is not valid.
@@ -58,17 +62,9 @@ func EvaluateRole(firstRole, secondRole string) bool {
 
 // EvaluatePermission checks if the user's role has the permission to execute an action.
 func EvaluatePermission(userRole string, action int) bool {
-	switch userRole {
-	case MemberRoleToken:
-		return checkPermission(action, tokenPermission)
-	case MemberRoleObserver:
-		return checkPermission(action, observerPermissions)
-	case MemberRoleOperator:
-		return checkPermission(action, operatorPermissions)
-	case MemberRoleAdministrator:
-		return checkPermission(action, adminPermissions)
-	case MemberRoleOwner:
-		return checkPermission(action, ownerPermissions)
+	rolePermission := RolePermission[userRole]
+	if rolePermission != nil {
+		return checkPermission(action, rolePermission)
 	}
 
 	return false
