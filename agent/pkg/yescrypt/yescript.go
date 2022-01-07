f3f92d366e5dba0e73a8291da7e1bf89b9fadbbf
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2022-01-07 16:18:56 -0300

    agent: fix golangci-lint error

--- agent/pkg/yescrypt/yescript.go
+++ agent/pkg/yescrypt/yescript.go
@@ -8,20 +8,17 @@ package yescrypt
 #include <crypt.h>
 */
 import "C"
+import "unsafe"
 
-import (
-	"unsafe"
-)
-
-// Verify verifies a yescrypt hash against a given key
+// Verify verifies a yescrypt hash against a given key.
 func Verify(key, hash string) bool {
 	ckey := C.CString(key)
 	chash := C.CString(hash)
 
-	defer C.free(unsafe.Pointer(ckey))
-	defer C.free(unsafe.Pointer(chash))
-
 	out := C.crypt(ckey, chash)
+
+	C.free(unsafe.Pointer(ckey))
+	C.free(unsafe.Pointer(chash))
 
 	return C.GoString(out) == hash
 }
