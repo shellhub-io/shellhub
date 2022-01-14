e10faf4b1de03773d31611af7f04a7cc934da095
Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>
2022-01-14 16:27:03 -0300

    ui: move rename namespace to component
    This commit removes the part of the code responsible for renaming the
    namespace to a new component responsible for just that functionality.
    
    Signed-off-by: Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>

---
+++ ui/src/components/namespace/NamespaceRename.vue
@@ -0,0 +1,139 @@
+<template>
+  <fragment>
+    <ValidationObserver
+      ref="obs"
+      v-slot="{ passes }"
+    >
+      <v-row>
+        <v-col>
+          <h3>
+            Namespace
+          </h3>
+        </v-col>
+
+        <v-spacer />
+
+        <v-col
+          md="auto"
+          class="ml-auto"
+        >
+          <v-tooltip
+            bottom
+            :disabled="hasAuthorizationRenameNamespace"
+          >
+            <template #activator="{ on }">
+              <div v-on="on">
+                <v-btn
+                  :disabled="!hasAuthorizationRenameNamespace"
+                  color="primary"
+                  @click="passes(editNamespace)"
+                >
+                  Rename Namespace
+                </v-btn>
+              </div>
+            </template>
+
+            <span>
+              You don't have this kind of authorization.
+            </span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+
+      <div class="mt-4 mb-2">
+        <ValidationProvider
+          v-slot="{ errors }"
+          ref="providerName"
+          vid="name"
+          name="Priority"
+          rules="required|rfc1123|noDot|namespace"
+        >
+          <v-text-field
+            v-model="name"
+            class="ml-3"
+            label="Name"
+            :error-messages="errors"
+            required
+            data-test="name-text"
+          />
+        </ValidationProvider>
+      </div>
+    </ValidationObserver>
+  </fragment>
+</template>
+
+<script>
+
+import {
+  ValidationObserver,
+  ValidationProvider,
+} from 'vee-validate';
+
+import hasPermission from '@/components/filter/permission';
+
+export default {
+  name: 'NamespaceAddComponent',
+
+  filters: { hasPermission },
+
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+
+  data() {
+    return {
+      name: '',
+    };
+  },
+
+  computed: {
+    namespace() {
+      return this.$store.getters['namespaces/get'];
+    },
+
+    tenant() {
+      return this.$store.getters['auth/tenant'];
+    },
+
+    hasAuthorizationRenameNamespace() {
+      const role = this.$store.getters['auth/role'];
+      if (role !== '') {
+        return hasPermission(
+          this.$authorizer.role[role],
+          this.$actions.namespace.rename,
+        );
+      }
+
+      return false;
+    },
+  },
+
+  created() {
+    this.name = this.namespace.name;
+  },
+
+  methods: {
+    async editNamespace() {
+      try {
+        await this.$store.dispatch('namespaces/put', { id: this.tenant, name: this.name });
+        await this.$store.dispatch('namespaces/get', this.tenant);
+        this.$store.dispatch('snackbar/showSnackbarSuccessAction', this.$success.namespaceEdit);
+      } catch (error) {
+        if (error.response.status === 400) {
+          this.$refs.obs.setErrors({
+            namespace: this.$errors.form.invalid('namespace', 'nonStandardCharacters'),
+          });
+        } else if (error.response.status === 409) {
+          this.$refs.obs.setErrors({
+            namespace: this.$errors.form.invalid('namespace', 'nameUsed'),
+          });
+        } else {
+          this.$store.dispatch('snackbar/showSnackbarErrorAction', this.$errors.snackbar.namespaceEdit);
+        }
+      }
+    },
+  },
+};
+
+</script>
---
+++ ui/tests/unit/components/namespaces/NamespaceRename.spec.js
@@ -0,0 +1,243 @@
+import Vuex from 'vuex';
+import { mount, createLocalVue } from '@vue/test-utils';
+import { ValidationProvider, ValidationObserver } from 'vee-validate';
+import flushPromises from 'flush-promises';
+import Vuetify from 'vuetify';
+import NamespaceRename from '@/components/namespace/NamespaceRename';
+import '@/vee-validate';
+import { actions, authorizer } from '../../../../src/authorizer';
+
+describe('SettingNamespace', () => {
+  const localVue = createLocalVue();
+  const vuetify = new Vuetify();
+  localVue.use(Vuex);
+  localVue.component('ValidationProvider', ValidationProvider);
+  localVue.component('ValidationObserver', ValidationObserver);
+
+  document.body.setAttribute('data-app', true);
+
+  let wrapper;
+
+  const role = ['owner', 'operator'];
+
+  const hasAuthorizationRenameNamespace = {
+    owner: true,
+    operator: false,
+  };
+
+  const members = [
+    {
+      id: 'xxxxxxxx',
+      type: 'owner',
+      username: 'user1',
+    },
+    {
+      id: 'xxxxxxxy',
+      type: 'observer',
+      username: 'user2',
+    },
+  ];
+
+  const openNamespace = {
+    name: 'namespace',
+    members,
+    owner: 'owner',
+    tenant_id: 'xxxxxxxx',
+    devices_count: 1,
+    max_devices: 3,
+  };
+
+  const hostedNamespace = { ...openNamespace, max_devices: -1 };
+
+  const invalidNamespaces = [
+    '\'', '"', '!', '@', '#', '$', '%', '¨', '&', '*', '(', ')', '-', '_', '=', '+', '´', '`', '[',
+    '{', '~', '^', ']', ',', '<', '..', '>', ';', ':', '/', '?',
+  ];
+
+  const invalidMinAndMaxCharacters = [
+    's', 'sh', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  ];
+
+  const tests = [
+    {
+      description: 'Open version',
+      variables: {
+        namespace: openNamespace,
+        tenant: 'xxxxxxxx',
+        hasTenant: true,
+        isEnterprise: false,
+      },
+      data: {
+        name: openNamespace.name,
+      },
+      computed: {
+        namespace: openNamespace,
+        tenant: 'xxxxxxxx',
+      },
+      template: {
+        'name-text': true,
+      },
+    },
+    {
+      description: 'Hosted version',
+      variables: {
+        namespace: hostedNamespace,
+        tenant: 'xxxxxxxx',
+        hasTenant: true,
+        isEnterprise: true,
+      },
+      data: {
+        name: hostedNamespace.name,
+      },
+      computed: {
+        namespace: hostedNamespace,
+        tenant: 'xxxxxxxx',
+      },
+      template: {
+        'name-text': true,
+      },
+    },
+  ];
+
+  const storeVuex = (namespace, tenant, currentrole) => new Vuex.Store({
+    namespaced: true,
+    state: {
+      namespace,
+      tenant,
+      currentrole,
+    },
+    getters: {
+      'namespaces/get': (state) => state.namespace,
+      'auth/tenant': (state) => state.tenant,
+      'auth/role': (state) => state.currentrole,
+    },
+    actions: {
+      'namespaces/put': () => {},
+      'namespaces/get': () => {},
+      'namespaces/removeUser': () => {},
+      'snackbar/showSnackbarSuccessAction': () => {},
+      'snackbar/showSnackbarErrorAction': () => {},
+    },
+  });
+
+  tests.forEach((test) => {
+    role.forEach((currentrole) => {
+      describe(`${test.description} ${currentrole}`, () => {
+        beforeEach(async () => {
+          jest.spyOn(Storage.prototype, 'getItem').mockReturnValue('e359bf484715');
+
+          wrapper = mount(NamespaceRename, {
+            store: storeVuex(
+              test.variables.namespace,
+              test.variables.tenant,
+              currentrole,
+            ),
+            localVue,
+            stubs: ['fragment'],
+            vuetify,
+            mocks: {
+              $authorizer: authorizer,
+              $actions: actions,
+            },
+          });
+        });
+
+        ///////
+        // Component Rendering
+        //////
+
+        it('Is a Vue instance', () => {
+          expect(wrapper).toBeTruthy();
+        });
+        it('Renders the component', () => {
+          expect(wrapper.html()).toMatchSnapshot();
+        });
+
+        ///////
+        // Data checking
+        //////
+
+        it('Compare data with default value', () => {
+          Object.keys(test.data).forEach((item) => {
+            expect(wrapper.vm[item]).toEqual(test.data[item]);
+          });
+        });
+        it('Process data in the computed', () => {
+          Object.keys(test.computed).forEach((item) => {
+            expect(wrapper.vm[item]).toEqual(test.computed[item]);
+          });
+          expect(wrapper.vm.hasAuthorizationRenameNamespace)
+            .toEqual(hasAuthorizationRenameNamespace[currentrole]);
+        });
+
+        //////
+        // HTML validation
+        //////
+
+        it('Renders the template with data', () => {
+          Object.keys(test.template).forEach((item) => {
+            expect(wrapper.find(`[data-test="${item}"]`).exists()).toBe(test.template[item]);
+          });
+        });
+
+        //////
+        // In this case, invalid RFC1123.
+        //////
+
+        invalidNamespaces.forEach((inamespace) => {
+          it(`Shows invalid namespace error for ${inamespace}`, async () => {
+            wrapper.setData({ name: inamespace });
+            await flushPromises();
+
+            const validator = wrapper.vm.$refs.providerName;
+
+            await validator.validate();
+            expect(validator.errors[0]).toBe('You entered an invalid RFC1123 name');
+          });
+        });
+
+        //////
+        // In this case, password should be 3-30 characters long.
+        //////
+
+        invalidMinAndMaxCharacters.forEach((character) => {
+          it(`Shows invalid namespace error for ${character}`, async () => {
+            wrapper.setData({ name: character });
+            await flushPromises();
+
+            const validator = wrapper.vm.$refs.providerName;
+
+            await validator.validate();
+            expect(validator.errors[0]).toBe('Your namespace should be 3-30 characters long');
+          });
+        });
+
+        it('Show validation messages', async () => {
+          //////
+          // In this case, validate fields required.
+          //////
+
+          wrapper.setData({ name: '' });
+          await flushPromises();
+
+          let validator = wrapper.vm.$refs.providerName;
+
+          await validator.validate();
+          expect(validator.errors[0]).toBe('This field is required');
+
+          //////
+          // In this case, must not contain dots.
+          //////
+
+          wrapper.setData({ name: 'ShelHub.' });
+          await flushPromises();
+
+          validator = wrapper.vm.$refs.providerName;
+
+          await validator.validate();
+          expect(validator.errors[0]).toBe('The name must not contain dots');
+        });
+      });
+    });
+  });
+});
