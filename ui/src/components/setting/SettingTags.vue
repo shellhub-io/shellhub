7056a88187265394efb65f22d36270f17d0d1125
Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>
2022-01-26 15:31:15 -0300

    ui: add text about tags
    This commit adds text in tag settings.
    
    Signed-off-by: Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>

--- ui/src/components/setting/SettingTags.vue
+++ ui/src/components/setting/SettingTags.vue
@@ -14,6 +14,17 @@
               <v-spacer />
               <v-spacer />
             </div>
+
+            <div class="text-justify">
+              Tags are a keyword or term assigned to information, as if it were attached to
+              someone or something, for device identification purposes.
+
+              In this case, it is possible to perform some actions, including editing,
+              changing the tag name on all connected devices.
+
+              Following the same logic, when you delete a tag, it is deleted from all
+              related devices.
+            </div>
           </v-card>
 
           <TagList data-test="tagList-component" />
