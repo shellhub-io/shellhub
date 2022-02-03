d36d949dbe29d721ec7aaa21686e903efa3500a7
Henry Barreto <henry.barreto@ossystems.com.br>
2022-02-03 09:06:13 -0300

    openapi: rewrite server and documentation provide

--- openapi/server/index.js
+++ openapi/server/index.js
@@ -1,37 +1,9 @@
-const httpProxy = require('http-proxy');
-const http = require('http');
-const url = require('url');
-const finalhandler = require('finalhandler')
-const serveStatic = require('serve-static')
+import koa from "koa"
+import koaStatic from "koa-static"
 
-// Proxy targets
-const targets = {
-    '/community': 'http://localhost:8081',
-    '/cloud': 'http://localhost:8082'
-};
+const app = new koa()
 
-const proxy = httpProxy.createServer();
+app.use(koaStatic("./www"))
 
-// Generic error handler
-proxy.on('error', function(err, req, res) {
-    res.writeHead(500);
-    res.end('Internal Server Error on Proxy\n');
-});
-
-
-const staticServer = serveStatic('./www', { 'index': ['index.html'] })
-
-http.createServer((req, res) => {
-    const path = url.parse(req.url).pathname;
-
-    for (const target in targets) {
-        if (path.startsWith(target)) {
-            const targetUrl = `${targets[target]}/${path.replace(target, '')}` // rewrite URL removing target prefix
-            return proxy.web(req, res, { target: targetUrl, ignorePath: true });
-        }
-    }
-
-    staticServer(req, res, finalhandler(req, res))
-}).listen(8080);
-
-console.log('Proxy running on port 8080');
+app.listen("8080")
+console.info("Listing on 8080")
