aa48e5e502eeed4064a433ee6b8f7e98a247208f
Henry Barreto <henry.barreto@ossystems.com.br>
2022-02-07 19:09:50 -0300

    api: split errors to an own region on source code

--- api/services/errors.go
+++ api/services/errors.go
@@ -2,29 +2,42 @@ package services
 
 import (
 	"errors"
+	"fmt"
+
+	"github.com/shellhub-io/shellhub/api/store"
+)
+
+func newError(prefix string, err error) error {
+	return fmt.Errorf("%s: %w", prefix, err)
+}
+
+var (
+	ErrNamespaceMemberNotFound   = newError("namespace", errors.New("namespace: member not found"))
+	ErrNamespaceDuplicatedMember = newError("namespace", errors.New("this member already exist in this namespace"))
+	ErrNamespaceNameInvalid      = newError("namespace", errors.New("name is invalid"))
+	ErrNamespaceTenantInvalid    = newError("namespace", errors.New("tenant is invalid"))
+	ErrNamespaceRename           = newError("namespace", store.ErrNamespaceRename)
+	ErrNamespaceNotFound         = newError("namespace", store.ErrNamespaceNotFound)
 )
 
 var (
-	ErrNotFound                  = errors.New("not found")
-	ErrConflict                  = errors.New("conflict")
-	ErrBadRequest                = errors.New("bad request")
-	ErrUnauthorized              = errors.New("unauthorized")
-	ErrForbidden                 = errors.New("forbidden")
-	ErrUserNotFound              = errors.New("user not found")
-	ErrNamespaceNotFound         = errors.New("namespace not found")
-	ErrNamespaceMemberNotFound   = errors.New("member not found on namespace")
-	ErrNamespaceDuplicatedMember = errors.New("namespace already have this member")
-	ErrDuplicateID               = errors.New("user already member of this namespace")
-	ErrConflictName              = errors.New("this Name already exists")
-	ErrInvalidFormat             = errors.New("invalid Name format")
-	ErrMaxDeviceCountReached     = errors.New("maximum number of accepted devices reached")
-	ErrDuplicatedDeviceName      = errors.New("the Name already exists in the namespace")
-	ErrDuplicateFingerprint      = errors.New("this fingerprint already exits")
-	ErrReport                    = errors.New("report error")
-	ErrMaxTagReached             = errors.New("maximum number of tags reached")
-	ErrDeviceNotFound            = errors.New("device not found")
-	ErrDuplicateTagName          = errors.New("this tag Name already exists")
-	ErrTagNameNotFound           = errors.New("tag Name not found")
-	ErrNoTags                    = errors.New("no tags has found")
-	ErrTypeAssertion             = errors.New("type assertion failed")
+	ErrNotFound              = errors.New("not found")
+	ErrConflict              = errors.New("conflict")
+	ErrBadRequest            = errors.New("bad request")
+	ErrUnauthorized          = errors.New("unauthorized")
+	ErrForbidden             = errors.New("forbidden")
+	ErrUserNotFound          = errors.New("user not found")
+	ErrDuplicateID           = errors.New("user already member of this namespace")
+	ErrConflictName          = errors.New("this Name already exists")
+	ErrInvalidFormat         = errors.New("invalid Name format")
+	ErrMaxDeviceCountReached = errors.New("maximum number of accepted devices reached")
+	ErrDuplicatedDeviceName  = errors.New("the Name already exists in the namespace")
+	ErrDuplicateFingerprint  = errors.New("this fingerprint already exits")
+	ErrReport                = errors.New("report error")
+	ErrMaxTagReached         = errors.New("maximum number of tags reached")
+	ErrDeviceNotFound        = errors.New("device not found")
+	ErrDuplicateTagName      = errors.New("this tag Name already exists")
+	ErrTagNameNotFound       = errors.New("tag Name not found")
+	ErrNoTags                = errors.New("no tags has found")
+	ErrTypeAssertion         = errors.New("type assertion failed")
 )
