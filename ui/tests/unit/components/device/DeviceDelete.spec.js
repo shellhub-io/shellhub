81ad04f79bd58bf94c50f50fe1b15eaa9fdbca93
Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>
2022-02-17 14:05:00 -0300

    ui: refactor the component device delete
    This commit refactors the component to block click actions on the listing when
    the user does not have permission. Also, tests are modified to accommodate the
    changes made.
    
    Signed-off-by: Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>

--- ui/tests/unit/components/device/DeviceDelete.spec.js
+++ ui/tests/unit/components/device/DeviceDelete.spec.js
@@ -2,7 +2,6 @@ import Vuex from 'vuex';
 import { mount, createLocalVue } from '@vue/test-utils';
 import Vuetify from 'vuetify';
 import DeviceDelete from '@/components/device/DeviceDelete';
-import { actions, authorizer } from '../../../../src/authorizer';
 
 describe('DeviceDelete', () => {
   const localVue = createLocalVue();
@@ -13,72 +12,77 @@ describe('DeviceDelete', () => {
 
   let wrapper;
 
-  const role = ['owner', 'operator'];
-
-  const hasAuthorization = {
-    owner: true,
-    operator: false,
-  };
-
   const tests = [
     {
-      description: 'Icon',
+      description: 'Dialog closed',
       props: {
         uid: 'a582b47a42d',
         redirect: false,
         show: false,
       },
-      data: {
-        action: 'remove',
-      },
       template: {
+        'remove-icon': true,
+        'remove-title': true,
         'deviceDelete-card': false,
-        'close-btn': false,
-        'remove-btn': false,
+      },
+      templateText: {
+        'remove-title': 'Remove',
       },
     },
     {
-      description: 'Dialog without redirect',
+      description: 'Dialog opened without redirect',
       props: {
         uid: 'a582b47a42d',
         redirect: false,
         show: true,
       },
-      data: {
-        action: 'remove',
-      },
       template: {
+        'remove-icon': true,
+        'remove-title': true,
         'deviceDelete-card': true,
+        'text-title': true,
+        'text-text': true,
         'close-btn': true,
         'remove-btn': true,
       },
+      templateText: {
+        'remove-title': 'Remove',
+        'text-title': 'Are you sure?',
+        'text-text': 'You are about to remove this device.',
+        'close-btn': 'Close',
+        'remove-btn': 'Remove',
+      },
     },
     {
-      description: 'Dialog with redirect',
+      description: 'Dialog opened with redirect',
       props: {
         uid: 'a582b47a42d',
         redirect: true,
         show: true,
       },
-      data: {
-        action: 'remove',
-      },
       template: {
+        'remove-icon': true,
+        'remove-title': true,
         'deviceDelete-card': true,
+        'text-title': true,
+        'text-text': true,
         'close-btn': true,
         'remove-btn': true,
+      },
+      templateText: {
+        'remove-title': 'Remove',
+        'text-title': 'Are you sure?',
+        'text-text': 'You are about to remove this device.',
+        'close-btn': 'Close',
+        'remove-btn': 'Remove',
       },
     },
   ];
 
-  const storeVuex = (currentrole) => new Vuex.Store({
+  const storeVuex = () => new Vuex.Store({
     namespaced: true,
-    state: {
-      currentrole,
-    },
-    getters: {
-      'auth/role': (state) => state.currentrole,
-    },
+    state: { },
+    getters: { },
     actions: {
       'devices/remove': () => {},
       'snackbar/showSnackbarSuccessAction': () => {},
@@ -87,67 +91,54 @@ describe('DeviceDelete', () => {
   });
 
   tests.forEach((test) => {
-    role.forEach((currentrole) => {
-      describe(`${test.description} ${currentrole}`, () => {
-        beforeEach(() => {
-          wrapper = mount(DeviceDelete, {
-            store: storeVuex(currentrole),
-            localVue,
-            stubs: ['fragment'],
-            propsData: {
-              uid: test.props.uid,
-              redirect: test.props.redirect,
-              show: test.props.show,
-            },
-            vuetify,
-            mocks: {
-              $authorizer: authorizer,
-              $actions: actions,
-            },
-          });
+    describe(`${test.description}`, () => {
+      beforeEach(() => {
+        wrapper = mount(DeviceDelete, {
+          store: storeVuex(),
+          localVue,
+          stubs: ['fragment'],
+          propsData: {
+            uid: test.props.uid,
+            redirect: test.props.redirect,
+            show: test.props.show,
+          },
+          vuetify,
         });
+      });
 
-        ///////
-        // Component Rendering
-        //////
+      ///////
+      // Component Rendering
+      //////
 
-        it('Is a Vue instance', () => {
-          expect(wrapper).toBeTruthy();
+      it('Is a Vue instance', () => {
+        expect(wrapper).toBeTruthy();
+      });
+      it('Renders the component', () => {
+        expect(wrapper.html()).toMatchSnapshot();
+      });
+
+      ///////
+      // Data checking
+      //////
+
+      it('Receive data in props', () => {
+        Object.keys(test.props).forEach((item) => {
+          expect(wrapper.vm[item]).toEqual(test.props[item]);
         });
-        it('Renders the component', () => {
-          expect(wrapper.html()).toMatchSnapshot();
-        });
+      });
 
-        ///////
-        // Data checking
-        //////
+      //////
+      // HTML validation
+      //////
 
-        it('Receive data in props', () => {
-          Object.keys(test.props).forEach((item) => {
-            expect(wrapper.vm[item]).toEqual(test.props[item]);
-          });
+      it('Renders the template with data', () => {
+        Object.keys(test.template).forEach((item) => {
+          expect(wrapper.find(`[data-test="${item}"]`).exists()).toBe(test.template[item]);
         });
-        it('Compare data with default value', () => {
-          Object.keys(test.data).forEach((item) => {
-            expect(wrapper.vm[item]).toEqual(test.data[item]);
-          });
-        });
-        it('Process data in the computed', () => {
-          expect(wrapper.vm.hasAuthorization).toEqual(hasAuthorization[currentrole]);
-        });
-
-        //////
-        // HTML validation
-        //////
-
-        it('Renders the template with data', () => {
-          Object.keys(test.template).forEach((item) => {
-            if (!hasAuthorization[currentrole] && currentrole === 'operator' && test.props.show) {
-              expect(wrapper.find(`[data-test="${item}"]`).exists()).toBe(!test.template[item]);
-            } else {
-              expect(wrapper.find(`[data-test="${item}"]`).exists()).toBe(test.template[item]);
-            }
-          });
+      });
+      it('Renders template with expected text', () => {
+        Object.keys(test.templateText).forEach((item) => {
+          expect(wrapper.find(`[data-test="${item}"]`).text()).toContain(test.templateText[item]);
         });
       });
     });
