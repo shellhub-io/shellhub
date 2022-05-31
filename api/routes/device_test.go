d10808198ff7b114ff90c8f34ff6adcaf631edcc
Henry Barreto <henry.barreto@ossystems.com.br>
2022-05-31 11:13:07 -0300

    api: remove cycle import from device_test.go

--- api/routes/device_test.go
+++ api/routes/device_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/shellhub-io/shellhub/api/pkg/gateway"
 	"github.com/shellhub-io/shellhub/api/pkg/guard"
-	"github.com/shellhub-io/shellhub/api/routes/handlers/converter"
 	svc "github.com/shellhub-io/shellhub/api/services"
 	"github.com/shellhub-io/shellhub/api/services/mocks"
 	"github.com/shellhub-io/shellhub/pkg/errors"
@@ -23,6 +22,27 @@ func TestUpdatePendingStatus(t *testing.T) {
 	mock := new(mocks.Service)
 	ctx := context.TODO()
 	h := NewHandler(mock)
+
+	fromErrServiceToHTTPStatus := func(code int) int {
+		switch code {
+		case svc.ErrCodeNotFound:
+			return http.StatusNotFound
+		case svc.ErrCodeInvalid:
+			return http.StatusBadRequest
+		case svc.ErrCodeLimit:
+			return http.StatusForbidden
+		case svc.ErrCodePayment:
+			return http.StatusPaymentRequired
+		case svc.ErrCodeDuplicated:
+			return http.StatusConflict
+		case svc.ErrCodeUnauthorized:
+			return http.StatusUnauthorized
+		case svc.ErrCodeForbidden:
+			return http.StatusForbidden
+		default:
+			return http.StatusInternalServerError
+		}
+	}
 
 	t.Run("Return payment required when the max count is reached", func(t *testing.T) {
 		rec := httptest.NewRecorder()
@@ -41,6 +61,6 @@ func TestUpdatePendingStatus(t *testing.T) {
 		err, ok := output.(errors.Error)
 		assert.True(t, ok)
 
-		assert.Equal(t, http.StatusPaymentRequired, converter.FromErrServiceToHTTPStatus(err.Code))
+		assert.Equal(t, http.StatusPaymentRequired, fromErrServiceToHTTPStatus(err.Code))
 	})
 }
