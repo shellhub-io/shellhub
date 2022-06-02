50f55537143f737cc14635f8abd9781ab1a25bd3
Henry Barreto <henry.barreto@ossystems.com.br>
2022-06-02 14:37:14 -0300

    api: add migration to public key username from empty to .*

---
+++ api/store/mongo/migrations/migration_46_test.go
@@ -0,0 +1,100 @@
+package migrations
+
+import (
+	"context"
+	"sort"
+	"testing"
+
+	"github.com/shellhub-io/shellhub/api/pkg/dbtest"
+	"github.com/shellhub-io/shellhub/pkg/models"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func TestMigration46(t *testing.T) {
+	logrus.Info("Testing Migration 46")
+
+	db := dbtest.DBServer{}
+	defer db.Stop()
+
+	keyUsernameEmpty := &models.PublicKey{
+		Fingerprint: "fingerprint",
+		TenantID:    "tenant",
+		PublicKeyFields: models.PublicKeyFields{
+			Name:     "key",
+			Username: "",
+			Filter: models.PublicKeyFilter{
+				Tags: []string{"tag1", "tag2", "tag3"},
+			},
+		},
+	}
+
+	keyUsernameRegexp := &models.PublicKey{
+		Fingerprint: "fingerprint",
+		TenantID:    "tenant",
+		PublicKeyFields: models.PublicKeyFields{
+			Name:     "key",
+			Username: ".*",
+			Filter: models.PublicKeyFilter{
+				Tags: []string{"tag1", "tag2", "tag3"},
+			},
+		},
+	}
+
+	_, err := db.Client().Database("test").Collection("public_keys").InsertOne(context.Background(), keyUsernameEmpty)
+	assert.NoError(t, err)
+
+	cases := []struct {
+		description string
+		Test        func(t *testing.T)
+	}{
+		{
+			"Success to apply up on migration 46",
+			func(t *testing.T) {
+				t.Helper()
+
+				migrations := GenerateMigrations()[45:46]
+				migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
+				err := migrates.Up(migrate.AllAvailable)
+				assert.NoError(t, err)
+
+				key := new(models.PublicKey)
+				result := db.Client().Database("test").Collection("public_keys").FindOne(context.Background(), bson.M{"tenant_id": keyUsernameEmpty.TenantID})
+				assert.NoError(t, result.Err())
+
+				err = result.Decode(key)
+				assert.NoError(t, err)
+
+				sort.Strings(key.Filter.Tags)
+
+				assert.Equal(t, keyUsernameRegexp, key)
+			},
+		},
+		{
+			"Success to apply down on migration 46",
+			func(t *testing.T) {
+				t.Helper()
+
+				migrations := GenerateMigrations()[45:46]
+				migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
+				err := migrates.Down(migrate.AllAvailable)
+				assert.NoError(t, err)
+
+				key := new(models.PublicKey)
+				result := db.Client().Database("test").Collection("public_keys").FindOne(context.Background(), bson.M{"tenant_id": keyUsernameRegexp.TenantID})
+				assert.NoError(t, result.Err())
+
+				err = result.Decode(key)
+				assert.NoError(t, err)
+
+				assert.Equal(t, keyUsernameEmpty, key)
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.description, tc.Test)
+	}
+}
--- pkg/api/request/publickey.go
+++ pkg/api/request/publickey.go
@@ -25,8 +25,12 @@ type PublicKeyGet struct {
 }
 
 type PublicKeyFilter struct {
-	Hostname string   `json:"hostname,omitempty" validate:"required_without=Tags,excluded_with=Tags,regexp"`
-	Tags     []string `json:"tags,omitempty" validate:"required_without=Hostname,excluded_with=Hostname,min=1,max=3,unique,dive,min=3,max=255,alphanum,ascii,excludes=/@&:"`
+	Hostname string `json:"hostname,omitempty" validate:"required_without=Tags,excluded_with=Tags,regexp"`
+	// FIXME: add validation for tags when it has at least one item.
+	//
+	// If used `min=1` to do that validation, when tags is empty, its zero value, and only hostname is provided,
+	// it throws a error even with `required_without` and `excluded_with`.
+	Tags []string `json:"tags,omitempty" validate:"required_without=Hostname,excluded_with=Hostname,max=3,unique,dive,min=3,max=255,alphanum,ascii,excludes=/@&:"`
 }
 
 // PublicKeyCreate is the structure for the request data at create public key endpoint.
