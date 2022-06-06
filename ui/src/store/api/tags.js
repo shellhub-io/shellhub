6dcdc51fbe3bb2ca7134fb660f658b7192a66aa4
Henry Barreto <henry.barreto@ossystems.com.br>
2022-06-06 15:20:58 -0300

    ui: change store function param from name to tag

--- ui/src/store/api/tags.js
+++ ui/src/store/api/tags.js
@@ -1,7 +1,7 @@
 import http from '@/store/helpers/http';
 
-export const updateTag = async (data) => http().put(`tags/${data.oldTag}`, { name: data.newTag });
+export const updateTag = async (data) => http().put(`tags/${data.oldTag}`, { tag: data.newTag });
 
-export const removeTag = async (name) => http().delete(`tags/${name}`);
+export const removeTag = async (tag) => http().delete(`tags/${tag}`);
 
 export const getTags = async () => http().get('/tags');
