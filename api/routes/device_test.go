2cf5db478f1bf2ac645e30fb0fc8ee404a487508
Henry Barreto <henry.barreto@ossystems.com.br>
2022-06-07 17:12:02 -0300

    api: change handlers and routes error to package
    In order to fix cyclic import in future routes tests, It was necessary
    to turn routes' packages into api's packages. Furthermore, the route's
    errors what were inside the routes package, it is now a package apart
    called errors.
    This refactor, what moves errors inside the routes package to an own
    package, also must be done for services and store, but it is not within
    the scope of this activity.

--- api/routes/device_test.go
+++ api/routes/device_test.go
@@ -35,7 +35,7 @@ func TestUpdatePendingStatus(t *testing.T) {
 		echoContext := e.NewContext(req, rec)
 		echoContext.SetParamNames("uid", "status")
 		echoContext.SetParamValues("123", "pending")
-		mock.On("UpdatePendingStatus", ctx, models.UID("123"), "pending", "").Return(svc.ErrDeviceLimit)
+		mock.On("UpdatePendingStatus", ctx, models.UID("123"), "pending", "00000000-0000-4000-0000-000000000000").Return(svc.ErrDeviceLimit)
 
 		apictx := gateway.NewContext(mock, echoContext)
 
