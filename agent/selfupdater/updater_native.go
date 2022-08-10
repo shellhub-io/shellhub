8e322708ac28d498d2d2d0603217ab91ca0e83dc
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2022-08-10 18:26:14 -0300

    agent: ensure that selfupdater are running inside a docker container

--- agent/selfupdater/updater_native.go
+++ agent/selfupdater/updater_native.go
@@ -3,26 +3,6 @@
 
 package selfupdater
 
-import (
-	"github.com/Masterminds/semver"
-)
-
-type nativeUpdater struct {
-	version string
-}
-
-func (n *nativeUpdater) CurrentVersion() (*semver.Version, error) {
-	return semver.NewVersion(n.version)
-}
-
-func (n *nativeUpdater) ApplyUpdate(_ *semver.Version) error {
-	return nil
-}
-
-func (n *nativeUpdater) CompleteUpdate() error {
-	return nil
-}
-
 func NewUpdater(version string) (Updater, error) {
 	return &nativeUpdater{version}, nil
 }
