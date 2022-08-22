81ab135cae5d14d820066ca578a16b7fb4dcf13d
Henry Barreto <me@henrybarreto.dev>
2022-08-22 09:32:19 -0300

    api: add migration to add position to session from its IP

--- api/store/mongo/migrations/main.go
+++ api/store/mongo/migrations/main.go
@@ -56,6 +56,7 @@ func GenerateMigrations() []migrate.Migration {
 		migration44,
 		migration45,
 		migration46,
+		migration47,
 	}
 }
 
---
+++ api/store/mongo/migrations/migration_47_test.go
@@ -0,0 +1,83 @@
+package migrations
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/shellhub-io/shellhub/api/pkg/dbtest"
+	"github.com/shellhub-io/shellhub/pkg/models"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func TestMigration47(t *testing.T) {
+	logrus.Info("Testing Migration 47")
+
+	db := dbtest.DBServer{}
+	defer db.Stop()
+
+	sessionWithoutPossition := &models.Session{
+		UID:       "test",
+		IPAddress: "201.182.197.68",
+	}
+
+	_, err := db.Client().Database("test").Collection("sessions").InsertOne(context.Background(), sessionWithoutPossition)
+	assert.NoError(t, err)
+
+	cases := []struct {
+		description string
+		Test        func(t *testing.T)
+	}{
+		{
+			"Success to apply up on migration 47",
+			func(t *testing.T) {
+				t.Helper()
+
+				migrations := GenerateMigrations()[46:47]
+				migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
+				err := migrates.Up(migrate.AllAvailable)
+				assert.NoError(t, err)
+
+				key := new(models.Session)
+				result := db.Client().Database("test").Collection("sessions").FindOne(context.Background(), bson.M{"uid": sessionWithoutPossition.UID})
+				assert.NoError(t, result.Err())
+
+				err = result.Decode(key)
+				assert.NoError(t, err)
+
+				if os.Getenv("GEOIP") == "true" {
+					assert.NotEqual(t, sessionWithoutPossition.Position, key.Position)
+				} else {
+					assert.Equal(t, sessionWithoutPossition.Position, key.Position)
+				}
+			},
+		},
+		{
+			"Success to apply down on migration 47",
+			func(t *testing.T) {
+				t.Helper()
+
+				migrations := GenerateMigrations()[46:47]
+				migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
+				err := migrates.Down(migrate.AllAvailable)
+				assert.NoError(t, err)
+
+				key := new(models.Session)
+				result := db.Client().Database("test").Collection("sessions").FindOne(context.Background(), bson.M{"uid": sessionWithoutPossition.UID})
+				assert.NoError(t, result.Err())
+
+				err = result.Decode(key)
+				assert.NoError(t, err)
+
+				assert.Equal(t, sessionWithoutPossition.Position, key.Position)
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.description, tc.Test)
+	}
+}
