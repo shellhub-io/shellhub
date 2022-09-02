4aff93b615ca5a5e34818fadb69e3fbe5cb65337
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2022-09-02 11:20:34 -0300

    agent: set agent platform during initialization
    As bundle install method uses the same binary used by docker
    container we need to detect in runtime if we are running
    in docker environment or somewhere

--- agent/init_docker.go
+++ agent/init_docker.go
@@ -4,13 +4,21 @@
 package main
 
 import (
+	"os"
+
 	"github.com/shellhub-io/shellhub/agent/pkg/osauth"
 	"github.com/shellhub-io/shellhub/agent/pkg/sysinfo"
 )
 
-var AgentPlatform = "docker"
+var AgentPlatform string
 
 func init() {
+	if _, err := os.Stat("/.dockerenv"); os.IsNotExist(err) {
+		AgentPlatform = "bundle"
+	} else {
+		AgentPlatform = "docker"
+	}
+
 	osauth.DefaultShadowFilename = "/host/etc/shadow"
 	sysinfo.DefaultOSReleaseFilename = "/host/etc/os-release"
 }
