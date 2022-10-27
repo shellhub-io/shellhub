c96f71edf81127537f3fe41638e184d60e909fb0
Henry Barreto <me@henrybarreto.dev>
2022-10-27 13:40:23 -0300

    api: fix migration 50 to count MaxNamespaces with billing actived

--- api/store/mongo/migrations/migration_50_test.go
+++ api/store/mongo/migrations/migration_50_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/shellhub-io/shellhub/api/pkg/dbtest"
 	"github.com/shellhub-io/shellhub/pkg/envs"
+	envMocks "github.com/shellhub-io/shellhub/pkg/envs/mocks"
 	"github.com/shellhub-io/shellhub/pkg/models"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
@@ -17,15 +18,11 @@ import (
 func TestMigration50(t *testing.T) {
 	logrus.Info("Testing Migration 50")
 
-	var maxNamespacesWanted int
-	if envs.IsCloud() {
-		maxNamespacesWanted = 1
-	} else {
-		maxNamespacesWanted = -1
-	}
-
 	db := dbtest.DBServer{}
 	defer db.Stop()
+
+	mock := &envMocks.Backend{}
+	envs.DefaultBackend = mock
 
 	user1ID, err := primitive.ObjectIDFromHex("507f1f77bcf86cd799439011")
 	assert.NoError(t, err)
@@ -42,6 +39,9 @@ func TestMigration50(t *testing.T) {
 	namespace1 := &models.Namespace{
 		Name:  "namespace1",
 		Owner: user1ID.String(),
+		Billing: &models.Billing{
+			Active: true,
+		},
 	}
 	namespace2 := &models.Namespace{
 		Name:  "namespace2",
@@ -52,65 +52,164 @@ func TestMigration50(t *testing.T) {
 		Owner: user2ID.String(),
 	}
 
-	_, err = db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user1)
-	assert.NoError(t, err)
-	_, err = db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user2)
-	assert.NoError(t, err)
-	_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace1)
-	assert.NoError(t, err)
-	_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace2)
-	assert.NoError(t, err)
-	_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace3)
-	assert.NoError(t, err)
-
 	cases := []struct {
 		description string
-		Test        func(t *testing.T)
+		before      func()
+		test        func() (int, error)
+		expected    int
+		after       func()
 	}{
 		{
-			"Success to apply up on migration 50",
-			func(t *testing.T) {
-				t.Helper()
+			"Success to apply up on migration 50 when it is a ShellHub Cloud instance",
+			func() {
+				_, err := db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user1)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user2)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace1)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace2)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace3)
+				assert.NoError(t, err)
+			},
+			func() (int, error) {
+				mock.On("Get", "SHELLHUB_CLOUD").Return("true").Once()
 
-				migrations := GenerateMigrations()[48:50]
+				migrations := GenerateMigrations()[49:50]
 				migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
 				err := migrates.Up(migrate.AllAvailable)
-				assert.NoError(t, err)
+				if err != nil {
+					return 0, err
+				}
 
 				user := new(models.User)
 				result := db.Client().Database("test").Collection("users").FindOne(context.TODO(), bson.M{"_id": user1ID.String()})
-				assert.NoError(t, result.Err())
+				if err != nil {
+					return 0, err
+				}
 
 				err = result.Decode(user)
-				assert.NoError(t, err)
+				if err != nil {
+					return 0, err
+				}
 
-				assert.Equal(t, maxNamespacesWanted, user.MaxNamespaces)
+				return user.MaxNamespaces, nil
+			},
+			2,
+			func() {
+				err = db.Client().Database("test").Collection("users").Drop(context.TODO())
+				assert.NoError(t, err)
+				err = db.Client().Database("test").Collection("namespaces").Drop(context.TODO())
+				assert.NoError(t, err)
+				err = db.Client().Database("test").Collection("migrations").Drop(context.TODO())
+				assert.NoError(t, err)
+			},
+		},
+		{
+			"Success to apply up on migration 50 when it is a ShellHub Community instance",
+			func() {
+				_, err := db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user1)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user2)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace1)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace2)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace3)
+				assert.NoError(t, err)
+			},
+			func() (int, error) {
+				mock.On("Get", "SHELLHUB_CLOUD").Return("false").Once()
+
+				migrations := GenerateMigrations()[49:50]
+				migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
+				err := migrates.Up(migrate.AllAvailable)
+				if err != nil {
+					return 0, err
+				}
+
+				user := new(models.User)
+				result := db.Client().Database("test").Collection("users").FindOne(context.TODO(), bson.M{"_id": user1ID.String()})
+				if err != nil {
+					return 0, err
+				}
+
+				err = result.Decode(user)
+				if err != nil {
+					return 0, err
+				}
+
+				return user.MaxNamespaces, nil
+			},
+			-1,
+			func() {
+				err = db.Client().Database("test").Collection("users").Drop(context.TODO())
+				assert.NoError(t, err)
+				err = db.Client().Database("test").Collection("namespaces").Drop(context.TODO())
+				assert.NoError(t, err)
+				err = db.Client().Database("test").Collection("migrations").Drop(context.TODO())
+				assert.NoError(t, err)
 			},
 		},
 		{
 			"Success to apply down on migration 50",
-			func(t *testing.T) {
-				t.Helper()
-
-				migrations := GenerateMigrations()[48:50]
+			func() {
+				_, err := db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user1)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("users").InsertOne(context.TODO(), user2)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace1)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace2)
+				assert.NoError(t, err)
+				_, err = db.Client().Database("test").Collection("namespaces").InsertOne(context.TODO(), namespace3)
+				assert.NoError(t, err)
+			},
+			func() (int, error) {
+				migrations := GenerateMigrations()[49:50]
 				migrates := migrate.NewMigrate(db.Client().Database("test"), migrations...)
 				err := migrates.Down(migrate.AllAvailable)
-				assert.NoError(t, err)
+				if err != nil {
+					return 0, err
+				}
 
 				user := new(models.User)
 				result := db.Client().Database("test").Collection("users").FindOne(context.TODO(), bson.M{"_id": user1ID.String()})
-				assert.NoError(t, result.Err())
+				if err != nil {
+					return 0, err
+				}
 
 				err = result.Decode(user)
-				assert.NoError(t, err)
+				if err != nil {
+					return 0, err
+				}
 
-				assert.Equal(t, 0, user.MaxNamespaces)
+				return user.MaxNamespaces, nil
+			},
+			0,
+			func() {
+				err = db.Client().Database("test").Collection("users").Drop(context.TODO())
+				assert.NoError(t, err)
+				err = db.Client().Database("test").Collection("namespaces").Drop(context.TODO())
+				assert.NoError(t, err)
+				err = db.Client().Database("test").Collection("migrations").Drop(context.TODO())
+				assert.NoError(t, err)
 			},
 		},
 	}
 
 	for _, test := range cases {
 		tc := test
-		t.Run(tc.description, tc.Test)
+		t.Run(tc.description, func(t *testing.T) {
+			tc.before()
+
+			actual, err := tc.test()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, actual)
+
+			tc.after()
+		})
 	}
 }
