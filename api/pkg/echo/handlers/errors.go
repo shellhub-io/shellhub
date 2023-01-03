de39a1f93e3410998e44ebd9ebc0d8549edd268d
Henry Barreto <me@henrybarreto.dev>
2023-01-03 14:05:27 -0300

    api: simplify echo error handler

--- api/pkg/echo/handlers/errors.go
+++ api/pkg/echo/handlers/errors.go
@@ -13,6 +13,29 @@ import (
 	"github.com/shellhub-io/shellhub/pkg/errors"
 )
 
+func isErrorUnknown(err error) (bool, error) {
+	unknown, ok := err.(errors.Error)
+	if !ok {
+		return true, err
+	}
+
+	return (unknown.Layer != services.ErrLayer && unknown.Layer != store.ErrLayer), unknown
+}
+
+func isLastErrorUnknown(err error) (bool, error) {
+	converted, ok := err.(errors.Error)
+	if !ok {
+		return true, nil
+	}
+
+	last := errors.GetLastError(converted)
+	if last == nil {
+		return true, nil
+	}
+
+	return isErrorUnknown(last)
+}
+
 func report(reporter *sentry.Client, err error, request *http.Request) {
 	go func() {
 		if reporter != nil {
@@ -25,50 +48,42 @@ func report(reporter *sentry.Client, err error, request *http.Request) {
 	}()
 }
 
+func maybeReport(reporter *sentry.Client, err error, request *http.Request) {
+	if ok, last := isLastErrorUnknown(err); ok {
+		report(reporter, last, request)
+	}
+}
+
 // NewErrors returns a custom echo's error handler.
 //
 // When the error is from errors.Error type, it will check for the layer and response with the appropriated HTTP status
 // code. However, if the error is not from errors.Error type, it will respond with HTTP status code 500. When this error
 // occurs, it will also try to send the error to Sentry.
 func NewErrors(reporter *sentry.Client) func(error, echo.Context) {
-	return func(e error, ctx echo.Context) {
-		if err, ok := e.(errors.Error); ok { //nolint:nestif
-			switch err.Layer {
+	return func(err error, ctx echo.Context) {
+		var status int
+
+		if converted, ok := err.(errors.Error); ok {
+			switch converted.Layer {
 			case guard.ErrLayer:
-				ctx.NoContent(http.StatusForbidden) //nolint:errcheck
+				status = http.StatusForbidden
 			case routes.ErrLayer:
-				if last := errors.GetLastError(err); last != nil {
-					if converted, ok := last.(errors.Error); !ok || (converted.Layer != services.ErrLayer && converted.Layer != store.ErrLayer) {
-						report(reporter, last, ctx.Request())
-						ctx.NoContent(http.StatusInternalServerError) //nolint:errcheck
-
-						return
-					}
-				}
-
-				ctx.NoContent(converter.FromErrRouteToHTTPStatus(err.Code)) //nolint:errcheck
+				status = converter.FromErrRouteToHTTPStatus(converted.Code)
 			case services.ErrLayer:
-				if last := errors.GetLastError(err); last != nil {
-					if converted, ok := last.(errors.Error); !ok || (converted.Layer != services.ErrLayer && converted.Layer != store.ErrLayer) {
-						report(reporter, last, ctx.Request())
-						ctx.NoContent(http.StatusInternalServerError) //nolint:errcheck
-
-						return
-					}
-				}
-
-				ctx.NoContent(converter.FromErrServiceToHTTPStatus(err.Code)) //nolint:errcheck
+				status = converter.FromErrServiceToHTTPStatus(converted.Code)
 			case store.ErrLayer:
-				report(reporter, err, ctx.Request())
-				ctx.NoContent(http.StatusInternalServerError) //nolint:errcheck
+				status = http.StatusInternalServerError
 			default:
-				report(reporter, err, ctx.Request())
-				ctx.NoContent(http.StatusInternalServerError) //nolint:errcheck
+				status = http.StatusInternalServerError
 			}
-		} else {
-			report(reporter, e, ctx.Request())
 
-			ctx.NoContent(http.StatusInternalServerError) //nolint:errcheck
+			maybeReport(reporter, converted, ctx.Request())
+		} else {
+			status = http.StatusInternalServerError
+
+			report(reporter, err, ctx.Request())
 		}
+
+		ctx.NoContent(status) //nolint:errcheck
 	}
 }
