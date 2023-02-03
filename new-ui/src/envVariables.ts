8fca2802133479f8af229949d9ff507c7b671808
Antony Rafael <depaulacostaantony@gmail.com>
2023-02-03 11:05:09 -0300

    new-ui: add window.env support  to take env in production

--- new-ui/src/envVariables.ts
+++ new-ui/src/envVariables.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
-// import.meta.env.MODE
+// @ts-nocheck
+
 export const envVariables = {
-  isEnterprise: process.env.VUE_APP_SHELLHUB_ENTERPRISE === "true",
-  isCloud: process.env.VUE_APP_SHELLHUB_CLOUD === "true",
-  stripePublishableKey: process.env.VUE_APP_SHELLHUB_STRIPE_PUBLISHABLE_KEY,
-  billingEnable: process.env.VUE_APP_SHELLHUB_BILLING === "true",
-  version: process.env.SHELLHUB_VERSION,
-  announcementsEnable: process.env.VUE_APP_SHELLHUB_ANNOUNCEMENTS === "true",
-  stripeKey: process.env.VUE_APP_SHELLHUB_STRIPE_PUBLISHABLE_KEY,
+  isEnterprise: (window.env || process.env).VUE_APP_SHELLHUB_ENTERPRISE === "true",
+  isCloud: (window.env || process.env).VUE_APP_SHELLHUB_CLOUD === "true",
+  stripePublishableKey: (window.env || process.env).VUE_APP_SHELLHUB_STRIPE_PUBLISHABLE_KEY,
+  billingEnable: (window.env || process.env).VUE_APP_SHELLHUB_BILLING === "true",
+  version: (window.env || process.env).SHELLHUB_VERSION,
+  announcementsEnable: (window.env || process.env).VUE_APP_SHELLHUB_ANNOUNCEMENTS === "true",
+  stripeKey: (window.env || process.env).VUE_APP_SHELLHUB_STRIPE_PUBLISHABLE_KEY,
 };
