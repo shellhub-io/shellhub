ec52a5aefde8556ab4870dba2fc93dfb3c00c07a
Antony Rafael <depaulacostaantony@gmail.com>
2023-02-16 13:50:12 -0300

    new-ui: setting billing page fix
    - Remove unuseful request when the user is not the namespace owner
    - Fix `SettingOwnerInfo` to show the namespace owner username
    - Update `SettingOwnerInfo` test

--- ui/src/components/Setting/SettingBilling.vue
+++ ui/src/components/Setting/SettingBilling.vue
@@ -28,177 +28,185 @@
               </div>
             </v-alert>
           </div>
-        </div>
 
-        <v-row class="mt-4">
-          <v-col>
-            <h3>Subscription info</h3>
-          </v-col>
+          <v-row class="mt-4">
+            <v-col>
+              <h3>Subscription info</h3>
+            </v-col>
 
-          <v-spacer />
+            <v-spacer />
 
-          <v-col v-if="state === 'inactive'" md="auto" class="ml-auto">
-            <BillingPaymentMethod
-              type-operation="subscription"
-              data-test="subscriptionPaymentMethod-component"
-            />
-          </v-col>
-        </v-row>
+            <v-col v-if="state === 'inactive'" md="auto" class="ml-auto">
+              <BillingPaymentMethod
+                type-operation="subscription"
+                data-test="subscriptionPaymentMethod-component"
+              />
+            </v-col>
+          </v-row>
 
-        <div class="mt-6 pl-4 pr-4">
+          <div class="mt-6 pl-4 pr-4">
+            <div
+              v-if="state === 'pending' && !retrialExceeded"
+              data-test="pendingRetrial-div"
+            >
+              <p class="ma-4">
+                You have a pending request, please wait a while ...
+              </p>
+            </div>
+
+            <div
+              v-else-if="state === 'pending' && retrialExceeded"
+              data-test="pendingExceeded-div"
+            >
+              <p class="ma-4">
+                Couldn't proccess your last request, please try again later.
+              </p>
+            </div>
+
+            <div v-else-if="state === 'inactive'">
+              <div data-test="freePlan-div">
+                <p>Plan: <b> Free </b></p>
+
+                <p>
+                  Description: You can add up to 3 devices while using the
+                  'Free' plan.
+                </p>
+              </div>
+            </div>
+
+            <div v-else-if="active && state === 'processed'">
+              <div data-test="premiumPlan-div">
+                <p>Plan: <b> Premium usage </b></p>
+
+                <p>
+                  Description: In this plan, the amount is charged according to
+                  the number of devices used.
+                </p>
+              </div>
+            </div>
+          </div>
+
           <div
-            v-if="state === 'pending' && !retrialExceeded"
-            data-test="pendingRetrial-div"
+            v-if="
+              hasAuthorization && active && renderData && state === 'processed'
+            "
+            class="mt-4 mb-4"
+            data-test="subscriptionActive-div"
           >
-            <p class="ma-4">
-              You have a pending request, please wait a while ...
-            </p>
-          </div>
-
-          <div
-            v-else-if="state === 'pending' && retrialExceeded"
-            data-test="pendingExceeded-div"
-          >
-            <p class="ma-4">
-              Couldn't proccess your last request, please try again later.
-            </p>
-          </div>
-
-          <div v-else-if="state === 'inactive'">
-            <div data-test="freePlan-div">
-              <p>Plan: <b> Free </b></p>
-
-              <p>
-                Description: You can add up to 3 devices while using the 'Free'
-                plan.
-              </p>
-            </div>
-          </div>
-
-          <div v-else-if="active && state === 'processed'">
-            <div data-test="premiumPlan-div">
-              <p>Plan: <b> Premium usage </b></p>
-
-              <p>
-                Description: In this plan, the amount is charged according to
-                the number of devices used.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div
-          v-if="
-            hasAuthorization && active && renderData && state === 'processed'
-          "
-          class="mt-4 mb-4"
-          data-test="subscriptionActive-div"
-        >
-          <v-divider />
-          <v-divider />
-
-          <div class="mt-6 mb-6">
-            <v-row>
-              <v-col>
-                <h3>Next bill</h3>
-              </v-col>
-            </v-row>
-
-            <div class="mt-6 pl-4 pr-4">
-              <p>
-                Date:
-                <b>
-                  {{ formatDateWithoutDayAndHours(infoBillingData.periodEnd) }}
-                </b>
-              </p>
-
-              <p>
-                Estimated total:
-                <b v-if="renderData">
-                  {{ formatCurrency(infoBillingData.nextPaymentDue, infoBillingData.currency) }}
-                </b>
-              </p>
-            </div>
-          </div>
-
-          <v-divider />
-          <v-divider />
-
-          <div class="mt-6 mb-6">
-            <v-row>
-              <v-col>
-                <h3>Latest invoices</h3>
-              </v-col>
-            </v-row>
-          </div>
-
-          <BillingInvoiceList data-test="invoiceList-component" />
-
-          <v-divider />
-          <v-divider />
-          <div class="mt-6 mb-2">
-            <v-row>
-              <v-col>
-                <h3>Payment methods</h3>
-              </v-col>
-
-              <v-spacer />
-
-              <v-col md="auto" class="ml-auto">
-                <BillingPaymentMethod
-                  type-operation="update"
-                  data-test="updatePaymentMethod-component"
-                  @update="getSubscriptionInfo()"
-                />
-              </v-col>
-            </v-row>
-          </div>
-
-          <div>
-            <BillingPaymentList
-              data-test="paymentMethods-component"
-              v-model:cards="cardBillingData"
-            />
-
             <v-divider />
             <v-divider />
-          </div>
 
-          <div data-test="cancel-div" class="mt-6">
-            <v-row>
-              <v-col>
-                <h3>Cancel Subscription</h3>
-              </v-col>
-            </v-row>
-
-            <div class="mt-2 pl-4">
+            <div class="mt-6 mb-6">
               <v-row>
                 <v-col>
-                  <p>
-                    When canceling subscription, you may lose access to devices.
-                  </p>
+                  <h3>Next bill</h3>
+                </v-col>
+              </v-row>
+
+              <div class="mt-6 pl-4 pr-4">
+                <p>
+                  Date:
+                  <b>
+                    {{
+                      formatDateWithoutDayAndHours(infoBillingData.periodEnd)
+                    }}
+                  </b>
+                </p>
+
+                <p>
+                  Estimated total:
+                  <b v-if="renderData">
+                    {{
+                      formatCurrency(
+                        infoBillingData.nextPaymentDue,
+                        infoBillingData.currency,
+                      )
+                    }}
+                  </b>
+                </p>
+              </div>
+            </div>
+
+            <v-divider />
+            <v-divider />
+
+            <div class="mt-6 mb-6">
+              <v-row>
+                <v-col>
+                  <h3>Latest invoices</h3>
+                </v-col>
+              </v-row>
+            </div>
+
+            <BillingInvoiceList data-test="invoiceList-component" />
+
+            <v-divider />
+            <v-divider />
+            <div class="mt-6 mb-2">
+              <v-row>
+                <v-col>
+                  <h3>Payment methods</h3>
                 </v-col>
 
                 <v-spacer />
 
                 <v-col md="auto" class="ml-auto">
-                  <BillingCancel
-                    v-if="renderData"
-                    :nextPaymentDue="infoBillingData.nextPaymentDue"
-                    :currency="infoBillingData.currency"
+                  <BillingPaymentMethod
+                    type-operation="update"
+                    data-test="updatePaymentMethod-component"
+                    @update="getSubscriptionInfo()"
                   />
                 </v-col>
               </v-row>
             </div>
-          </div>
-        </div>
 
-        <div v-else-if="hasAuthorization && state === 'processed' && active">
-          <div data-test="activeLoading-div">
-            <v-divider />
-            <v-divider />
-            <div class="mt-6 mb-2">
-              <p>Loading data</p>
+            <div>
+              <BillingPaymentList
+                data-test="paymentMethods-component"
+                v-model:cards="cardBillingData"
+              />
+
+              <v-divider />
+              <v-divider />
+            </div>
+
+            <div data-test="cancel-div" class="mt-6">
+              <v-row>
+                <v-col>
+                  <h3>Cancel Subscription</h3>
+                </v-col>
+              </v-row>
+
+              <div class="mt-2 pl-4">
+                <v-row>
+                  <v-col>
+                    <p>
+                      When canceling subscription, you may lose access to
+                      devices.
+                    </p>
+                  </v-col>
+
+                  <v-spacer />
+
+                  <v-col md="auto" class="ml-auto">
+                    <BillingCancel
+                      v-if="renderData"
+                      :nextPaymentDue="infoBillingData.nextPaymentDue"
+                      :currency="infoBillingData.currency"
+                    />
+                  </v-col>
+                </v-row>
+              </div>
+            </div>
+          </div>
+
+          <div v-else-if="hasAuthorization && state === 'processed' && active">
+            <div data-test="activeLoading-div">
+              <v-divider />
+              <v-divider />
+              <div class="mt-6 mb-2">
+                <p>Loading data</p>
+              </div>
             </div>
           </div>
         </div>
@@ -209,7 +217,13 @@
 
 <script lang="ts">
 /* eslint-disable */
-import { defineComponent, ref, computed, onMounted, watch, onBeforeMount, onUpdated } from "vue";
+import {
+  defineComponent,
+  ref,
+  computed,
+  onMounted,
+  watch,
+} from "vue";
 import { loadStripe } from "@stripe/stripe-js";
 import { useStore } from "../../store";
 import hasPermission from "../../utils/permission";
@@ -308,7 +322,7 @@ export default defineComponent({
     };
 
     const getSubscriptionInfo = async () => {
-      if (active.value) {
+      if (active.value && hasAuthorization.value) {
         try {
           await store.dispatch("billing/getSubscription");
           renderData.value = true;
