135e307b047b7f0044b3250a0747a121d377091d
Henry Barreto <henry.barreto@ossystems.com.br>
2023-04-26 11:58:25 -0300

    gateway,docker: add certbot to enable SSL certification on gateway

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -15,7 +15,7 @@ if [ "$MAX_WORKER_OPEN_FILES" == "0" ]; then
     MAX_WORKER_OPEN_FILES=$(($(ulimit -Sn) - 1024))
 
     if [ "$MAX_WORKER_OPEN_FILES" -lt "1024" ]; then
-	MAX_WORKER_OPEN_FILES=1024
+        MAX_WORKER_OPEN_FILES=1024
     fi
 fi
 
@@ -29,20 +29,74 @@ export MAX_WORKER_CONNECTIONS
 
 export HOST_IP=$(ip -4 route show default | awk '{ print $3 }')
 
+# The certificate generation is only available in production mode and if the SHELLHUB_AUTO_SSL is set to true.
+if [ "$SHELLHUB_ENV" != "development" ] && [ "$SHELLHUB_AUTO_SSL" == "true" ]; then
+    if [ -z "$SHELLHUB_DOMAIN" ]; then
+        echo "SHELLHUB_DOMAIN cannot be empty"
+        exit 1
+    fi
+
+    if ! echo "$SHELLHUB_DOMAIN" | grep -qE '^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$'; then
+        echo "SHELLHUB_DOMAIN must be a valid domain name"
+        exit 1
+    fi
+
+    # If the certificate is not generated yet, generate it.
+    if [ ! -f /etc/letsencrypt/live/$SHELLHUB_DOMAIN/fullchain.pem ]; then
+        echo "Generating SSL certificate"
+
+        certbot certonly --non-interactive --agree-tos --register-unsafely-without-email --standalone --preferred-challenges http -n -d $SHELLHUB_DOMAIN
+        if [ $? -ne 0 ]; then
+            echo "Failed to generate SSL certificate"
+            exit 1
+        fi
+
+        echo "SSL certificate successfully generated"
+
+        curl https://ssl-config.mozilla.org/ffdhe2048.txt > /etc/letsencrypt/live/$SHELLHUB_DOMAIN/dhparam.pem 2> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "Failed to download Mozilla's DH parameters"
+            exit 1
+        fi
+
+        echo "Mozilla's DH parameters successfully downloaded"
+    fi
+
+    # Loop every 24 hours to check if certificate is about to expire.
+    # It is safe to run the renew each 24 hours because the command will only renew if the certificate is about to expire.
+    # About to expire is defined as: "if a certificate is going to expire in less than 30 days, it will be renewed."
+    # https://eff-certbot.readthedocs.io/en/stable/using.html#renewing-certificates
+    while sleep "24h" ; do
+        # If certificate is already generated, check if it is about to expire. If so, renew it.
+        # You may want to renew near the 60 day mark, to provide enough time for any possible problems that may arise.
+        # https://letsencrypt.org/docs/faq/#what-is-the-lifetime-for-let-s-encrypt-certificates-for-how-long-are-they-valid
+
+        echo "Checking if SSL certificate needs to be renewed"
+        certbot renew
+        if [ $? -ne 0 ]; then
+            echo "Failed to renew SSL certificate"
+            exit 1
+        fi
+
+        nginx -s reload
+
+        echo "SSL certificate successfully renewed"
+    done &
+fi
+
 generate() {
-	gomplate -f /app/nginx.conf -o /usr/local/openresty/nginx/conf/nginx.conf
-	gomplate -f /app/conf.d/shellhub.conf -o /etc/nginx/conf.d/shellhub.conf
+    gomplate -f /app/nginx.conf -o /usr/local/openresty/nginx/conf/nginx.conf
+    gomplate -f /app/conf.d/shellhub.conf -o /etc/nginx/conf.d/shellhub.conf
 }
 
-watch () {
-  while inotifywait -q -r -e close_write "/app/nginx.conf" "/app/conf.d/" > /dev/null;
-  do
-	  generate
-	  nginx -s reload
-  done
-}
+if [ "$SHELLHUB_ENV" == "development" ]; then
+    while inotifywait -q -r -e close_write "/app/nginx.conf" "/app/conf.d/" > /dev/null; do
+        generate
+        nginx -s reload
+    done &
+fi
 
 generate
-watch &
 
+echo "Starting NGINX"
 exec "$@"
