3f741cc2d368dacab7072eb473eb0eb0fda81e1a
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2023-05-31 10:23:54 -0300

    refactor(ui): refactor API setup to create a unique axios instance

--- ui/src/api/http.ts
+++ ui/src/api/http.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosInterface } from "axios";
 import * as axiosTs from "./client";
 import { Configuration } from "./client";
 import { setupInterceptorsTo } from "./interceptors";
@@ -11,39 +12,69 @@ configuration.accessToken = localStorage.getItem("token") || "";
 const cloudApiConfiguration = new Configuration();
 cloudApiConfiguration.basePath = "https://cloud.shellhub.io";
 
-const sessionsApi = new axiosTs.SessionsApi(configuration);
-const devicesApi = new axiosTs.DevicesApi(configuration);
-const defaultApi = new axiosTs.DefaultApi(configuration);
-const namespacesApi = new axiosTs.NamespacesApi(configuration);
-const sshApi = new axiosTs.SshApi(configuration);
-const tagsApi = new axiosTs.TagsApi(configuration);
-const usersApi = new axiosTs.UsersApi(configuration);
-const billingApi = new axiosTs.BillingApi(configuration);
-const rulesApi = new axiosTs.RulesApi(configuration);
-const announcementApi = new axiosTs.AnnouncementsApi(cloudApiConfiguration);
+// Creates a new axios instance and setup interceptors by default
+const newAxiosInstance = (setupInterceptor = true): AxiosInterface => {
+  const instance = axios.create();
+  if (setupInterceptor) setupInterceptorsTo(instance);
+  return instance;
+};
+
+const sessionsApi = new axiosTs.SessionsApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const devicesApi = new axiosTs.DevicesApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const defaultApi = new axiosTs.DefaultApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const namespacesApi = new axiosTs.NamespacesApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const sshApi = new axiosTs.SshApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const tagsApi = new axiosTs.TagsApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const usersApi = new axiosTs.UsersApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const billingApi = new axiosTs.BillingApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const rulesApi = new axiosTs.RulesApi(
+  configuration,
+  undefined,
+  newAxiosInstance(),
+);
+const announcementApi = new axiosTs.AnnouncementsApi(
+  cloudApiConfiguration,
+  undefined,
+  newAxiosInstance(false),
+);
 
 /**
  * @deprecated This method is deprecated and no longer performs any action,
  * kept for backward compatibility but it will be removed in the future.
-**/
+ **/
 export const createNewClient = () => Function;
-
-[
-  sessionsApi,
-  devicesApi,
-  defaultApi,
-  namespacesApi,
-  sshApi,
-  tagsApi,
-  usersApi,
-  billingApi,
-  rulesApi,
-].forEach((instance) => {
-  /* eslint-disable */
-  // @ts-ignore Ignore next line because 'axios' is a protected property
-  setupInterceptorsTo(instance.axios);
-  /* eslint-enable */
-});
 
 export {
   configuration,
