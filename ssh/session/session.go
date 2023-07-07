59e783c57393837ac1a83ded79e98e207e8c9764
Henry Barreto <henry.barreto@ossystems.com.br>
2023-07-07 16:01:44 -0300

    chore(ssh): improve SSH output when device limit is exceeded

--- ssh/session/session.go
+++ ssh/session/session.go
@@ -23,7 +23,7 @@ import (
 
 // Errors returned by the NewSession to the client.
 var (
-	ErrBillingBlock       = fmt.Errorf("you cannot connect to this device because the namespace is not eligible for the free plan.\\nPlease contact the namespace owner's to upgrade the plan.\\nSee our pricing plans on https://www.shellhub.io/pricing to estimate the cost of your use cases on ShellHub Cloud or go to https://cloud.shellhub.io/settings/billing to upgrade the plan")
+	ErrBillingBlock       = fmt.Errorf("Connection to this device is not available as your current namespace doesn't qualify for the free plan. To gain access, you'll need to contact the namespace owner to initiate an upgrade.\n\nFor a detailed estimate of costs based on your use-cases with ShellHub Cloud, visit our pricing page at https://www.shellhub.io/pricing. If you wish to upgrade immediately, navigate to https://cloud.shellhub.io/settings/billing. Your cooperation is appreciated.") //nolint:all
 	ErrFirewallBlock      = fmt.Errorf("you cannot connect to this device because a firewall rule block your connection")
 	ErrFirewallConnection = fmt.Errorf("failed to communicate to the firewall")
 	ErrFirewallUnknown    = fmt.Errorf("failed to evaluate the firewall rule")
