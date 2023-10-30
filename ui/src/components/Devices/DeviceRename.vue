acce4f40ed29d7469aa51bd5567168719e7167ea
Luann Moreira <luannmcurioso@gmail.com>
2023-10-30 17:42:49 -0300

    fix(ui): device rename persisting the input on devices
    This commit is intended to address an issue where input from the Device Rename
    feature was persisting with every change in the v-text-field, even if the user
    didn't complete the changes. It also resolved the same bug that was causing
    unsaved changes in other devices to persist.

--- ui/src/components/Devices/DeviceRename.vue
+++ ui/src/components/Devices/DeviceRename.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list-item v-bind="$attrs" @click="showDialog = true">
+  <v-list-item v-bind="$attrs" @click="open()">
     <div class="d-flex align-center">
       <div class="mr-2">
         <v-icon data-test="rename-icon"> mdi-pencil </v-icon>
@@ -24,14 +24,14 @@
           :messages="messages"
           require
           variant="underlined"
-          data-test="hostname-field"
+          data-test="rename-field"
         />
       </v-card-text>
 
       <v-card-actions>
         <v-spacer />
 
-        <v-btn data-test="close-btn" variant="text" @click="showDialog = false">
+        <v-btn data-test="close-btn" variant="text" @click="close()">
           Close
         </v-btn>
 
@@ -48,8 +48,8 @@
   </v-dialog>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+<script setup lang="ts">
+import { ref, computed, watch } from "vue";
 import { useField } from "vee-validate";
 import * as yup from "yup";
 import axios, { AxiosError } from "axios";
@@ -60,79 +60,74 @@ import {
 } from "../../interfaces/INotifications";
 import handleError from "@/utils/handleError";
 
-export default defineComponent({
-  props: {
-    uid: {
-      type: String,
-      required: true,
-    },
-
-    name: {
-      type: String,
-      required: true,
-    },
-  },
-  emits: ["new-hostname"],
-  setup(props, ctx) {
-    const name = computed(() => props.name);
-    const showDialog = ref(false);
-    const messages = ref(
-      "Examples: (foobar, foo-bar-ba-z-qux, foo-example, 127-0-0-1)",
-    );
-    const store = useStore();
-
-    const {
-      value: editName,
-      errorMessage: editNameError,
-      setErrors: setEditNameError,
-    } = useField<string | undefined>("name", yup.string().required(), {
-      initialValue: name.value,
-    });
-
-    const close = () => {
-      setEditNameError("");
-      showDialog.value = false;
-    };
-
-    const rename = async () => {
-      try {
-        await store.dispatch("devices/rename", {
-          uid: props.uid,
-          name: { name: editName.value },
-        });
-
-        ctx.emit("new-hostname", editName.value);
-        close();
-        store.dispatch(
-          "snackbar/showSnackbarSuccessAction",
-          INotificationsSuccess.deviceRename,
-        );
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-          const axiosError = error as AxiosError;
-          if (axiosError.response?.status === 400) {
-            setEditNameError("nonStandardCharacters");
-          } else if (error.response?.status === 409) {
-            setEditNameError("The name already exists in the namespace");
-          }
-          handleError(error);
-        } else {
-          store.dispatch(
-            "snackbar/showSnackbarErrorAction",
-            INotificationsError.deviceRename,
-          );
-          handleError(error);
-        }
-      }
-    };
-
-    return {
-      editName,
-      editNameError,
-      messages,
-      showDialog,
-      rename,
-    };
+const props = defineProps({
+  uid: {
+    type: String,
+    required: true,
   },
 });
+const emit = defineEmits(["new-hostname"]);
+const showDialog = ref(false);
+const messages = ref(
+  "Examples: (foobar, foo-bar-ba-z-qux, foo-example, 127-0-0-1)",
+);
+const store = useStore();
+const deviceName = computed(() => store.getters["devices/getName"]);
+const {
+  value: editName,
+  errorMessage: editNameError,
+  setErrors: setEditNameError,
+} = useField<string | undefined>("name", yup.string().required(), {
+  initialValue: deviceName.value,
+});
+
+const open = () => {
+  showDialog.value = true;
+  editName.value = deviceName.value;
+};
+
+const close = () => {
+  setEditNameError("");
+  showDialog.value = false;
+};
+
+watch(showDialog, (newValue, oldValue) => {
+  if (oldValue === true && newValue === false) {
+    close();
+  }
+});
+
+const rename = async () => {
+  try {
+    await store.dispatch("devices/rename", {
+      uid: props.uid,
+      name: { name: editName.value },
+    });
+
+    emit("new-hostname", editName.value);
+    close();
+    store.dispatch(
+      "snackbar/showSnackbarSuccessAction",
+      INotificationsSuccess.deviceRename,
+    );
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.status === 400) {
+        setEditNameError("nonStandardCharacters");
+      } else if (error.response?.status === 409) {
+        setEditNameError("The name already exists in the namespace");
+      }
+      handleError(error);
+    } else {
+      store.dispatch(
+        "snackbar/showSnackbarErrorAction",
+        INotificationsError.deviceRename,
+      );
+      handleError(error);
+    }
+  }
+};
+
+defineExpose({ showDialog });
 </script>
