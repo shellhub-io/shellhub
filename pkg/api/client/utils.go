7effdc5ab076923ebf2a1719486a7ab4eb14c167
Henry Barreto <henry.barreto@ossystems.com.br>
2023-11-06 17:06:32 -0300

    feat(pkg,api): use context to control reverse listener

--- pkg/api/client/utils.go
+++ pkg/api/client/utils.go
@@ -1,12 +1,16 @@
 package client
 
 import (
+	"context"
 	"errors"
 	"net"
 	"net/http"
+	"net/url"
+	"regexp"
 	"strings"
 
 	resty "github.com/go-resty/resty/v2"
+	"github.com/gorilla/websocket"
 )
 
 func getHostname(host string) (hostname string) {
@@ -40,4 +44,37 @@ func SameDomainRedirectPolicy() resty.RedirectPolicyFunc {
 
 		return nil
 	}
+}
+
+// DialContext creates a websocket connection to ShellHub's SSH server.
+//
+// It receivees the endpoint to connect and the necessary headers for authentication on the server. If the server
+// redirect the connection with status [http.StatusTemporaryRedirect] or [http.StatusPermanentRedirect], the DialContext
+// method will follow. Any other response from the server will result in an error as result of this function.
+func DialContext(ctx context.Context, address string, header http.Header) (*websocket.Conn, *http.Response, error) {
+	parseToWS := func(uri string) string {
+		return regexp.MustCompile(`^http`).ReplaceAllString(uri, "ws")
+	}
+
+	uri, err := url.QueryUnescape(address)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	conn, res, err := websocket.DefaultDialer.DialContext(ctx, parseToWS(uri), header)
+	if err != nil {
+		switch res.StatusCode {
+		case http.StatusTemporaryRedirect, http.StatusPermanentRedirect:
+			location, err := res.Location()
+			if err != nil {
+				return nil, nil, err
+			}
+
+			return DialContext(ctx, parseToWS(location.String()), header)
+		default:
+			return nil, nil, err
+		}
+	}
+
+	return conn, res, nil
 }
