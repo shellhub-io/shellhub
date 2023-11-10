8fb253d3d0bf4d73b14a21ed0eaeec1b9dd128a4
Heitor Danilo <heitor.danilo@ossystems.com.br>
2023-11-10 11:53:20 -0300

    chore(ssh): improve log system of the session authentication

--- ssh/server/auth/password.go
+++ ssh/server/auth/password.go
@@ -1,28 +1,28 @@
 package auth
 
 import (
-	"github.com/gliderlabs/ssh"
+	gliderssh "github.com/gliderlabs/ssh"
 	"github.com/shellhub-io/shellhub/pkg/api/internalclient"
 	"github.com/shellhub-io/shellhub/ssh/pkg/metadata"
 	log "github.com/sirupsen/logrus"
 )
 
-// PasswordHandler handles ShellHub client`s connection using password authentication method.
-// PasswordHandler authentication is the second authentication method tried by the server to connect the client to agent.
-//
-// It receives the password from the client and tries to authenticate it.
-//
+// PasswordHandler handles ShellHub client's connection using the password authentication method.
+// Password authentication is the second authentication method tried by the server to connect the client to the agent.
+// It receives the password from the client and attempts to authenticate it.
 // Returns true if the password authentication method is used and false otherwise.
-// PasswordHandler authentication is last method tried by the server to connect the client to agent.
-func PasswordHandler(ctx ssh.Context, password string) bool {
+func PasswordHandler(ctx gliderssh.Context, password string) bool {
 	sshid := metadata.MaybeStoreSSHID(ctx, ctx.User())
 
-	log.WithFields(log.Fields{
-		"sshid": sshid,
-	}).Trace("trying to use password authentication")
+	log.WithFields(log.Fields{"session": ctx.SessionID(), "sshid": sshid}).
+		Trace("trying to use password authentication")
 
 	tag, err := metadata.MaybeStoreTarget(ctx, sshid)
 	if err != nil {
+		log.WithError(err).
+			WithFields(log.Fields{"session": ctx.SessionID(), "sshid": sshid}).
+			Error("failed to parse sshid to target")
+
 		return false
 	}
 
@@ -30,20 +30,27 @@ func PasswordHandler(ctx ssh.Context, password string) bool {
 
 	lookup, err := metadata.MaybeStoreLookup(ctx, tag, api)
 	if err != nil {
+		log.WithError(err).
+			WithFields(log.Fields{"session": ctx.SessionID(), "sshid": sshid}).
+			Error("failed to store lookup")
+
 		return false
 	}
 
 	_, errs := metadata.MaybeStoreDevice(ctx, lookup, api)
 	if len(errs) > 0 {
+		log.WithError(err).
+			WithFields(log.Fields{"session": ctx.SessionID(), "sshid": sshid}).
+			Error("failed to store the device")
+
 		return false
 	}
 
 	metadata.StorePassword(ctx, password)
 	metadata.StoreAuthenticationMethod(ctx, metadata.PasswordAuthenticationMethod)
 
-	log.WithFields(log.Fields{
-		"sshid": sshid,
-	}).Info("using password authentication method to connect the client to agent")
+	log.WithFields(log.Fields{"session": ctx.SessionID(), "sshid": sshid}).
+		Info("using password authentication method to connect the client to agent")
 
 	return true
 }
