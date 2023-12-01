235ad4f0421d9f083f2475e775b1cc7d5e00a3ac
Heitor Danilo <heitor.danilo@ossystems.com.br>
2023-12-01 17:59:40 -0300

    refactor(api): move worker logic to Workers struct

--- api/workers/workers.go
+++ api/workers/workers.go
@@ -1,42 +1,117 @@
 package workers
 
 import (
-	"github.com/shellhub-io/shellhub/pkg/envs"
+	"fmt"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/hibiken/asynq"
+	"github.com/shellhub-io/shellhub/api/store"
 	log "github.com/sirupsen/logrus"
 )
 
-type Envs struct {
-	MongoURI                      string `env:"MONGO_URI,default=mongodb://mongo:27017/main"`
-	RedisURI                      string `env:"REDIS_URI,default=redis://redis:6379"`
-	SessionRecordCleanupSchedule  string `env:"SESSION_RECORD_CLEANUP_SCHEDULE,default=@daily"`
-	SessionRecordCleanupRetention int    `env:"RECORD_RETENTION,default=0"`
-	// AsynqGroupMaxDelay is the maximum duration to wait before processing a group of tasks.
-	//
-	// Its time unit is second.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupMaxDelay int `env:"ASYNQ_GROUP_MAX_DELAY,default=1"`
-	// AsynqGroupGracePeriod is the grace period has configurable upper bound: you can set a maximum aggregation delay, after which Asynq server
-	// will aggregate the tasks regardless of the remaining grace period.
-	///
-	// Its time unit is second.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupGracePeriod int64 `env:"ASYNQ_GROUP_GRACE_PERIOD,default=1"`
-	// AsynqGroupMaxSize is the maximum number of tasks that can be aggregated together. If that number is reached, Asynq
-	// server will aggregate the tasks immediately.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupMaxSize int `env:"ASYNQ_GROUP_MAX_SIZE,default=500"`
+type Workers struct {
+	store store.Store
+
+	addr      asynq.RedisConnOpt
+	srv       *asynq.Server
+	mux       *asynq.ServeMux
+	env       *Envs
+	scheduler *asynq.Scheduler
 }
 
-func getEnvs() (*Envs, error) {
-	env, err := envs.ParseWithPrefix[Envs]("API_")
+// New creates a new Workers instance with the provided store. It initializes
+// the worker's components, such as server, scheduler, and environment settings.
+func New(store store.Store) (*Workers, error) {
+	env, err := getEnvs()
 	if err != nil {
-		log.WithError(err).Error("Failed to parse environment variables with prefix 'api'")
+		log.WithFields(log.Fields{"component": "worker"}).
+			WithError(err).
+			Error("Failed to parse the envs.")
 
 		return nil, err
 	}
 
-	return env, nil
+	addr, err := asynq.ParseRedisURI(env.RedisURI)
+	if err != nil {
+		log.WithFields(log.Fields{"component": "worker"}).
+			WithError(err).
+			Errorf("Failed to parse redis URI: %s.", env.RedisURI)
+
+		return nil, err
+	}
+
+	mux := asynq.NewServeMux()
+	srv := asynq.NewServer(
+		addr,
+		asynq.Config{ //nolint:exhaustruct
+			// NOTICE:
+			// To include any new task binding to a new queue (e.g., "queue:group" where 'queue' is the new queue),
+			// ensure that the created queue is added here. Failure to do so will result in the server not executing the task handler.
+			Queues: map[string]int{
+				"api":            1,
+				"session_record": 1,
+			},
+			GroupAggregator: asynq.GroupAggregatorFunc(
+				func(group string, tasks []*asynq.Task) *asynq.Task {
+					var b strings.Builder
+
+					for _, task := range tasks {
+						b.WriteString(fmt.Sprintf("%s:%d\n", task.Payload(), time.Now().Unix()))
+					}
+
+					return asynq.NewTask(TaskHeartbeat, []byte(b.String()))
+				},
+			),
+			GroupMaxDelay:    time.Duration(env.AsynqGroupMaxDelay) * time.Second,
+			GroupGracePeriod: time.Duration(env.AsynqGroupGracePeriod) * time.Second,
+			GroupMaxSize:     env.AsynqGroupMaxSize,
+			Concurrency:      runtime.NumCPU(),
+		},
+	)
+	scheduler := asynq.NewScheduler(addr, nil)
+
+	w := &Workers{
+		addr:      addr,
+		env:       env,
+		srv:       srv,
+		mux:       mux,
+		scheduler: scheduler,
+		store:     store,
+	}
+
+	return w, nil
+}
+
+// Start initiates the server. It creates two new goroutines: one for the server itself
+// and another for the scheduler. This method is also responsible for setting up all
+// the server handlers.
+func (w *Workers) Start() {
+	log.WithFields(log.Fields{"component": "worker"}).Info("Starting workers")
+
+	w.setupHandlers()
+
+	go func() {
+		if err := w.srv.Run(w.mux); err != nil {
+			log.WithFields(log.Fields{"component": "worker"}).
+				WithError(err).
+				Error("Unable to run the server.")
+		}
+	}()
+
+	go func() {
+		if err := w.scheduler.Run(); err != nil {
+			log.WithFields(log.Fields{"component": "worker"}).
+				WithError(err).
+				Error("Unable to run the scheduler.")
+		}
+	}()
+}
+
+// setupHandlers is responsible for registering all the handlers of the server. It needs
+// to be called before any initialization.
+func (w *Workers) setupHandlers() {
+	w.registerSessionCleanup()
+	w.registerHeartbeat()
 }
