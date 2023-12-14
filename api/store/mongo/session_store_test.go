bb49540cefce40e64d6b2b428359edfe71289ec2
Heitor Danilo <heitor.danilo@ossystems.com.br>
2023-12-14 18:06:50 -0300

    test(api): enhancing test coverage for session lists

--- api/store/mongo/session_store_test.go
+++ api/store/mongo/session_store_test.go
@@ -2,6 +2,7 @@ package mongo
 
 import (
 	"context"
+	"sort"
 	"testing"
 	"time"
 
@@ -23,11 +24,13 @@ func TestSessionList(t *testing.T) {
 
 	cases := []struct {
 		description string
+		page        paginator.Query
 		fixtures    []string
 		expected    Expected
 	}{
 		{
 			description: "succeeds when sessions are found",
+			page:        paginator.Query{Page: -1, PerPage: -1},
 			fixtures: []string{
 				fixtures.FixtureNamespaces,
 				fixtures.FixtureDevices,
@@ -66,15 +69,123 @@ func TestSessionList(t *testing.T) {
 							Acceptable:       false,
 						},
 						Active:        true,
-						Closed:        false,
+						Closed:        true,
 						Authenticated: true,
-						Recorded:      true,
-						Type:          "",
-						Term:          "",
+						Recorded:      false,
+						Type:          "shell",
+						Term:          "xterm",
 						Position:      models.SessionPosition{Longitude: 0, Latitude: 0},
 					},
+					{
+						StartedAt: time.Date(2023, 1, 2, 12, 0, 0, 0, time.UTC),
+						LastSeen:  time.Date(2023, 1, 2, 12, 0, 0, 0, time.UTC),
+						UID:       "e7f3a56d8b9e1dc4c285c98c8ea9c33032a17bda5b6c6b05a6213c2a02f97824",
+						DeviceUID: "2300230e3ca2f637636b4d025d2235269014865db5204b6d115386cbee89809c",
+						TenantID:  "00000000-0000-4000-0000-000000000000",
+						Username:  "john_doe",
+						IPAddress: "0.0.0.0",
+						Device: &models.Device{
+							CreatedAt:        time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							StatusUpdatedAt:  time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							LastSeen:         time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							UID:              "2300230e3ca2f637636b4d025d2235269014865db5204b6d115386cbee89809c",
+							Name:             "device-3",
+							Identity:         &models.DeviceIdentity{MAC: "mac-3"},
+							Info:             nil,
+							PublicKey:        "",
+							TenantID:         "00000000-0000-4000-0000-000000000000",
+							Online:           true,
+							Namespace:        "namespace-1",
+							Status:           "accepted",
+							RemoteAddr:       "",
+							Position:         nil,
+							Tags:             []string{"tag-1"},
+							PublicURL:        false,
+							PublicURLAddress: "",
+							Acceptable:       false,
+						},
+						Active:        false,
+						Closed:        true,
+						Authenticated: true,
+						Recorded:      true,
+						Type:          "shell",
+						Term:          "xterm",
+						Position:      models.SessionPosition{Longitude: 45.6789, Latitude: -12.3456},
+					},
+					{
+						StartedAt: time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+						LastSeen:  time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+						UID:       "fc2e1493d8b6a4c17bf6a2f7f9e55629e384b2d3a21e0c3d90f6e35b0c946178a",
+						DeviceUID: "2300230e3ca2f637636b4d025d2235269014865db5204b6d115386cbee89809c",
+						TenantID:  "00000000-0000-4000-0000-000000000000",
+						Username:  "john_doe",
+						IPAddress: "0.0.0.0",
+						Device: &models.Device{
+							CreatedAt:        time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							StatusUpdatedAt:  time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							LastSeen:         time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							UID:              "2300230e3ca2f637636b4d025d2235269014865db5204b6d115386cbee89809c",
+							Name:             "device-3",
+							Identity:         &models.DeviceIdentity{MAC: "mac-3"},
+							Info:             nil,
+							PublicKey:        "",
+							TenantID:         "00000000-0000-4000-0000-000000000000",
+							Online:           true,
+							Namespace:        "namespace-1",
+							Status:           "accepted",
+							RemoteAddr:       "",
+							Position:         nil,
+							Tags:             []string{"tag-1"},
+							PublicURL:        false,
+							PublicURLAddress: "",
+							Acceptable:       false,
+						},
+						Active:        false,
+						Closed:        true,
+						Authenticated: true,
+						Recorded:      false,
+						Type:          "exec",
+						Term:          "",
+						Position:      models.SessionPosition{Longitude: -78.9012, Latitude: 23.4567},
+					},
+					{
+						StartedAt: time.Date(2023, 1, 4, 12, 0, 0, 0, time.UTC),
+						LastSeen:  time.Date(2023, 1, 4, 12, 0, 0, 0, time.UTC),
+						UID:       "bc3d75821a29cfe70bf7986f9ee5629e384b2d3a21e0c3d90f6e35b0c946178a",
+						DeviceUID: "2300230e3ca2f637636b4d025d2235269014865db5204b6d115386cbee89809c",
+						TenantID:  "00000000-0000-4000-0000-000000000000",
+						Username:  "john_doe",
+						IPAddress: "0.0.0.0",
+						Device: &models.Device{
+							CreatedAt:        time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							StatusUpdatedAt:  time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							LastSeen:         time.Date(2023, 1, 3, 12, 0, 0, 0, time.UTC),
+							UID:              "2300230e3ca2f637636b4d025d2235269014865db5204b6d115386cbee89809c",
+							Name:             "device-3",
+							Identity:         &models.DeviceIdentity{MAC: "mac-3"},
+							Info:             nil,
+							PublicKey:        "",
+							TenantID:         "00000000-0000-4000-0000-000000000000",
+							Online:           true,
+							Namespace:        "namespace-1",
+							Status:           "accepted",
+							RemoteAddr:       "",
+							Position:         nil,
+							Tags:             []string{"tag-1"},
+							PublicURL:        false,
+							PublicURLAddress: "",
+							Acceptable:       false,
+						},
+						Active:        false,
+						Closed:        true,
+						Authenticated: true,
+						Recorded:      true,
+						Type:          "shell",
+						Term:          "xterm",
+						Position:      models.SessionPosition{Longitude: -56.7890, Latitude: 34.5678},
+					},
 				},
-				count: 1,
+				count: 4,
 				err:   nil,
 			},
 		},
@@ -86,12 +197,22 @@ func TestSessionList(t *testing.T) {
 	mongostore := NewStore(db.Client().Database("test"), cache.NewNullCache())
 	fixtures.Init(db.Host, "test")
 
+	// Due to the non-deterministic order of applying fixtures when dealing with multiple datasets,
+	// we ensure that both the expected and result arrays are correctly sorted.
+	sort := func(s []models.Session) {
+		sort.Slice(s, func(i, j int) bool {
+			return s[i].UID < s[j].UID
+		})
+	}
+
 	for _, tc := range cases {
 		t.Run(tc.description, func(t *testing.T) {
 			assert.NoError(t, fixtures.Apply(tc.fixtures...))
 			defer fixtures.Teardown() // nolint: errcheck
 
-			s, count, err := mongostore.SessionList(context.TODO(), paginator.Query{Page: -1, PerPage: -1})
+			s, count, err := mongostore.SessionList(context.TODO(), tc.page)
+			sort(tc.expected.s)
+			sort(s)
 			assert.Equal(t, tc.expected, Expected{s: s, count: count, err: err})
 		})
 	}
@@ -164,11 +285,11 @@ func TestSessionGet(t *testing.T) {
 						Acceptable:       false,
 					},
 					Active:        true,
-					Closed:        false,
+					Closed:        true,
 					Authenticated: true,
-					Recorded:      true,
-					Type:          "",
-					Term:          "",
+					Recorded:      false,
+					Type:          "shell",
+					Term:          "xterm",
 					Position:      models.SessionPosition{Longitude: 0, Latitude: 0},
 				},
 				err: nil,
@@ -447,13 +568,13 @@ func TestSessionGetRecordFrame(t *testing.T) {
 	}{
 		{
 			description: "succeeds",
-			UID:         models.UID("a3b0431f5df6a7827945d2e34872a5c781452bc36de42f8b1297fd9ecb012f68"),
+			UID:         models.UID("e7f3a56d8b9e1dc4c285c98c8ea9c33032a17bda5b6c6b05a6213c2a02f97824"),
 			fixtures:    []string{fixtures.FixtureSessions, fixtures.FixtureRecordedSessions},
 			expected: Expected{
 				r: []models.RecordedSession{
 					{
-						Time:     time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC),
-						UID:      "a3b0431f5df6a7827945d2e34872a5c781452bc36de42f8b1297fd9ecb012f68",
+						Time:     time.Date(2023, 1, 2, 12, 0, 0, 0, time.UTC),
+						UID:      "e7f3a56d8b9e1dc4c285c98c8ea9c33032a17bda5b6c6b05a6213c2a02f97824",
 						Message:  "message",
 						TenantID: "00000000-0000-4000-0000-000000000000",
 						Width:    0,
@@ -553,7 +674,7 @@ func TestSessionDeleteRecordFrame(t *testing.T) {
 		},
 		{
 			description: "succeeds when record frame is found",
-			UID:         models.UID("a3b0431f5df6a7827945d2e34872a5c781452bc36de42f8b1297fd9ecb012f68"),
+			UID:         models.UID("e7f3a56d8b9e1dc4c285c98c8ea9c33032a17bda5b6c6b05a6213c2a02f97824"),
 			fixtures:    []string{fixtures.FixtureSessions, fixtures.FixtureRecordedSessions},
 			expected:    nil,
 		},
@@ -591,7 +712,7 @@ func TestSessionDeleteRecordFrameByDate(t *testing.T) {
 	}{
 		{
 			description: "succeeds when there are no sessions to update or delete",
-			lte:         time.Date(2023, time.February, 10, 8, 30, 0, 0, time.UTC),
+			lte:         time.Date(2023, time.January, 30, 12, 00, 0, 0, time.UTC),
 			fixtures:    []string{},
 			expected: Expected{
 				deletedCount: 0,
@@ -601,14 +722,14 @@ func TestSessionDeleteRecordFrameByDate(t *testing.T) {
 		},
 		{
 			description: "succeeds to delete and update recorded sessions before specified date",
-			lte:         time.Date(2023, time.January, 2, 12, 01, 0, 0, time.UTC),
+			lte:         time.Date(2023, time.January, 30, 12, 00, 0, 0, time.UTC),
 			fixtures: []string{
 				fixtures.FixtureSessions,
 				fixtures.FixtureRecordedSessions,
 			},
 			expected: Expected{
-				deletedCount: 1,
-				updatedCount: 1,
+				deletedCount: 2,
+				updatedCount: 2,
 				err:          nil,
 			},
 		},
