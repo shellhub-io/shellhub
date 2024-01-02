a4391b6ad27fd4b1a54f02c3edd04426f642c4ec
Henry Barreto <henry.barreto@ossystems.com.br>
2024-01-02 12:59:03 -0300

    refactor(ssh): inclose tunnel routers logic inside itself

--- ssh/pkg/tunnel/tunnel.go
+++ ssh/pkg/tunnel/tunnel.go
@@ -2,6 +2,9 @@ package tunnel
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
 
@@ -14,6 +17,7 @@ import (
 type Tunnel struct {
 	Tunnel *httptunnel.Tunnel
 	API    internalclient.Client
+	router *echo.Echo
 }
 
 func NewTunnel(connection, dial string) *Tunnel {
@@ -36,17 +40,83 @@ func NewTunnel(connection, dial string) *Tunnel {
 		}
 	}
 
+	tunnel.router = tunnel.Tunnel.Router().(*echo.Echo)
+
+	// `/sessions/:uid/close` is the endpoint that is called by the agent to inform the SSH's server that the session is
+	// closed.
+	tunnel.router.POST("/sessions/:uid/close", func(c echo.Context) error {
+		var data struct {
+			UID    string `param:"uid"`
+			Device string `json:"device"`
+		}
+
+		if err := c.Bind(&data); err != nil {
+			return err
+		}
+
+		ctx := c.Request().Context()
+
+		conn, err := tunnel.Dial(ctx, data.Device)
+		if err != nil {
+			return err
+		}
+
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/ssh/close/%s", data.UID), nil)
+		if err != nil {
+			return err
+		}
+
+		if err := req.Write(conn); err != nil {
+			return err
+		}
+
+		return c.NoContent(http.StatusOK)
+	})
+
+	tunnel.router.Any("/ssh/http", func(c echo.Context) error {
+		dev, err := tunnel.API.GetDeviceByPublicURLAddress(c.Request().Header.Get("X-Public-URL-Address"))
+		if err != nil {
+			return err
+		}
+
+		if !dev.PublicURL {
+			return err
+		}
+
+		in, err := tunnel.Dial(c.Request().Context(), dev.UID)
+		if err != nil {
+			return err
+		}
+
+		defer in.Close()
+
+		if err := c.Request().Write(in); err != nil {
+			return err
+		}
+
+		ctr := http.NewResponseController(c.Response())
+		out, _, err := ctr.Hijack()
+		if err != nil {
+			return err
+		}
+
+		defer out.Close()
+		if _, err := io.Copy(out, in); errors.Is(err, io.ErrUnexpectedEOF) {
+			return err
+		}
+
+		return nil
+	})
+
+	tunnel.router.GET("/healthcheck", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+
 	return tunnel
 }
 
 func (t *Tunnel) GetRouter() *echo.Echo {
-	router, ok := t.Tunnel.Router().(*echo.Echo)
-	if !ok {
-		// TODO: should the Connect does not up when this assertion fail?
-		log.Error("type assertion failed")
-	}
-
-	return router
+	return t.router
 }
 
 func (t *Tunnel) Dial(ctx context.Context, id string) (net.Conn, error) {
