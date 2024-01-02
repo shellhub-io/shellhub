f83e46e9690a84a7825d20cc9895690415419e55
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2024-01-02 18:24:05 -0300

    feat(ssh): enable profiling in development mode

--- pkg/envs/envs.go
+++ pkg/envs/envs.go
@@ -31,15 +31,20 @@ func IsCloud() bool {
 	return DefaultBackend.Get("SHELLHUB_CLOUD") == ENABLED
 }
 
-// HasBilling returns true if the current ShellHub server instance has billing feature enabled.
-func HasBilling() bool {
-	return DefaultBackend.Get("SHELLHUB_BILLING") == ENABLED
-}
-
 // IsCommunity return true if the current ShellHub server instance is community.
 // It evaluates if the current ShellHub instance is neither enterprise or cloud .
 func IsCommunity() bool {
 	return (DefaultBackend.Get("SHELLHUB_CLOUD") != ENABLED && DefaultBackend.Get("SHELLHUB_ENTERPRISE") != ENABLED)
+}
+
+// IsDevelopment returns true if the current environment is development
+func IsDevelopment() bool {
+	return DefaultBackend.Get("SHELLHUB_ENV") == "development"
+}
+
+// HasBilling returns true if the current ShellHub server instance has billing feature enabled.
+func HasBilling() bool {
+	return DefaultBackend.Get("SHELLHUB_BILLING") == ENABLED
 }
 
 var ErrParseWithPrefix = errors.New("failed to parse environment variables for the given prefix")
--- ssh/go.mod
+++ ssh/go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/labstack/echo-contrib v0.15.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.2.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -51,7 +52,7 @@ require (
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
