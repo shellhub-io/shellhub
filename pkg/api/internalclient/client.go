6af2056ff9b9c9559a7b3216e24af642872e1726
Henry Barreto <henry.barreto@ossystems.com.br>
2024-01-04 09:19:43 -0300

    ssh(refactor): remove asynq initialization from main

--- pkg/api/internalclient/client.go
+++ pkg/api/internalclient/client.go
@@ -69,6 +69,31 @@ func NewClient(opts ...Opt) Client {
 	return c
 }
 
+// NewClientWithAsynq creates a new API internal client with the Asynq client injected to turn the API internal client
+// able to enqueue ping tasks to the Asynq server and late process by API server.
+//
+// It uses the [NewClient] function to create a new API internal client and injects the Asynq client to it through the
+// [Options] structure.
+func NewClientWithAsynq(uri string) Client {
+	return NewClient(func(o *Options) error {
+		// The internal client used by the SSH server needs to be able to enqueue tasks to the Asynq server, due that,
+		// we must set the Asynq client to the internal client as a configuration function.
+		options, err := asynq.ParseRedisURI(uri)
+		if err != nil {
+			return err
+		}
+
+		client := asynq.NewClient(options)
+		if client == nil {
+			return errors.New("failed to create Asynq client")
+		}
+
+		o.Asynq = client
+
+		return nil
+	})
+}
+
 type commonAPI interface {
 	ListDevices() ([]models.Device, error)
 	GetDevice(uid string) (*models.Device, error)
