830592386f399358cde46425de2717b0c10c1b83
Heitor Danilo <heitor.danilo@ossystems.com.br>
2024-01-10 14:28:39 -0300

    feat(api,pkg): new query filters package

--- api/store/mongo/queries/builder.go
+++ api/store/mongo/queries/builder.go
@@ -1,6 +1,7 @@
 package queries
 
 import (
+	"github.com/shellhub-io/shellhub/api/store/mongo/queries/internal"
 	"github.com/shellhub-io/shellhub/pkg/api/query"
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -38,4 +39,64 @@ func FromSorter(s *query.Sorter) []bson.M {
 			},
 		},
 	}
+}
+
+// FromFilters converts the Filters instance to a BSON filter expression for MongoDB queries.
+// Returns an error when an invalid filter is found.
+func FromFilters(fs *query.Filters) ([]bson.M, error) {
+	if len(fs.Data) < 1 {
+		return []bson.M{}, nil
+	}
+
+	queryFilter := make([]bson.M, 0)
+	queryMatcher := make([]bson.M, 0)
+
+	for _, filter := range fs.Data {
+		switch filter.Type {
+		case query.FilterTypeProperty:
+			param, ok := filter.Params.(*query.FilterProperty)
+			if !ok {
+				return nil, query.ErrFilterInvalid
+			}
+
+			prop, ok, err := internal.ParseFilterProperty(param)
+			if err != nil {
+				return nil, query.ErrFilterPropertyInvalid
+			}
+
+			if !ok {
+				continue
+			}
+
+			queryFilter = append(queryFilter, bson.M{param.Name: prop})
+		case query.FilterTypeOperator:
+			param, ok := filter.Params.(*query.FilterOperator)
+			if !ok {
+				return nil, query.ErrFilterInvalid
+			}
+
+			op, ok := internal.ParseFilterOperator(param)
+			if !ok {
+				continue
+			}
+
+			queryMatcher = append(queryMatcher, bson.M{
+				"$match": bson.M{op: queryFilter},
+			})
+
+			queryFilter = nil
+		default:
+			return nil, query.ErrFilterInvalid
+		}
+	}
+
+	if len(queryFilter) > 0 {
+		queryMatcher = []bson.M{
+			{
+				"$match": bson.M{"$or": queryFilter},
+			},
+		}
+	}
+
+	return queryMatcher, nil
 }
