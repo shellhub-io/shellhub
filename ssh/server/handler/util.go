b92eac04acfb988e641ce9a4dbff4bac84edc8b0
Heitor Danilo <heitor.danilo@ossystems.com.br>
2024-01-17 13:46:56 -0300

    chore(ssh): abstract evaluation of possible broken agents

--- ssh/server/handler/util.go
+++ ssh/server/handler/util.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/Masterminds/semver"
+	gliderssh "github.com/gliderlabs/ssh"
+	"github.com/shellhub-io/shellhub/ssh/pkg/metadata"
 	"github.com/shellhub-io/shellhub/ssh/session"
 	log "github.com/sirupsen/logrus"
 )
@@ -22,4 +25,43 @@ func writeError(sess *session.Session, msg string, iErr, eError error) {
 		Error(msg)
 
 	sess.Client.Write([]byte(fmt.Sprintf("%s\n", eError.Error()))) // nolint: errcheck
+}
+
+// evaluateContext evaluates the given context and returns an error if there's anything
+// that may cause issues during the connection.
+func evaluateContext(ctx gliderssh.Context, opts *ConfigOptions) error {
+	if !opts.AllowPublickeyAccessBelow060 {
+		return checkAgentVersionForPublicKey(ctx)
+	}
+
+	return nil
+}
+
+// checkAgentVersionForPublicKey checks if the agent's version supports public key authentication.
+//
+// Versions earlier than 0.6.0 do not validate the user when receiving a public key
+// authentication request. This implies that requests with invalid users are
+// treated as "authenticated" because the connection does not raise any error.
+// Moreover, the agent panics after the connection ends. To avoid this, connections
+// with public key are not permitted when agent version is 0.5.x or earlier
+func checkAgentVersionForPublicKey(ctx gliderssh.Context) error {
+	if metadata.RestoreAuthenticationMethod(ctx) != metadata.PublicKeyAuthenticationMethod {
+		return nil
+	}
+
+	version := metadata.RestoreDevice(ctx).Info.Version
+	if version == "latest" {
+		return nil
+	}
+
+	semverVersion, err := semver.NewVersion(version)
+	if err != nil {
+		return ErrInvalidVersion
+	}
+
+	if semverVersion.LessThan(semver.MustParse("0.6.0")) {
+		return ErrUnsuportedPublicKeyAuth
+	}
+
+	return nil
 }
---
+++ ssh/server/handler/util_test.go
@@ -0,0 +1,82 @@
+package handler
+
+import (
+	"testing"
+
+	gliderssh "github.com/gliderlabs/ssh"
+	"github.com/shellhub-io/shellhub/pkg/models"
+	"github.com/shellhub-io/shellhub/ssh/pkg/metadata"
+	metadataMocks "github.com/shellhub-io/shellhub/ssh/pkg/metadata/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckAgentVersionForPublicKey(t *testing.T) {
+	cases := []struct {
+		description   string
+		requiredMocks func(gliderssh.Context)
+		expected      error
+	}{
+		{
+			description: "succeeds when authentication method is passwod",
+			requiredMocks: func(ctx gliderssh.Context) {
+				metadataMock := new(metadataMocks.Metadata)
+				metadata.SetBackend(metadataMock)
+
+				metadataMock.On("RestoreAuthenticationMethod", ctx).
+					Return(metadata.PasswordAuthenticationMethod).
+					Once()
+			},
+			expected: nil,
+		},
+		{
+			description: "succeeds when device's version is 'latest'",
+			requiredMocks: func(ctx gliderssh.Context) {
+				metadataMock := new(metadataMocks.Metadata)
+				metadata.SetBackend(metadataMock)
+
+				metadataMock.On("RestoreAuthenticationMethod", ctx).
+					Return(metadata.PublicKeyAuthenticationMethod).
+					Once()
+
+				metadataMock.On("RestoreDevice", ctx).
+					Return(&models.Device{
+						Info: &models.DeviceInfo{
+							Version: "latest",
+						},
+					}).
+					Once()
+			},
+			expected: nil,
+		},
+		{
+			description: "fails when device's version is 0.5.x or earlier and authentication method is PUBLIC KEY",
+			requiredMocks: func(ctx gliderssh.Context) {
+				metadataMock := new(metadataMocks.Metadata)
+				metadata.SetBackend(metadataMock)
+
+				metadataMock.On("RestoreAuthenticationMethod", ctx).
+					Return(metadata.PublicKeyAuthenticationMethod).
+					Once()
+
+				metadataMock.On("RestoreDevice", ctx).
+					Return(&models.Device{
+						Info: &models.DeviceInfo{
+							Version: "0.5.2",
+						},
+					}).
+					Once()
+			},
+			expected: ErrUnsuportedPublicKeyAuth,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.description, func(t *testing.T) {
+			ctx := new(gliderssh.Context)
+			tc.requiredMocks(*ctx)
+
+			err := checkAgentVersionForPublicKey(*ctx)
+			assert.Equal(t, tc.expected, err)
+		})
+	}
+}
