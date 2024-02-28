7b4bbc5c3572fdea607fd3b03c17bf0f425a4299
Luann Moreira <luannmcurioso@gmail.com>
2024-02-28 08:57:20 -0300

    feat(ui): alert for private keys privacy policy
    This commit adds a alert on the SettingsPrivateKeys with an v-alert
    that shows the way ShellHub handles Private Keys.

--- ui/src/components/Setting/SettingPrivateKeys.vue
+++ ui/src/components/Setting/SettingPrivateKeys.vue
@@ -2,15 +2,36 @@
   <v-container>
     <v-row align="center" justify="center" class="mt-4">
       <v-col sm="8">
-        <v-card class="pb-0 elevation-0">
-          <div class="d-flex pa-0 align-center bg-v-theme-surface">
-            <v-spacer />
-            <v-spacer />
+        <v-alert
+          class="text-subtitle-2"
+          type="info"
+          title="ShellHub does not save your private keys"
+          text="Your private keys remain confidential and are not shared with the ShellHub server.
+            Instead, they are securely stored in your browser's local storage."
+          color="primary"
+          variant="tonal"
+          data-test="privacy-policy-alert"
+        />
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center" class="mt-4">
+      <v-col sm="8">
+        <v-card class="pb-0 elevation-0 bg-v-theme-surface">
+          <v-row>
+            <v-col>
+              <h3>Private Keys</h3>
+            </v-col>
+            <v-col>
+              <div class="d-flex pa-0 align-center bg-v-theme-surface">
+                <v-spacer />
+                <v-spacer />
 
-            <span>
-              <PrivateKeyAdd data-test="privateKeyFormDialogFirst-component" />
-            </span>
-          </div>
+                <span>
+                  <PrivateKeyAdd data-test="privateKeyFormDialogFirst-component" />
+                </span>
+              </div>
+            </v-col>
+          </v-row>
 
           <PrivateKeyList />
         </v-card>
@@ -19,17 +40,7 @@
   </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import PrivateKeyAdd from "../PrivateKeys/PrivateKeyAdd.vue";
 import PrivateKeyList from "../PrivateKeys/PrivateKeyList.vue";
-
-export default defineComponent({
-  setup() {
-    return {
-      //
-    };
-  },
-  components: { PrivateKeyAdd, PrivateKeyList },
-});
 </script>
