dcc258c61500552728b0dd7e3aaa9548a715f670
Luann Moreira <luannmcurioso@gmail.com>
2024-02-28 08:57:20 -0300

    feat(ui): alert for private keys privacy policy
    This commit adds a alert on the SettingsPrivateKeys with an v-alert
    that shows the way ShellHub handles Private Keys.

---
+++ ui/tests/components/PrivateKeys/SettingsPrivateKeys.spec.ts
@@ -0,0 +1,101 @@
+import { createVuetify } from "vuetify";
+import { mount, VueWrapper } from "@vue/test-utils";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import MockAdapter from "axios-mock-adapter";
+import SettingPrivateKeys from "@/components/Setting/SettingPrivateKeys.vue";
+import { namespacesApi, usersApi } from "@/api/http";
+import { store, key } from "@/store";
+import { router } from "@/router";
+import { envVariables } from "@/envVariables";
+import { SnackbarPlugin } from "@/plugins/snackbar";
+
+type SettingPrivateKeysWrapper = VueWrapper<InstanceType<typeof SettingPrivateKeys>>;
+
+describe("Setting Private Keys", () => {
+  let wrapper: SettingPrivateKeysWrapper;
+
+  const vuetify = createVuetify();
+
+  let mockNamespace: MockAdapter;
+
+  let mockUser: MockAdapter;
+
+  const members = [
+    {
+      id: "xxxxxxxx",
+      username: "test",
+      role: "owner",
+    },
+  ];
+
+  const namespaceData = {
+    name: "test",
+    owner: "test",
+    tenant_id: "fake-tenant-data",
+    members,
+    settings: {
+      session_record: true,
+    },
+    max_devices: 3,
+    devices_count: 3,
+    created_at: "",
+  };
+
+  const authData = {
+    status: "success",
+    token: "",
+    user: "test",
+    name: "test",
+    tenant: "fake-tenant-data",
+    email: "test@test.com",
+    id: "xxxxxxxx",
+    role: "owner",
+    mfa: {
+      enable: false,
+      validate: false,
+    },
+  };
+
+  const session = true;
+
+  beforeEach(async () => {
+    vi.useFakeTimers();
+    localStorage.setItem("tenant", "fake-tenant-data");
+    envVariables.isCloud = true;
+
+    mockNamespace = new MockAdapter(namespacesApi.getAxios());
+    mockUser = new MockAdapter(usersApi.getAxios());
+
+    mockNamespace.onGet("http://localhost:3000/api/namespaces/fake-tenant-data").reply(200, namespaceData);
+    mockUser.onGet("http://localhost:3000/api/users/security").reply(200, session);
+    mockUser.onGet("http://localhost:3000/api/auth/user").reply(200, authData);
+
+    store.commit("auth/authSuccess", authData);
+    store.commit("auth/changeData", authData);
+    store.commit("namespaces/setNamespace", namespaceData);
+    store.commit("security/setSecurity", session);
+
+    wrapper = mount(SettingPrivateKeys, {
+      global: {
+        plugins: [[store, key], vuetify, router, SnackbarPlugin],
+      },
+    });
+  });
+
+  it("Is a Vue instance", () => {
+    expect(wrapper.vm).toBeTruthy();
+  });
+
+  it("Renders the component", () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("Data is defined", () => {
+    expect(wrapper.vm.$data).toBeDefined();
+  });
+
+  it("Renders components", async () => {
+    expect(wrapper.find('[data-test="privateKeyFormDialogFirst-component"]').exists());
+    expect(wrapper.find('[data-test="privacy-policy-alert"]').exists()).toBe(true);
+  });
+});
