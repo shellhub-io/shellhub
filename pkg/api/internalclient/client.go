e7e054e525ba16c4542cd72d54250eec9d92b1bb
Heitor Danilo <heitor.danilo@ossystems.com.br>
2024-03-01 10:21:52 -0300

    Revert "chore(api,gateway): bind a random port on api startup"
    This reverts commit f3091c39538d36903001140fa3912da7237e24a6.

--- pkg/api/internalclient/client.go
+++ pkg/api/internalclient/client.go
@@ -50,6 +50,7 @@ type Opt func(*Options) error
 
 func NewClient(opts ...Opt) Client {
 	httpClient := resty.New()
+	httpClient.SetBaseURL("http://api:8080")
 	httpClient.SetRetryCount(math.MaxInt32)
 	httpClient.AddRetryCondition(func(r *resty.Response, err error) bool {
 		if _, ok := err.(net.Error); ok { // if the error is a network error, retry.
@@ -58,9 +59,6 @@ func NewClient(opts ...Opt) Client {
 
 		return r.StatusCode() >= http.StatusInternalServerError && r.StatusCode() != http.StatusNotImplemented
 	})
-
-	env, _ := getEnvs()
-	httpClient.SetBaseURL("http://api:" + env.ServerPort)
 
 	c := &client{http: httpClient}
 
