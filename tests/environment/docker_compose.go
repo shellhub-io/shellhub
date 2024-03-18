acb9f14807cc1448ffd4b501a25ea7444bcfcb39
Heitor Danilo <heitor.danilo@ossystems.com.br>
2024-03-18 09:44:51 -0300

    feat(tests): new framework for integration tests
    The environment framework provides an easy way to create, manage and
    destroy ShellHub instances. with Docker Compose. Internally, it uses
    `testcontainers-go` to manage services.
    
    To get started, call `New`, which creates a new
    `DockerComposeConfigurator`. A configurator is a helper struct to manage
    "docker-composes". By default, a new configurator reads from `.env` to
    set up all environment variables. To avoid boilerplate between test
    cases a clone of a configurator can be made; a clone has the same data
    as the original configurator but is an isolated pointer. Every
    configurator is associated with a `testing.T`, which is used to make
    standard assertations.
    
    To start the instance, you can call `DockerComposeConfigurator.Up`,
    which returns a `DockerCompose`. A Docker Compose is a code
    representation of the running instance; it also has a
    `DockerCompose.Down` method, which stops and cleans up all allocated
    resources for the instance. Generally, it is passed to
    `testing.T.Cleanup`.
    
    The running instance provides helper methods to facilitate
    docker-compose manipulation and comunication. It also provides helper
    methods for generic pipelines (e.g., creating a user).

---
+++ tests/environment/docker_compose.go
@@ -0,0 +1,141 @@
+package environment
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/shellhub-io/shellhub/pkg/models"
+	"github.com/stretchr/testify/assert"
+	tc "github.com/testcontainers/testcontainers-go"
+)
+
+type DockerCompose struct {
+	// t is the [testing.T] associated with the [DockerCompose] instance. It is used for
+	// making assertions.
+	t *testing.T
+
+	// services is a list of running services such as API and CLI.
+	services map[Service]*tc.DockerContainer
+
+	// client is a HTTP client with "http://localhost:{SHELLHUB_HTTP_PORT}" as the base URL.
+	client *resty.Client
+
+	// envs is a map containing all environment variables passed to the services.
+	envs map[string]string
+
+	// down is a function designed to be invoked internally within [Down] method calls. This
+	// attribute is necessary because when initializing docker-compose with testcontainer,
+	// the returned value is of a private type, rendering it inaccessible for passing as a
+	// function parameter, for example. Consequently, we construct the Down method within
+	// the Up method, encapsulating it within an attribute and invoking it within a method.
+	down func()
+}
+
+// Down stops the [DockerCompose] instance, removing images, services, networks, and volumes
+// associated with it. It's generally a good idea to encapsulate it inside a [t.Cleanup]
+// function.
+func (dc *DockerCompose) Down() {
+	dc.down()
+}
+
+// R return a [resty.R] with `http://localhost:{SHELLHUB_HTTP_PORT}` as base URL.
+func (dc *DockerCompose) R(ctx context.Context) *resty.Request {
+	dc.t.Helper()
+
+	return dc.client.R().SetContext(ctx)
+}
+
+// Env retrieves a environment variable with the specified key.
+func (dc *DockerCompose) Env(key string) string {
+	dc.t.Helper()
+
+	return dc.envs[key]
+}
+
+// Service retrieves the specified service.
+func (dc *DockerCompose) Service(service Service) *tc.DockerContainer {
+	dc.t.Helper()
+
+	return dc.services[service]
+}
+
+// NewUser creates a new user with the specified values. It is an abstraction around the "user create" method
+// of the CLI.
+//
+// It is not intended to be a test of the method, but it makes some assertions to guarantee that the following
+// instructions will not fail, calling assert.FailNow if any do.
+func (dc *DockerCompose) NewUser(ctx context.Context, username, email, password string) {
+	dc.t.Helper()
+
+	exitCode, result, err := dc.
+		Service(ServiceCLI).
+		Exec(ctx, []string{"./cli", "user", "create", username, password, email})
+
+	if !assert.NoError(dc.t, err) {
+		assert.FailNow(dc.t, err.Error())
+	}
+
+	if !assert.Equal(dc.t, 0, exitCode) {
+		assert.FailNow(dc.t, "cli user create exited with a non-zero status")
+	}
+
+	if !assert.Contains(dc.t, ReaderToString(dc.t, result), fmt.Sprintf("\nUsername: %s\nEmail: %s\n", username, email)) {
+		assert.FailNow(dc.t, "cli user create exited with a non-expected result")
+	}
+}
+
+// NewNamespace creates a new namespace with the specified values. It is an abstraction around the "namespace
+// create" method of the CLI.
+//
+// It is not intended to be a test of the method, but it makes some assertions to guarantee that the following
+// instructions will not fail, calling assert.FailNow if any do.
+func (dc *DockerCompose) NewNamespace(ctx context.Context, owner, name, tenant string) {
+	dc.t.Helper()
+
+	exitCode, result, err := dc.
+		Service(ServiceCLI).
+		Exec(ctx, []string{"./cli", "namespace", "create", name, owner, tenant})
+
+	if !assert.NoError(dc.t, err) {
+		assert.FailNow(dc.t, err.Error())
+	}
+
+	if !assert.Equal(dc.t, 0, exitCode) {
+		assert.FailNow(dc.t, "cli namsepace create exited with a non-zero status")
+	}
+
+	if !assert.Contains(dc.t, ReaderToString(dc.t, result), fmt.Sprintf("Namespace created successfully\nNamespace: %s\nTenant: %s\nOwner:", name, tenant)) {
+		assert.FailNow(dc.t, "cli namespace create exited with a non-expected result")
+	}
+}
+
+// AuthUser logs in with the provided username and password. It is an abstraction around the "/api/login"
+// endpoint.
+//
+// It is not intended to be a test of the endpoint, but it makes some assertions to guarantee that the following
+// instructions will not fail, calling assert.FailNow if any do.
+func (dc *DockerCompose) AuthUser(ctx context.Context, username, password string) *models.UserAuthResponse {
+	dc.t.Helper()
+
+	auth := new(models.UserAuthResponse)
+
+	res, err := dc.R(ctx).
+		SetBody(map[string]string{
+			"username": username,
+			"password": password,
+		}).
+		SetResult(auth).
+		Post("/api/login")
+
+	if !assert.NoError(dc.t, err) {
+		assert.FailNow(dc.t, err.Error())
+	}
+
+	if !assert.Equal(dc.t, 200, res.StatusCode()) {
+		assert.FailNow(dc.t, "login fails")
+	}
+
+	return auth
+}
---
+++ tests/environment/utils.go
@@ -0,0 +1,54 @@
+package environment
+
+import (
+	"io"
+	"net"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Service string
+
+const (
+	ServiceGateway Service = "gateway"
+	ServiceAgent   Service = "agent"
+	ServiceAPI     Service = "api"
+	ServiceCLI     Service = "cli"
+	ServiceSSH     Service = "ssh"
+	ServiceUI      Service = "ui"
+)
+
+// getFreePort returns a randomly available TCP port. It can be used to avoid
+// network conflicts in Docker Compose.
+func getFreePort(t *testing.T) string {
+	t.Helper()
+
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, err.Error())
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, err.Error())
+	}
+	defer l.Close()
+
+	return strconv.Itoa(l.Addr().(*net.TCPAddr).Port)
+}
+
+func ReaderToString(t *testing.T, reader io.Reader) string {
+	t.Helper()
+
+	buf := new(strings.Builder)
+
+	_, err := io.Copy(buf, reader)
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, err.Error())
+	}
+
+	return buf.String()
+}
