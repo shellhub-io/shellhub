2bfa847afaf65a5e3e79c54b7c10a21110061ef9
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2024-04-10 10:42:57 -0300

    fix(agent): resolve segfault caused by non-thread-safe crypt function
    Fixed a segfault issue caused by the use of the non-thread-safe crypt
    function by replacing it with the thread-safe crypt_r function.

--- pkg/agent/pkg/yescrypt/yescript.go
+++ pkg/agent/pkg/yescrypt/yescript.go
@@ -13,10 +13,11 @@ import "unsafe"
 
 // Verify verifies a yescrypt hash against a given key.
 func Verify(key, hash string) bool {
+	cdata := C.struct_crypt_data{}
 	ckey := C.CString(key)
 	chash := C.CString(hash)
 
-	out := C.crypt(ckey, chash)
+	out := C.crypt_r(ckey, chash, &cdata)
 
 	C.free(unsafe.Pointer(ckey))
 	C.free(unsafe.Pointer(chash))
