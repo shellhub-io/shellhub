6a227523099092585164b6586b7826f0ee4e137c
CHANGEME <CHANGEME>
2024-06-14 11:49:27 -0300

    api: add tests for api/pkg/gateway

---
+++ api/pkg/gateway/context_test.go
@@ -0,0 +1,67 @@
+package gateway
+
+import (
+	"testing"
+	"context"
+	"github.com/labstack/echo/v4"
+	"github.com/shellhub-io/shellhub/pkg/models"
+)
+
+func TestNewContext(t *testing.T) {
+
+}
+
+func TestService(t *testing.T) {
+
+
+}
+
+func TestRole(t *testing.T) {
+
+
+}
+
+func TestTenant(t *testing.T) {
+
+
+}
+
+func TestUsername(t *testing.T) {
+
+
+}
+
+func TestID(t *testing.T) {
+
+
+}
+
+func TestCtx(t *testing.T) {
+
+
+}
+
+func TestGetID(t *testing.T) {
+
+
+}
+
+func TestGetTennat(t *testing.T) {
+
+
+}
+
+func TestGetUsername(t *testing.T) {
+
+	/*
+	req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/sshkeys/public-keys/%s", tc.query.Fingerprint), nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Role", guard.RoleOwner)
+	req.Header.Set("X-Tenant-ID", "tenant")
+	*/
+}
+
+func TestGetRole(t *testing.T) {
+
+
+}
---
+++ api/pkg/gateway/gateway_test.go
@@ -0,0 +1,21 @@
+package gateway
+
+import (
+	"testing"
+)
+
+
+func TestFromContext(t *testing.T) {
+
+
+}
+
+func TestUsernameFromContext(t *testing.T) {
+
+
+}
+
+func TestIDFromContext(t *testing.T) {
+
+
+}
