dff2b99d4f7beb1c92d9844166e9dbbcc3c39c02
Heitor Danilo <heitor.danilo@ossystems.com.br>
2024-08-05 19:30:46 -0300

    refactor(api): server initialization
    Introducing a new struct `Server` to manage server start and shutdown.

--- api/server.go
+++ api/server.go
@@ -2,99 +2,37 @@ package main
 
 import (
 	"context"
-	"errors"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/getsentry/sentry-go"
+	"github.com/labstack/echo/v4"
 	"github.com/shellhub-io/shellhub/api/routes"
 	"github.com/shellhub-io/shellhub/api/services"
-	"github.com/shellhub-io/shellhub/api/store"
 	"github.com/shellhub-io/shellhub/api/store/mongo"
-	"github.com/shellhub-io/shellhub/api/store/mongo/options"
-	requests "github.com/shellhub-io/shellhub/pkg/api/internalclient"
-	storecache "github.com/shellhub-io/shellhub/pkg/cache"
+	mongooptions "github.com/shellhub-io/shellhub/api/store/mongo/options"
+	"github.com/shellhub-io/shellhub/pkg/api/internalclient"
+	"github.com/shellhub-io/shellhub/pkg/cache"
 	"github.com/shellhub-io/shellhub/pkg/geoip"
-	workerlib "github.com/shellhub-io/shellhub/pkg/worker"
+	"github.com/shellhub-io/shellhub/pkg/worker"
 	"github.com/shellhub-io/shellhub/pkg/worker/asynq"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
 )
 
-var serverCmd = &cobra.Command{
-	Use: "server",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx, cancel := context.WithCancel(cmd.Context())
-
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-		cfg, ok := ctx.Value("cfg").(*config)
-		if !ok {
-			log.Fatal("Failed to retrieve environment config from context")
-		}
-
-		log.Trace("Connecting to Redis")
-
-		cache, err := storecache.NewRedisCache(cfg.RedisURI, cfg.RedisCachePoolSize)
-		if err != nil {
-			log.WithError(err).Error("Failed to configure redis store cache")
-		}
-
-		log.Info("Connected to Redis")
-
-		log.Trace("Connecting to MongoDB")
-
-		_, db, err := mongo.Connect(ctx, cfg.MongoURI)
-		if err != nil {
-			log.
-				WithError(err).
-				Fatal("unable to connect to MongoDB")
-		}
-
-		store, err := mongo.NewStore(ctx, db, cache, options.RunMigatrions)
-		if err != nil {
-			log.
-				WithError(err).
-				Fatal("failed to create the store")
-		}
-
-		log.Info("Connected to MongoDB")
-
-		go func() {
-			sig := <-sigs
-
-			log.WithFields(log.Fields{
-				"signal": sig,
-			}).Info("signal received to terminate API")
-
-			cancel()
-		}()
-
-		return startServer(ctx, cfg, store, cache)
-	},
-}
-
-// Provides the configuration for the API service.
-// The values are load from the system environment variables.
-type config struct {
-	// MongoDB connection string (URI format)
+type env struct {
+	// MongoURI is the mongodb connection uri.
 	MongoURI string `env:"MONGO_URI,default=mongodb://mongo:27017/main"`
-	// Redis connection string (URI format)
+	// RedisURI is the redis connection uri.
 	RedisURI string `env:"REDIS_URI,default=redis://redis:6379"`
 	// RedisCachePoolSize is the pool size of connections available for Redis cache.
 	RedisCachePoolSize int `env:"REDIS_CACHE_POOL_SIZE,default=0"`
-	// Enable GeoIP feature.
+	// GeoIP defines whether the GeoIP feature is enabled or not.
 	//
 	// GeoIP features enable the ability to get the logitude and latitude of the client from the IP address.
 	// The feature is disabled by default. To enable it, it is required to have a `MAXMIND` database license and feed it
 	// to `SHELLHUB_MAXMIND_LICENSE` with it, and `SHELLHUB_GEOIP=true`.
 	GeoIP               bool   `env:"GEOIP,default=false"`
 	GeoIPMaxMindLicense string `env:"MAXMIND_LICENSE,default="`
-	// Session record cleanup worker schedule
-	// Sentry DSN.
-	SentryDSN string `env:"SENTRY_DSN,default="`
+	SentryDSN           string `env:"SENTRY_DSN,default="`
 	// AsynqGroupMaxDelay is the maximum duration to wait before processing a group of tasks.
 	//
 	// Its time unit is second.
@@ -117,95 +55,114 @@ type config struct {
 	SessionRecordCleanupRetention int    `env:"RECORD_RETENTION,default=0"`
 }
 
-// startSentry initializes the Sentry client.
-//
-// The Sentry client is used to report errors to the Sentry server, and is initialized only if the `SHELLHUB_SENTRY_DSN`
-// environment variable is set. Else, the function returns a error with a not initialized Sentry client.
-func startSentry(dsn string) (*sentry.Client, error) {
-	if dsn != "" {
-		var err error
-		reporter, err := sentry.NewClient(sentry.ClientOptions{ //nolint:exhaustruct
-			Dsn:              dsn,
-			Release:          os.Getenv("SHELLHUB_VERSION"),
-			EnableTracing:    true,
-			TracesSampleRate: 1,
-		})
-		if err != nil {
-			log.WithError(err).Error("Failed to create Sentry client")
-
-			return nil, err
-		}
-		log.Info("Sentry client started")
-
-		return reporter, nil
-	}
-
-	return nil, errors.New("sentry DSN not provided")
+type Server struct {
+	env    *env
+	router *echo.Echo // TODO: evaluate if we can create a custom struct in router (e.g. router.Router)
+	worker worker.Server
 }
 
-func startServer(ctx context.Context, cfg *config, store store.Store, cache storecache.Cache) error {
-	log.Info("Starting API server")
+func (s *Server) Setup(ctx context.Context, env *env) error {
+	s.env = env
 
-	requestClient, err := requests.NewClient()
+	cache, err := cache.NewRedisCache(s.env.RedisURI, s.env.RedisCachePoolSize)
 	if err != nil {
 		log.WithError(err).
-			Fatal("failed to create the internalclient")
+			Error("Failed to configure redis store cache")
+
+		return err
+	}
+
+	_, db, err := mongo.Connect(ctx, s.env.MongoURI)
+	if err != nil {
+		log.WithError(err).
+			Error("unable to connect to MongoDB")
+
+		return err
+	}
+
+	store, err := mongo.NewStore(ctx, db, cache, mongooptions.RunMigatrions)
+	if err != nil {
+		log.WithError(err).
+			Error("failed to create the store")
+
+		return err
+	}
+
+	apiClient, err := internalclient.NewClient()
+	if err != nil {
+		log.WithError(err).
+			Error("failed to create the api client")
+
+		return err
 	}
 
 	servicesOptions := []services.Option{}
-
-	if cfg.GeoIP {
-		log.Info("GeoIP feature is enable")
-
-		locator, err := geoip.NewGeoLite2(cfg.GeoIPMaxMindLicense)
+	if s.env.GeoIP {
+		locator, err := geoip.NewGeoLite2(s.env.GeoIPMaxMindLicense)
 		if err != nil {
-			log.WithError(err).Fatal("Failed to init GeoIP")
+			log.WithError(err).
+				Error("failed to init GeoIP")
+
+			return err
 		}
 
 		servicesOptions = append(servicesOptions, services.WithLocator(locator))
 	}
 
-	service := services.NewService(store, nil, nil, cache, requestClient, servicesOptions...)
+	service := services.NewService(store, nil, nil, cache, apiClient, servicesOptions...)
+
+	s.worker = asynq.NewServer(s.env.RedisURI)
+	s.worker.HandleTask(services.TaskDevicesHeartbeat, service.DevicesHeartbeat(), asynq.BatchTask())
+	if s.env.SessionRecordCleanupRetention > 0 {
+		s.worker.HandleCron(worker.CronSpec(s.env.SessionRecordCleanupSchedule), service.CleanupSessions(s.env.SessionRecordCleanupRetention))
+	}
 
 	routerOptions := []routes.Option{}
+	if s.env.SentryDSN != "" {
+		opts := sentry.ClientOptions{ //nolint:exhaustruct
+			Dsn:              s.env.SentryDSN,
+			Release:          os.Getenv("SHELLHUB_VERSION"),
+			EnableTracing:    true,
+			TracesSampleRate: 1,
+		}
 
-	if cfg.SentryDSN != "" {
-		log.Info("Sentry report is enabled")
-
-		reporter, err := startSentry(cfg.SentryDSN)
+		reporter, err := sentry.NewClient(opts)
 		if err != nil {
-			log.WithField("DSN", cfg.SentryDSN).WithError(err).Warn("Failed to start Sentry")
-		} else {
-			log.Info("Sentry client started")
+			log.WithError(err).
+				Error("Failed to create Sentry client")
+
+			return err
 		}
 
 		routerOptions = append(routerOptions, routes.WithReporter(reporter))
 	}
 
-	worker := asynq.NewServer(cfg.RedisURI, asynq.BatchConfig(cfg.AsynqGroupMaxSize, cfg.AsynqGroupMaxDelay, int(cfg.AsynqGroupGracePeriod)))
-	worker.HandleTask(services.TaskDevicesHeartbeat, service.DevicesHeartbeat(), asynq.BatchTask())
-	if cfg.SessionRecordCleanupRetention > 0 {
-		worker.HandleCron(workerlib.CronSpec(cfg.SessionRecordCleanupSchedule), service.CleanupSessions(cfg.SessionRecordCleanupRetention))
-	}
-
-	if err := worker.Start(); err != nil {
-		log.WithError(err).
-			Fatal("failed to start the worker")
-	}
-
-	router := routes.NewRouter(service, routerOptions...)
-
-	go func() {
-		<-ctx.Done()
-
-		log.Debug("Closing HTTP server due context cancellation")
-
-		worker.Shutdown()
-		router.Close()
-	}()
-
-	err = router.Start(":8080") //nolint:errcheck
-	log.WithError(err).Info("HTTP server closed")
+	s.router = routes.NewRouter(service, routerOptions...)
 
 	return nil
+}
+
+// Start starts the server. It will block the routine.
+func (s *Server) Start() error {
+	if err := s.worker.Start(); err != nil {
+		log.WithError(err).
+			Error("Failed to start workers.")
+
+		return err
+	}
+
+	if err := s.router.Start(":8080"); err != nil {
+		log.WithError(err).
+			Error("Failed to start router.")
+
+		return err
+	}
+
+	return nil
+}
+
+// Shutdown gracefully shuts down the server.
+func (s *Server) Shutdown() {
+	s.worker.Shutdown()
+	s.router.Close() // nolint: errcheck
 }
