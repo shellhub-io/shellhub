de8f6bde207f9009ae2b7fd996ee1c3c884d6041
Heitor Danilo <heitor.danilo@ossystems.com.br>
2024-09-09 02:26:40 -0300

    refactor(api): server initialization
    - Introducing a new struct `Server` to manage server start and shutdown.
    - Move environment variables to a `Server` attribute.
    - Move server shutdown to `main.go` and removed useless
      `ContextWithCancel`.

--- api/main.go
+++ api/main.go
@@ -2,29 +2,67 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/shellhub-io/shellhub/pkg/envs"
 	"github.com/shellhub-io/shellhub/pkg/loglevel"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	loglevel.SetLogLevel()
+	loglevel.UseEnvs()
 
 	rootCmd := &cobra.Command{Use: "api"}
+	rootCmd.AddCommand(&cobra.Command{
+		Use: "server",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			server := new(Server)
 
-	rootCmd.AddCommand(serverCmd)
+			env, err := envs.ParseWithPrefix[env]("API_")
+			if err != nil {
+				log.WithError(err).
+					Error("Failed to load environment variables")
 
-	// Populates configuration based on environment variables prefixed with 'API_'.
-	cfg, err := envs.ParseWithPrefix[config]("API_")
-	if err != nil {
-		logrus.WithError(err).Fatal("Failed to load environment variables")
-	}
+				return err
+			}
 
-	ctx := context.WithValue(context.TODO(), "cfg", cfg) //nolint:revive
+			if err := server.Setup(cmd.Context(), env); err != nil {
+				log.WithError(err).
+					Error("failed to setup the server")
 
-	if err := rootCmd.ExecuteContext(ctx); err != nil {
-		logrus.Fatal(err)
+				return err
+			}
+
+			sigs := make(chan os.Signal, 1)
+			signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+			go func() {
+				sig := <-sigs
+				log.WithField("signal", sig).
+					Info("shutting down the server")
+
+				server.Shutdown()
+				os.Exit(0)
+			}()
+
+			if err := server.Start(); err != nil {
+				log.WithError(err).
+					Error("failed too start the server")
+
+				return err
+			}
+
+			return nil
+		},
+	})
+
+	if err := rootCmd.ExecuteContext(context.Background()); err != nil {
+		log.WithError(err).
+			Error("failed to execute command")
+
+		os.Exit(1)
 	}
 }
--- api/server.go
+++ api/server.go
@@ -2,88 +2,32 @@ package main
 
 import (
 	"context"
-	"errors"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/getsentry/sentry-go"
+	"github.com/labstack/echo/v4"
 	"github.com/shellhub-io/shellhub/api/routes"
 	"github.com/shellhub-io/shellhub/api/services"
-	"github.com/shellhub-io/shellhub/api/store"
 	"github.com/shellhub-io/shellhub/api/store/mongo"
-	"github.com/shellhub-io/shellhub/api/store/mongo/options"
+	mongooptions "github.com/shellhub-io/shellhub/api/store/mongo/options"
 	"github.com/shellhub-io/shellhub/pkg/api/internalclient"
-	storecache "github.com/shellhub-io/shellhub/pkg/cache"
+	"github.com/shellhub-io/shellhub/pkg/cache"
 	"github.com/shellhub-io/shellhub/pkg/geoip"
+	"github.com/shellhub-io/shellhub/pkg/worker"
 	"github.com/shellhub-io/shellhub/pkg/worker/asynq"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
 )
 
-var serverCmd = &cobra.Command{
-	Use: "server",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx, cancel := context.WithCancel(cmd.Context())
-
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-		cfg, ok := ctx.Value("cfg").(*config)
-		if !ok {
-			log.Fatal("Failed to retrieve environment config from context")
-		}
-
-		log.Trace("Connecting to Redis")
-
-		cache, err := storecache.NewRedisCache(cfg.RedisURI, cfg.RedisCachePoolSize)
-		if err != nil {
-			log.WithError(err).Error("Failed to configure redis store cache")
-		}
-
-		log.Info("Connected to Redis")
-
-		log.Trace("Connecting to MongoDB")
-
-		_, db, err := mongo.Connect(ctx, cfg.MongoURI)
-		if err != nil {
-			log.
-				WithError(err).
-				Fatal("unable to connect to MongoDB")
-		}
-
-		store, err := mongo.NewStore(ctx, db, cache, options.RunMigatrions)
-		if err != nil {
-			log.
-				WithError(err).
-				Fatal("failed to create the store")
-		}
-
-		log.Info("Connected to MongoDB")
-
-		go func() {
-			sig := <-sigs
-
-			log.WithFields(log.Fields{
-				"signal": sig,
-			}).Info("signal received to terminate API")
-
-			cancel()
-		}()
-
-		return startServer(ctx, cfg, store, cache)
-	},
-}
-
-// Provides the configuration for the API service.
-// The values are load from the system environment variables.
-type config struct {
-	// MongoDB connection string (URI format)
+type env struct {
+	// MongoDB connection string.
 	MongoURI string `env:"MONGO_URI,default=mongodb://mongo:27017/main"`
-	// Redis connection string (URI format)
+
+	// Redis connection string.
 	RedisURI string `env:"REDIS_URI,default=redis://redis:6379"`
+
 	// RedisCachePoolSize is the pool size of connections available for Redis cache.
 	RedisCachePoolSize int `env:"REDIS_CACHE_POOL_SIZE,default=0"`
+
 	// Enable GeoIP feature.
 	//
 	// GeoIP features enable the ability to get the logitude and latitude of the client from the IP address.
@@ -91,115 +35,141 @@ type config struct {
 	// to `SHELLHUB_MAXMIND_LICENSE` with it, and `SHELLHUB_GEOIP=true`.
 	GeoIP               bool   `env:"GEOIP,default=false"`
 	GeoIPMaxMindLicense string `env:"MAXMIND_LICENSE,default="`
+
 	// Session record cleanup worker schedule
 	// Sentry DSN.
 	SentryDSN string `env:"SENTRY_DSN,default="`
-	// AsynqGroupMaxDelay is the maximum duration to wait before processing a group of tasks.
-	//
-	// Its time unit is second.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupMaxDelay int `env:"ASYNQ_GROUP_MAX_DELAY,default=1"`
-	// AsynqGroupGracePeriod is the grace period has configurable upper bound: you can set a maximum aggregation delay, after which Asynq server
-	// will aggregate the tasks regardless of the remaining grace period.
-	///
-	// Its time unit is second.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupGracePeriod int64 `env:"ASYNQ_GROUP_GRACE_PERIOD,default=1"`
-	// AsynqGroupMaxSize is the maximum number of tasks that can be aggregated together. If that number is reached, Asynq
-	// server will aggregate the tasks immediately.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupMaxSize int `env:"ASYNQ_GROUP_MAX_SIZE,default=500"`
 }
 
-// startSentry initializes the Sentry client.
-//
-// The Sentry client is used to report errors to the Sentry server, and is initialized only if the `SHELLHUB_SENTRY_DSN`
-// environment variable is set. Else, the function returns a error with a not initialized Sentry client.
-func startSentry(dsn string) (*sentry.Client, error) {
-	if dsn != "" {
-		var err error
-		reporter, err := sentry.NewClient(sentry.ClientOptions{ //nolint:exhaustruct
-			Dsn:              dsn,
+type Server struct {
+	env    *env
+	router *echo.Echo // TODO: evaluate if we can create a custom struct in router (e.g. router.Router)
+	worker worker.Server
+}
+
+func (s *Server) Setup(ctx context.Context, env *env) error {
+	s.env = env
+
+	cache, err := cache.NewRedisCache(s.env.RedisURI, s.env.RedisCachePoolSize)
+	if err != nil {
+		log.WithError(err).
+			Error("Failed to configure redis store cache")
+
+		return err
+	}
+
+	_, db, err := mongo.Connect(ctx, s.env.MongoURI)
+	if err != nil {
+		log.WithError(err).
+			Error("unable to connect to MongoDB")
+
+		return err
+	}
+
+	store, err := mongo.NewStore(ctx, db, cache, mongooptions.RunMigatrions)
+	if err != nil {
+		log.WithError(err).
+			Error("failed to create the store")
+
+		return err
+	}
+
+	apiClient, err := internalclient.NewClient(internalclient.WithAsynqWorker(env.RedisURI))
+	if err != nil {
+		log.WithError(err).
+			Error("failed to create the api client")
+
+		return err
+	}
+
+	servicesOptions, err := s.serviceOptions()
+	if err != nil {
+		log.WithError(err).
+			Error("failed to create the service's options")
+
+		return err
+	}
+
+	service := services.NewService(store, nil, nil, cache, apiClient, servicesOptions...)
+
+	s.worker = asynq.NewServer(s.env.RedisURI)
+	s.worker.HandleTask(services.TaskDevicesHeartbeat, service.DevicesHeartbeat(), asynq.BatchTask())
+
+	routerOptions, err := s.routerOptions()
+	if err != nil {
+		log.WithError(err).
+			Error("failed to create the router's options")
+
+		return err
+	}
+
+	s.router = routes.NewRouter(service, routerOptions...)
+
+	return nil
+}
+
+// Start starts the server. It will block the routine.
+func (s *Server) Start() error {
+	if err := s.worker.Start(); err != nil {
+		log.WithError(err).
+			Error("Failed to start workers.")
+
+		return err
+	}
+
+	if err := s.router.Start(":8080"); err != nil {
+		log.WithError(err).
+			Error("Failed to start router.")
+
+		return err
+	}
+
+	return nil
+}
+
+// Shutdown gracefully shuts down the server.
+func (s *Server) Shutdown() {
+	s.worker.Shutdown()
+	s.router.Close() // nolint: errcheck
+}
+
+// serviceOptions returns a list of options used to start the services.
+func (s *Server) serviceOptions() ([]services.Option, error) {
+	opts := []services.Option{}
+
+	if s.env.GeoIP {
+		locator, err := geoip.NewGeoLite2(s.env.GeoIPMaxMindLicense)
+		if err != nil {
+			return []services.Option{}, err
+		}
+
+		opts = append(opts, services.WithLocator(locator))
+	}
+
+	return opts, nil
+}
+
+// routerOptions returns a list of options used to start the services.
+func (s *Server) routerOptions() ([]routes.Option, error) {
+	opts := []routes.Option{}
+
+	if s.env.SentryDSN != "" {
+		sentryOpts := sentry.ClientOptions{ //nolint:exhaustruct
+			Dsn:              s.env.SentryDSN,
 			Release:          os.Getenv("SHELLHUB_VERSION"),
 			EnableTracing:    true,
 			TracesSampleRate: 1,
-		})
+		}
+
+		reporter, err := sentry.NewClient(sentryOpts)
 		if err != nil {
 			log.WithError(err).Error("Failed to create Sentry client")
 
 			return nil, err
 		}
-		log.Info("Sentry client started")
 
-		return reporter, nil
+		opts = append(opts, routes.WithReporter(reporter))
 	}
 
-	return nil, errors.New("sentry DSN not provided")
-}
-
-func startServer(ctx context.Context, cfg *config, store store.Store, cache storecache.Cache) error {
-	log.Info("Starting API server")
-
-	apiClient, err := internalclient.NewClient(internalclient.WithAsynqWorker(cfg.RedisURI))
-	if err != nil {
-		log.WithError(err).
-			Fatal("failed to create the internalclient")
-	}
-
-	servicesOptions := []services.Option{}
-
-	if cfg.GeoIP {
-		log.Info("GeoIP feature is enable")
-
-		locator, err := geoip.NewGeoLite2(cfg.GeoIPMaxMindLicense)
-		if err != nil {
-			log.WithError(err).Fatal("Failed to init GeoIP")
-		}
-
-		servicesOptions = append(servicesOptions, services.WithLocator(locator))
-	}
-
-	service := services.NewService(store, nil, nil, cache, apiClient, servicesOptions...)
-
-	routerOptions := []routes.Option{}
-
-	if cfg.SentryDSN != "" {
-		log.Info("Sentry report is enabled")
-
-		reporter, err := startSentry(cfg.SentryDSN)
-		if err != nil {
-			log.WithField("DSN", cfg.SentryDSN).WithError(err).Warn("Failed to start Sentry")
-		} else {
-			log.Info("Sentry client started")
-		}
-
-		routerOptions = append(routerOptions, routes.WithReporter(reporter))
-	}
-
-	worker := asynq.NewServer(cfg.RedisURI, asynq.BatchConfig(cfg.AsynqGroupMaxSize, cfg.AsynqGroupMaxDelay, int(cfg.AsynqGroupGracePeriod)))
-	worker.HandleTask(services.TaskDevicesHeartbeat, service.DevicesHeartbeat(), asynq.BatchTask())
-
-	if err := worker.Start(); err != nil {
-		log.WithError(err).
-			Fatal("failed to start the worker")
-	}
-
-	router := routes.NewRouter(service, routerOptions...)
-
-	go func() {
-		<-ctx.Done()
-
-		log.Debug("Closing HTTP server due context cancellation")
-
-		worker.Shutdown()
-		router.Close()
-	}()
-
-	err = router.Start(":8080") //nolint:errcheck
-	log.WithError(err).Info("HTTP server closed")
-
-	return nil
+	return opts, nil
 }
