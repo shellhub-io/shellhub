4dd809a04cc2252d289148599a240358ead3da83
Heitor Danilo <heitor.danilo@ossystems.com.br>
2024-09-17 16:57:37 -0300

    fix(api): default asynq values

--- api/server.go
+++ api/server.go
@@ -107,15 +107,24 @@ type config struct {
 	SessionRecordCleanupSchedule string `env:"SESSION_RECORD_CLEANUP_SCHEDULE,default=@daily"`
 	// Sentry DSN.
 	SentryDSN string `env:"SENTRY_DSN,default="`
-}
-
-func init() {
-	if value, ok := os.LookupEnv("SHELLHUB_ENV"); ok && value == "development" {
-		log.SetLevel(log.TraceLevel)
-		log.Debug("Log level set to Trace")
-	} else {
-		log.Debug("Log level default")
-	}
+	// AsynqGroupMaxDelay is the maximum duration to wait before processing a group of tasks.
+	//
+	// Its time unit is second.
+	//
+	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
+	AsynqGroupMaxDelay int `env:"ASYNQ_GROUP_MAX_DELAY,default=1"`
+	// AsynqGroupGracePeriod is the grace period has configurable upper bound: you can set a maximum aggregation delay, after which Asynq server
+	// will aggregate the tasks regardless of the remaining grace period.
+	///
+	// Its time unit is second.
+	//
+	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
+	AsynqGroupGracePeriod int64 `env:"ASYNQ_GROUP_GRACE_PERIOD,default=2"`
+	// AsynqGroupMaxSize is the maximum number of tasks that can be aggregated together. If that number is reached, Asynq
+	// server will aggregate the tasks immediately.
+	//
+	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
+	AsynqGroupMaxSize int `env:"ASYNQ_GROUP_MAX_SIZE,default=1000"`
 }
 
 // startSentry initializes the Sentry client.
