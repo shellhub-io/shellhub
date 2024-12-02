dc766edea1eba749553cc6c77c8c1ced7bf62d4a
haller33 <eliseu42lucena@gmail.com>
2024-12-02 09:55:14 -0300

    feat: tags collection redesign

--- api/store/mongo/device_tags.go
+++ api/store/mongo/device_tags.go
@@ -6,19 +6,54 @@ import (
 	"github.com/shellhub-io/shellhub/api/store"
 	"github.com/shellhub-io/shellhub/pkg/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 )
 
 func (s *Store) DevicePushTag(ctx context.Context, uid models.UID, tag string) error {
-	t, err := s.db.Collection("devices").UpdateOne(ctx, bson.M{"uid": uid}, bson.M{"$push": bson.M{"tags": tag}})
+	session, err := s.db.Client().StartSession()
 	if err != nil {
 		return FromMongoError(err)
 	}
+	defer session.EndSession(ctx)
 
-	if t.ModifiedCount < 1 {
-		return store.ErrNoDocuments
-	}
+	_, erro := session.WithTransaction(ctx, func(sessCtx mongo.SessionContext) (interface{}, error) {
+		device := new(models.Device)
 
-	return nil
+		err := s.db.Collection("devices", options.Collection().SetWriteConcern(writeconcern.Majority())).
+			FindOne(sessCtx, bson.M{"uid": uid}).
+			Decode(device)
+		if err != nil {
+			return nil, FromMongoError(err)
+		}
+
+		exist, err := s.VerifyTagExist(sessCtx, tag, device.TenantID)
+		if err != nil {
+			return nil, FromMongoError(err)
+		}
+
+		if !exist {
+			err := s.TagsPushTag(sessCtx, tag, device.TenantID)
+			if err != nil {
+				return nil, FromMongoError(err)
+			}
+		}
+
+		t, err := s.db.Collection("devices", options.Collection().SetWriteConcern(writeconcern.Majority())).
+			UpdateOne(sessCtx, bson.M{"uid": uid}, bson.M{"$push": bson.M{"tags": tag}})
+		if err != nil {
+			return nil, FromMongoError(err)
+		}
+
+		if t.ModifiedCount < 1 {
+			return nil, store.ErrNoDocuments
+		}
+
+		return nil, nil
+	})
+
+	return erro
 }
 
 func (s *Store) DevicePullTag(ctx context.Context, uid models.UID, tag string) error {
@@ -35,19 +70,59 @@ func (s *Store) DevicePullTag(ctx context.Context, uid models.UID, tag string) e
 }
 
 func (s *Store) DeviceSetTags(ctx context.Context, uid models.UID, tags []string) (int64, int64, error) {
-	tag, err := s.db.Collection("devices").UpdateOne(ctx, bson.M{"uid": uid}, bson.M{"$set": bson.M{"tags": tags}})
+	tag, err := s.db.Collection("devices").
+		UpdateOne(ctx, bson.M{"uid": uid}, bson.M{"$set": bson.M{"tags": tags}})
 
 	return tag.MatchedCount, tag.ModifiedCount, FromMongoError(err)
 }
 
 func (s *Store) DeviceBulkRenameTag(ctx context.Context, tenant, currentTag, newTag string) (int64, error) {
-	res, err := s.db.Collection("devices").UpdateMany(ctx, bson.M{"tenant_id": tenant, "tags": currentTag}, bson.M{"$set": bson.M{"tags.$": newTag}})
+	res, err := s.db.Collection("devices").
+		UpdateMany(ctx, bson.M{"tenant_id": tenant, "tags": currentTag}, bson.M{"$set": bson.M{"tags.$": newTag}})
 
 	return res.ModifiedCount, FromMongoError(err)
 }
 
+func (s *Store) TagsBulkRenameTag(ctx context.Context, tenant, currentTag, newTag string) (int64, error) {
+	mongoSession, err := s.db.Client().StartSession()
+	if err != nil {
+		return 0, FromMongoError(err)
+	}
+	defer mongoSession.EndSession(ctx)
+
+	res := int64(0)
+
+	_, err2 := mongoSession.WithTransaction(ctx, func(sessCtx mongo.SessionContext) (interface{}, error) {
+		tag := new(models.Tags)
+
+		err := s.db.Collection("tags", options.Collection().SetWriteConcern(writeconcern.Majority())).
+			FindOne(sessCtx, bson.M{"tenant_id": tenant}).Decode(tag)
+		if err != nil {
+			return 0, FromMongoError(err)
+		}
+
+		res, err := s.TagsBulkDeleteTag(sessCtx, tenant, currentTag)
+		if err != nil {
+			return res, FromMongoError(err)
+		}
+
+		tag.Name = newTag
+
+		_, err2 := s.db.Collection("tags", options.Collection().SetWriteConcern(writeconcern.Majority())).
+			InsertOne(sessCtx, tag)
+		if err2 != nil {
+			return res, FromMongoError(err2)
+		}
+
+		return res, FromMongoError(err2)
+	})
+
+	return res, err2
+}
+
 func (s *Store) DeviceBulkDeleteTag(ctx context.Context, tenant, tag string) (int64, error) {
-	res, err := s.db.Collection("devices").UpdateMany(ctx, bson.M{"tenant_id": tenant}, bson.M{"$pull": bson.M{"tags": tag}})
+	res, err := s.db.Collection("devices").
+		UpdateMany(ctx, bson.M{"tenant_id": tenant}, bson.M{"$pull": bson.M{"tags": tag}})
 
 	return res.ModifiedCount, FromMongoError(err)
 }
