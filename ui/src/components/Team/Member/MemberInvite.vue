3d0315b1ca9d15b16f129cfe7b660c51c4d8d8e7
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2025-01-18 19:10:41 -0300

    style(ui): update new member dialog layout

--- ui/src/components/Team/Member/MemberInvite.vue
+++ ui/src/components/Team/Member/MemberInvite.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <v-tooltip location="bottom" class="text-center" :disabled="hasAuthorization()">
+    <v-tooltip
+      location="bottom"
+      class="text-center"
+      :disabled="hasAuthorization()"
+    >
       <template v-slot:activator="{ props }">
         <div v-bind="props">
           <v-btn
@@ -16,13 +20,63 @@
       <span> You don't have this kind of authorization. </span>
     </v-tooltip>
 
-    <v-dialog v-model="dialog" max-width="450" @click:outside="close()">
-      <v-card data-test="namespaceNewMember-dialog" class="bg-v-theme-surface">
-        <v-card-title class="bg-primary"> Invite member </v-card-title>
+    <v-dialog
+      v-model="dialog"
+      max-width="450"
+      @click:outside="close()"
+    >
+      <v-card
+        data-test="namespaceNewMember-dialog"
+        class="bg-v-theme-surface"
+      >
+        <div class="mt-4 mb-4">
+          <div class="d-flex justify-center align-center bg-whitea">
+            <v-avatar
+              class="-right-32 z-0"
+              size="46"
+            >
+              <svg v-html="getAvatar(0)" />
+            </v-avatar>
+
+            <v-avatar
+              class="-right-16 z-1"
+              size="56"
+            >
+              <svg v-html="getAvatar(1)" />
+            </v-avatar>
+
+            <v-avatar
+              class="z-2"
+              size="72"
+              color="primary"
+            >
+              <v-icon color="white">mdi-account</v-icon>
+            </v-avatar>
+
+            <v-avatar
+              class="-left-16 z-1"
+              size="56"
+            >
+              <svg v-html="getAvatar(2)" />
+            </v-avatar>
+            <v-avatar
+              class="-left-32 z-0"
+              size="46"
+            >
+              <svg v-html="getAvatar(3)" />
+            </v-avatar>
+          </div>
+        </div>
+        <v-card-title class="text-center">
+          Invite Member
+        </v-card-title>
 
         <v-card-text>
-          <p class="text-caption text-grey-lighten-4 mb-1" v-if="envVariables.isCloud">
-            If this email isn't associated with an existing account, we'll send  an email to sign-up.
+          <p
+            class="text-caption text-grey-lighten-4 mb-1"
+            v-if="envVariables.isCloud"
+          >
+            If this email isn't associated with an existing account, we'll send an email to sign-up.
           </p>
           <v-text-field
             v-model="email"
@@ -48,9 +102,16 @@
 
         <v-card-actions>
           <v-spacer />
-          <v-btn data-test="close-btn" @click="close()"> Close </v-btn>
+          <v-btn
+            data-test="close-btn"
+            @click="close()"
+          > Close </v-btn>
 
-          <v-btn color="primary" data-test="invite-btn" @click="addMember()">
+          <v-btn
+            color="primary"
+            data-test="invite-btn"
+            @click="addMember()"
+          >
             Invite
           </v-btn>
         </v-card-actions>
@@ -64,6 +125,7 @@ import { ref } from "vue";
 import { useField } from "vee-validate";
 import * as yup from "yup";
 import axios, { AxiosError } from "axios";
+import multiavatar from "@multiavatar/multiavatar/esm";
 import hasPermission from "@/utils/permission";
 import { useStore } from "@/store";
 import { actions, authorizer } from "@/authorizer";
@@ -124,6 +186,8 @@ const hasErrors = () => {
   return false;
 };
 
+const getAvatar = (index: number) => multiavatar(`${Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - index + 1)) + index}`);
+
 const resetFields = () => {
   resetIdentifier();
   resetSelectedRole();
@@ -178,3 +242,21 @@ const addMember = async () => {
 
 defineExpose({ emailError });
 </script>
+
+<style lang="scss" scoped>
+@for $i from 0 through 32 {
+  .-left-#{$i} {
+    left: -#{$i}px;
+  }
+
+  .-right-#{$i} {
+    right: -#{$i}px;
+  }
+}
+
+@for $i from 0 through 2 {
+  .z-#{$i} {
+    z-index: #{$i};
+  }
+}
+</style>
