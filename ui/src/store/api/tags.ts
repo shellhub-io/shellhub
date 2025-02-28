ad8687af301562076eb66ec31e9e0f3aaeb19525
Luann Moreira <luann.curioso@ossystems.com.br>
2025-02-28 10:38:35 -0300

    refactor(ui): handle new tags objects
    This commit refactor all tags object with new handling capabilities, using
    pagination on lists and a new refactored TagFormUpdate on the device and
    container associated tags.

--- ui/src/store/api/tags.ts
+++ ui/src/store/api/tags.ts
@@ -1,12 +1,39 @@
-import { tagsApi } from "@/api/http";
+import { UpdateTagRequest } from "@/api/client";
+import { tagsApi } from "../../api/http";
 
-interface UpdateTagResponse {
-  oldTag: string;
-  newTag: string;
-}
+export const createTag = async (tenant: string, name: string) => tagsApi.createTag(tenant, { name });
 
-export const updateTag = async (data : UpdateTagResponse) => tagsApi.renameTag(data.oldTag, { tag: data.newTag });
+export const updateTag = async (
+  tenant: string,
+  currentName: string,
+  newName: UpdateTagRequest,
+) => tagsApi.updateTag(tenant, currentName, newName);
 
-export const removeTag = async (tag : string) => tagsApi.deleteTag(tag);
+export const removeTag = async (tenant: string, currentName: string) => tagsApi.deleteTag(tenant, currentName);
 
-export const getTags = async () => tagsApi.getTags();
+export const removeTagFromDevice = async (tenant: string, uid: string, name: string) => tagsApi.pullTagFromDevice(tenant, uid, name);
+
+export const removeTagFromFirewallRule = async (tenant: string, name: string) => tagsApi.pullTagFromFirewallRule(tenant, name);
+
+export const removeTagFromPublicKey = async (
+  tenant: string,
+  fingerprint: string,
+  name: string,
+) => tagsApi.pullTagFromPublicKey(tenant, fingerprint, name);
+
+export const pushTagToDevice = async (tenant: string, uid: string, name: string) => tagsApi.pushTagToDevice(tenant, uid, name);
+
+export const pushTagToFirewallRule = async (tenant: string, name: string) => tagsApi.pushTagToFirewallRule(tenant, name);
+
+export const pushTagToPublicKey = async (
+  tenant: string,
+  fingerprint: string,
+  name: string,
+) => tagsApi.pushTagToPublicKey(tenant, fingerprint, name);
+
+export const getTags = async (
+  tenant: string,
+  filter : string | undefined,
+  page : number,
+  perPage: number,
+) => tagsApi.getTags(tenant, filter, page, perPage);
