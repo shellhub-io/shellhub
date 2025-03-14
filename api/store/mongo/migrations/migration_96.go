9f39fef907617cdb788b247458d6f3cfe00dbd46
Henry Barreto <henry.barreto@ossystems.com.br>
2025-03-14 18:03:46 -0300

    feature(api,pkg): add migrations to convert recorded sesisons into Asciinema
    To be able to use our recorded sessions as Asciinema files, we migrated
    registers from this collection into session's events', turning them into
    events of a session, as others SSH request types, deleting the older
    collection, `recorded_sessions`, from the database. With events, we
    can sort and process them easily into the Asciinema records, as we can
    obtain all data needed in one single collection.

---
+++ api/store/mongo/migrations/migration_96.go
@@ -0,0 +1,49 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/shellhub-io/shellhub/pkg/envs"
+	log "github.com/sirupsen/logrus"
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var migration96 = migrate.Migration{
+	Version:     96,
+	Description: "Drops the recorded_session collection",
+	Up: migrate.MigrationFunc(func(ctx context.Context, db *mongo.Database) error {
+		log.WithFields(log.Fields{
+			"component": "migration",
+			"version":   96,
+			"action":    "Up",
+		}).Info("Applying migration")
+
+		if !envs.IsEnterprise() {
+			return nil
+		}
+
+		if err := db.Collection("recorded_sessions").Drop(ctx); err != nil {
+			return err
+		}
+
+		return nil
+	}),
+	Down: migrate.MigrationFunc(func(ctx context.Context, db *mongo.Database) error {
+		log.WithFields(log.Fields{
+			"component": "migration",
+			"version":   96,
+			"action":    "Down",
+		}).Info("Reverting migration")
+
+		if !envs.IsEnterprise() {
+			return nil
+		}
+
+		if err := db.CreateCollection(ctx, "recorded_sessions"); err != nil {
+			return err
+		}
+
+		return nil
+	}),
+}
