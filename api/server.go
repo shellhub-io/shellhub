2f0e054f1bffc02a8489df98535cacedc8ed796b
Heitor Danilo <heitor.danilo@ossystems.com.br>
2025-03-17 15:33:40 -0300

    refactor(api): server initialization

--- api/server.go
+++ api/server.go
@@ -2,219 +2,193 @@ package main
 
 import (
 	"context"
-	"errors"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/getsentry/sentry-go"
+	"github.com/labstack/echo/v4"
 	"github.com/shellhub-io/shellhub/api/routes"
 	"github.com/shellhub-io/shellhub/api/services"
-	"github.com/shellhub-io/shellhub/api/store"
 	"github.com/shellhub-io/shellhub/api/store/mongo"
 	"github.com/shellhub-io/shellhub/api/store/mongo/options"
 	"github.com/shellhub-io/shellhub/pkg/api/internalclient"
-	storecache "github.com/shellhub-io/shellhub/pkg/cache"
+	"github.com/shellhub-io/shellhub/pkg/cache"
 	"github.com/shellhub-io/shellhub/pkg/geoip/geolite2"
+	"github.com/shellhub-io/shellhub/pkg/worker"
 	"github.com/shellhub-io/shellhub/pkg/worker/asynq"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
 )
 
-var serverCmd = &cobra.Command{
-	Use: "server",
-	RunE: func(cmd *cobra.Command, _ []string) error {
-		ctx, cancel := context.WithCancel(cmd.Context())
-
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-		cfg, ok := ctx.Value("cfg").(*config)
-		if !ok {
-			log.Fatal("Failed to retrieve environment config from context")
-		}
-
-		log.Trace("Connecting to Redis")
-
-		cache, err := storecache.NewRedisCache(cfg.RedisURI, cfg.RedisCachePoolSize)
-		if err != nil {
-			log.WithError(err).Error("Failed to configure redis store cache")
-		}
-
-		log.Info("Connected to Redis")
-
-		log.Trace("Connecting to MongoDB")
-
-		store, err := mongo.NewStore(ctx, cfg.MongoURI, cache, options.RunMigatrions)
-		if err != nil {
-			log.
-				WithError(err).
-				Fatal("failed to create the store")
-		}
-
-		log.Info("Connected to MongoDB")
-
-		go func() {
-			sig := <-sigs
-
-			log.WithFields(log.Fields{
-				"signal": sig,
-			}).Info("signal received to terminate API")
-
-			cancel()
-		}()
-
-		return startServer(ctx, cfg, store, cache)
-	},
-}
-
-// Provides the configuration for the API service.
-// The values are load from the system environment variables.
-type config struct {
-	// MongoDB connection string (URI format)
+type env struct {
+	// MongoURI specifies the connection string for MongoDB.
 	MongoURI string `env:"MONGO_URI,default=mongodb://mongo:27017/main"`
-	// Redis connection string (URI format)
-	RedisURI string `env:"REDIS_URI,default=redis://redis:6379"`
-	// RedisCachePoolSize is the pool size of connections available for Redis cache.
-	RedisCachePoolSize int `env:"REDIS_CACHE_POOL_SIZE,default=0"`
-	// Session record cleanup worker schedule
-	// Sentry DSN.
-	SentryDSN string `env:"SENTRY_DSN,default="`
-	// AsynqGroupMaxDelay is the maximum duration to wait before processing a group of tasks.
-	//
-	// Its time unit is second.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupMaxDelay int `env:"ASYNQ_GROUP_MAX_DELAY,default=1"`
-	// AsynqGroupGracePeriod is the grace period has configurable upper bound: you can set a maximum aggregation delay, after which Asynq server
-	// will aggregate the tasks regardless of the remaining grace period.
-	///
-	// Its time unit is second.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupGracePeriod int64 `env:"ASYNQ_GROUP_GRACE_PERIOD,default=2"`
-	// AsynqGroupMaxSize is the maximum number of tasks that can be aggregated together. If that number is reached, Asynq
-	// server will aggregate the tasks immediately.
-	//
-	// Check [https://github.com/hibiken/asynq/wiki/Task-aggregation] for more information.
-	AsynqGroupMaxSize int `env:"ASYNQ_GROUP_MAX_SIZE,default=1000"`
 
-	// AsynqUniquenessTimeout defines the maximum duration, in hours, for which a unique job
-	// remains locked in the queue. If the job does not complete within this timeout, the lock
-	// is released, allowing a new instance of the job to be enqueued and executed.
+	// RedisURI specifies the connection string for Redis.
+	RedisURI string `env:"REDIS_URI,default=redis://redis:6379"`
+	// RedisCachePoolSize defines the maximum number of concurrent connections to Redis cache.
+	// Set to 0 for unlimited connections.
+	RedisCachePoolSize int `env:"REDIS_CACHE_POOL_SIZE,default=0"`
+
+	// SentryDSN specifies the Data Source Name for Sentry error tracking.
+	// Leave empty to disable Sentry integration.
+	SentryDSN string `env:"SENTRY_DSN,default="`
+
+	// AsynqGroupMaxDelay specifies the maximum time (in seconds) to wait before
+	// processing a group of tasks, regardless of other conditions.
+	AsynqGroupMaxDelay int `env:"ASYNQ_GROUP_MAX_DELAY,default=1"`
+	// AsynqGroupGracePeriod defines the grace period (in seconds) before task aggregation.
+	// Tasks arriving within this period will be aggregated with existing tasks in the group.
+	AsynqGroupGracePeriod int64 `env:"ASYNQ_GROUP_GRACE_PERIOD,default=2"`
+	// AsynqGroupMaxSize specifies the maximum number of tasks that can be aggregated in a group.
+	// When this limit is reached, the group will be processed immediately.
+	AsynqGroupMaxSize int `env:"ASYNQ_GROUP_MAX_SIZE,default=1000"`
+	// AsynqUniquenessTimeout defines how long (in hours) a unique job remains locked in the queue.
+	// If a job doesn't complete within this period, its lock is released, allowing a new instance
+	// to be enqueued and executed.
 	AsynqUniquenessTimeout int `env:"ASYNQ_UNIQUENESS_TIMEOUT,default=24"`
 
-	// GeoipMirror specifies an alternative mirror URL for downloading the GeoIP databases.
-	// This field takes precedence over [GeoipMaxmindLicense]; when both are configured,
-	// GeoipMirror will be used as the primary source for database downloads.
+	// GeoipMirror specifies an alternative URL for downloading GeoIP databases.
+	// When configured, this takes precedence over GeoipMaxmindLicense.
 	GeoipMirror string `env:"MAXMIND_MIRROR,default="`
-
-	// GeoipMaxmindLicense is the MaxMind license key used to authenticate requests for
-	// downloading the GeoIP database directly from MaxMind. If [GeoipMirror] is not set,
-	// this license key will be used as the fallback method for fetching the database.
+	// GeoipMaxmindLicense is the MaxMind license key for downloading GeoIP databases directly.
+	// This is used as a fallback when GeoipMirror is not configured.
 	GeoipMaxmindLicense string `env:"MAXMIND_LICENSE,default="`
 }
 
-// startSentry initializes the Sentry client.
-//
-// The Sentry client is used to report errors to the Sentry server, and is initialized only if the `SHELLHUB_SENTRY_DSN`
-// environment variable is set. Else, the function returns a error with a not initialized Sentry client.
-func startSentry(dsn string) (*sentry.Client, error) {
-	if dsn != "" {
-		var err error
-		reporter, err := sentry.NewClient(sentry.ClientOptions{ //nolint:exhaustruct
-			Dsn:              dsn,
-			Release:          os.Getenv("SHELLHUB_VERSION"),
-			EnableTracing:    true,
-			TracesSampleRate: 1,
-		})
-		if err != nil {
-			log.WithError(err).Error("Failed to create Sentry client")
-
-			return nil, err
-		}
-		log.Info("Sentry client started")
-
-		return reporter, nil
-	}
-
-	return nil, errors.New("sentry DSN not provided")
+type Server struct {
+	env    *env
+	router *echo.Echo // TODO: evaluate if we can create a custom struct in router (e.g. router.Router)
+	worker worker.Server
 }
 
-func startServer(ctx context.Context, cfg *config, store store.Store, cache storecache.Cache) error {
-	log.Info("Starting API server")
+// Setup initializes all server components including database connections, cache, services, API routes, and background workers.
+// It prepares the server for starting but does not actually begin serving requests.
+func (s *Server) Setup(ctx context.Context) error {
+	log.Info("Setting up server components")
 
-	apiClient, err := internalclient.NewClient(internalclient.WithAsynqWorker(cfg.RedisURI))
+	cache, err := cache.NewRedisCache(s.env.RedisURI, s.env.RedisCachePoolSize)
 	if err != nil {
-		log.WithError(err).
-			Fatal("failed to create the internalclient")
+		return err
 	}
 
-	servicesOptions := []services.Option{}
+	log.Debug("Redis cache initialized successfully")
 
-	var fetcher geolite2.GeoliteFetcher
-
-	switch {
-	case cfg.GeoipMirror != "":
-		fetcher = geolite2.FetchFromMirror(cfg.GeoipMirror)
-	case cfg.GeoipMaxmindLicense != "":
-		fetcher = geolite2.FetchFromLicenseKey(cfg.GeoipMaxmindLicense)
+	store, err := mongo.NewStore(ctx, s.env.MongoURI, cache, options.RunMigatrions)
+	if err != nil {
+		log.
+			WithError(err).
+			Fatal("failed to create the store")
 	}
 
-	if fetcher != nil {
-		locator, err := geolite2.NewLocator(ctx, fetcher)
-		if err != nil {
-			log.WithError(err).Fatal("Failed to init GeoIP")
-		}
+	log.Debug("MongoDB store connected successfully")
 
-		servicesOptions = append(servicesOptions, services.WithLocator(locator))
+	apiClient, err := internalclient.NewClient(internalclient.WithAsynqWorker(s.env.RedisURI))
+	if err != nil {
+		return err
+	}
 
-		log.Info("GeoIP feature is enable")
+	servicesOptions, err := s.serviceOptions(ctx)
+	if err != nil {
+		return err
+	}
+
+	routerOptions, err := s.routerOptions()
+	if err != nil {
+		return err
 	}
 
 	service := services.NewService(store, nil, nil, cache, apiClient, servicesOptions...)
+	s.router = routes.NewRouter(service, routerOptions...)
 
-	routerOptions := []routes.Option{}
-
-	if cfg.SentryDSN != "" {
-		log.Info("Sentry report is enabled")
-
-		reporter, err := startSentry(cfg.SentryDSN)
-		if err != nil {
-			log.WithField("DSN", cfg.SentryDSN).WithError(err).Warn("Failed to start Sentry")
-		} else {
-			log.Info("Sentry client started")
-		}
-
-		routerOptions = append(routerOptions, routes.WithReporter(reporter))
-	}
-
-	worker := asynq.NewServer(
-		cfg.RedisURI,
-		asynq.BatchConfig(cfg.AsynqGroupMaxSize, cfg.AsynqGroupMaxDelay, int(cfg.AsynqGroupGracePeriod)),
-		asynq.UniquenessTimeout(cfg.AsynqUniquenessTimeout),
+	s.worker = asynq.NewServer(
+		s.env.RedisURI,
+		asynq.BatchConfig(s.env.AsynqGroupMaxSize, s.env.AsynqGroupMaxDelay, int(s.env.AsynqGroupGracePeriod)),
+		asynq.UniquenessTimeout(s.env.AsynqUniquenessTimeout),
 	)
 
-	worker.HandleTask(services.TaskDevicesHeartbeat, service.DevicesHeartbeat(), asynq.BatchTask())
+	s.worker.HandleTask(services.TaskDevicesHeartbeat, service.DevicesHeartbeat(), asynq.BatchTask())
 
-	if err := worker.Start(); err != nil {
-		log.WithError(err).
-			Fatal("failed to start the worker")
-	}
-
-	router := routes.NewRouter(service, routerOptions...)
-
-	go func() {
-		<-ctx.Done()
-
-		log.Debug("Closing HTTP server due context cancellation")
-
-		worker.Shutdown()
-		router.Close()
-	}()
-
-	err = router.Start(":8080") //nolint:errcheck
-	log.WithError(err).Info("HTTP server closed")
+	log.Info("Server setup completed successfully")
 
 	return nil
+}
+
+// Start begins serving API requests and processing background tasks. It blocks the current goroutine until the server stops
+// or encounters an error.
+func (s *Server) Start() error {
+	log.Info("Starting server components")
+
+	if err := s.worker.Start(); err != nil {
+		return err
+	}
+
+	if err := s.router.Start(":8080"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Shutdown gracefully terminates all server components.
+func (s *Server) Shutdown() {
+	log.Info("Gracefully shutting down server")
+
+	s.worker.Shutdown()
+	s.router.Close() // nolint: errcheck
+
+	log.Info("Server shutdown complete")
+}
+
+// serviceOptions returns configuration options for the application services.
+func (s *Server) serviceOptions(ctx context.Context) ([]services.Option, error) {
+	opts := []services.Option{}
+
+	var geoipFetcher geolite2.GeoliteFetcher
+	switch {
+	case s.env.GeoipMirror != "":
+		log.Info("Using custom mirror for GeoIP database")
+		geoipFetcher = geolite2.FetchFromMirror(s.env.GeoipMirror)
+	case s.env.GeoipMaxmindLicense != "":
+		log.Info("Using MaxMind license key for GeoIP database")
+		geoipFetcher = geolite2.FetchFromLicenseKey(s.env.GeoipMaxmindLicense)
+	}
+
+	if geoipFetcher != nil {
+		locator, err := geolite2.NewLocator(ctx, geoipFetcher)
+		if err != nil {
+			return nil, err
+		}
+
+		log.Info("GeoIP locator initialized successfully")
+
+		opts = append(opts, services.WithLocator(locator))
+	}
+
+	return opts, nil
+}
+
+// routerOptions returns configuration options for the HTTP router.
+func (s *Server) routerOptions() ([]routes.Option, error) {
+	opts := []routes.Option{}
+
+	if s.env.SentryDSN != "" {
+		log.Info("Initializing Sentry error reporting")
+
+		sentryOpts := sentry.ClientOptions{ //nolint:exhaustruct
+			Dsn:              s.env.SentryDSN,
+			Release:          os.Getenv("SHELLHUB_VERSION"),
+			EnableTracing:    true,
+			TracesSampleRate: 1,
+		}
+
+		reporter, err := sentry.NewClient(sentryOpts)
+		if err != nil {
+			return nil, err
+		}
+
+		log.Info("Sentry error reporting initialized successfully")
+
+		opts = append(opts, routes.WithReporter(reporter))
+	}
+
+	return opts, nil
 }
