05ecaf25dacbc679e371fe1d35a978ae64d44c07
Heitor Danilo <heitor.danilo@ossystems.com.br>
2025-03-17 15:33:40 -0300

    refactor(api): server initialization

--- api/main.go
+++ api/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/shellhub-io/shellhub/pkg/envs"
 	"github.com/shellhub-io/shellhub/pkg/loglevel"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -13,18 +16,53 @@ func main() {
 	loglevel.UseEnvs()
 
 	rootCmd := &cobra.Command{Use: "api"}
+	rootCmd.AddCommand(&cobra.Command{
+		Use: "server",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			env, err := envs.ParseWithPrefix[env]("API_")
+			if err != nil {
+				log.WithError(err).
+					Error("Failed to load environment variables")
 
-	rootCmd.AddCommand(serverCmd)
+				return err
+			}
 
-	// Populates configuration based on environment variables prefixed with 'API_'.
-	cfg, err := envs.ParseWithPrefix[config]("API_")
-	if err != nil {
-		logrus.WithError(err).Fatal("Failed to load environment variables")
-	}
+			server := &Server{env: env}
 
-	ctx := context.WithValue(context.TODO(), "cfg", cfg) //nolint:revive
+			if err := server.Setup(cmd.Context()); err != nil {
+				log.WithError(err).
+					Error("failed to setup the server")
 
-	if err := rootCmd.ExecuteContext(ctx); err != nil {
-		logrus.Fatal(err)
+				return err
+			}
+
+			sigs := make(chan os.Signal, 1)
+			signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+			go func() {
+				sig := <-sigs
+				log.WithField("signal", sig).
+					Info("shutting down the server")
+
+				server.Shutdown()
+				os.Exit(0)
+			}()
+
+			if err := server.Start(); err != nil {
+				log.WithError(err).
+					Error("failed too start the server")
+
+				return err
+			}
+
+			return nil
+		},
+	})
+
+	if err := rootCmd.ExecuteContext(context.Background()); err != nil {
+		log.WithError(err).
+			Error("failed to execute command")
+
+		os.Exit(1)
 	}
 }
