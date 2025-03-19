c8a2a90d0012f4df3e9e5d3159ea8140debfc901
luizhf42 <luizhf42@gmail.com>
2025-03-19 11:07:31 -0300

    style(ui): remove spaces from methods' parameters

--- ui/src/store/api/users.ts
+++ ui/src/store/api/users.ts
@@ -1,7 +1,7 @@
 import { IUser, IUserPutSecurity, IUserUpdatePassword, IUserSignUp, IUserSetup } from "@/interfaces/IUSer";
 import { usersApi, systemApi } from "@/api/http";
 
-export const signUp = async (data : IUserSignUp) => usersApi.registerUser({
+export const signUp = async (data: IUserSignUp) => usersApi.registerUser({
   name: data.name,
   email: data.email,
   username: data.username,
@@ -10,29 +10,29 @@ export const signUp = async (data : IUserSignUp) => usersApi.registerUser({
   sig: data.sig,
 });
 
-export const postResendEmail = async (username : string) => usersApi.resendEmail({ username });
+export const postResendEmail = async (username: string) => usersApi.resendEmail({ username });
 
-export const postRecoverPassword = async (username : string) => usersApi.recoverPassword({ username });
+export const postRecoverPassword = async (username: string) => usersApi.recoverPassword({ username });
 
-export const postValidationAccount = async (data : IUser) => usersApi.getValidateAccount(data.email, data.token);
+export const postValidationAccount = async (data: IUser) => usersApi.getValidateAccount(data.email, data.token);
 
-export const setSessionRecordStatus = async (data : IUserPutSecurity) => usersApi.setSessionRecord(data.id, { session_record: data.status });
+export const setSessionRecordStatus = async (data: IUserPutSecurity) => usersApi.setSessionRecord(data.id, { session_record: data.status });
 
 export const getSessionRecordStatus = async () => usersApi.checkSessionRecord();
 
-export const postUpdatePassword = async (data : IUserUpdatePassword) => usersApi.updateRecoverPassword(data.id, {
+export const postUpdatePassword = async (data: IUserUpdatePassword) => usersApi.updateRecoverPassword(data.id, {
   token: data.token,
   password: data.password,
 });
 
-export const patchUserData = async (data : IUser) => usersApi.updateUser({
+export const patchUserData = async (data: IUser) => usersApi.updateUser({
   name: data.name,
   username: data.username,
   email: data.email,
   recovery_email: data.recovery_email,
 });
 
-export const patchUserPassword = async (data : IUser) => usersApi.updateUser({
+export const patchUserPassword = async (data: IUser) => usersApi.updateUser({
   name: data.name,
   username: data.username,
   email: data.email,
