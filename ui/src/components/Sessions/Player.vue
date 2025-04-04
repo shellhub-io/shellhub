d26e4ddfbdd139aa319e1af9ee21a4d6c12f7822
luizhf42 <luizhf42@gmail.com>
2025-04-04 11:18:34 -0300

    feat(ui): add Player component

---
+++ ui/src/components/Sessions/Player.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper" v-if="logs" ref="wrapper" />
+</template>
+
+<script setup lang="ts">
+import * as AsciinemaPlayer from "asciinema-player";
+import { onMounted, ref } from "vue";
+
+const { logs } = defineProps<{
+  logs: string | null;
+}>();
+
+const wrapper = ref<HTMLDivElement | null>(null);
+const player = ref<AsciinemaPlayer.AsciinemaPlayer | null>(null);
+
+onMounted(() => {
+  console.log(logs);
+  player.value = AsciinemaPlayer.create({ data: logs }, wrapper.value, { fit: "height" });
+});
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100%;
+  overflow: scroll-y;
+}
+</style>
