62ddabba02e6453c259a5e8275597607e762895b
Heitor Danilo <heitor.danilo@ossystems.com.br>
2025-04-13 23:17:30 -0300

    feat(api,cli): Save and Delete methods

--- api/store/pg/utils.go
+++ api/store/pg/utils.go
@@ -1,11 +1,73 @@
 package pg
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"io"
+	"reflect"
+	"time"
 
 	"github.com/shellhub-io/shellhub/api/store"
+	log "github.com/sirupsen/logrus"
 )
+
+func (pg *pg) Save(ctx context.Context, models ...any) error {
+	for _, m := range models {
+		if err := setUpdatedAt(m); err != nil {
+			log.Warnf("failed to set UpdatedAt: %v", err)
+		}
+
+		if _, err := pg.driver.NewUpdate().Model(ptr(m)).WherePK().Exec(ctx); err != nil {
+			return fromSqlError(err)
+		}
+	}
+
+	return nil
+}
+
+func (pg *pg) Delete(ctx context.Context, models ...any) error {
+	for _, m := range models {
+		if _, err := pg.driver.NewDelete().Model(ptr(m)).WherePK().Exec(ctx); err != nil {
+			return fromSqlError(err)
+		}
+	}
+
+	return nil
+}
+
+func setUpdatedAt(model any) error {
+	value := reflect.ValueOf(model)
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
+
+	if value.Kind() != reflect.Struct {
+		return nil
+	}
+
+	updatedField := value.FieldByName("UpdatedAt")
+	if updatedField.IsValid() && updatedField.CanSet() {
+		if updatedField.Type() == reflect.TypeOf(time.Time{}) {
+			updatedField.Set(reflect.ValueOf(time.Now().UTC()))
+			return nil
+		}
+		return fmt.Errorf("UpdatedAt field is not of type time.Time")
+	}
+
+	return nil
+}
+
+func ptr(arg any) any {
+	argPtr := arg
+	if val := reflect.ValueOf(argPtr); val.Kind() != reflect.Ptr {
+		newPtr := reflect.New(val.Type())
+		newPtr.Elem().Set(val)
+		argPtr = newPtr.Interface()
+	}
+
+	return argPtr
+}
 
 func fromSqlError(err error) error {
 	switch {
