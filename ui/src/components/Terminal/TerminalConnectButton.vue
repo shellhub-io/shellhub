de72fca6c88949c4629fa1490f97f734e8a02312
luizhf42 <luizhf42@gmail.com>
2025-04-24 10:33:36 -0300

    refactor(ui): move connection button to new TerminalConnectButton component

---
+++ ui/src/components/Terminal/TerminalConnectButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-btn
+    width="100%"
+    :disabled="!online"
+    :color="online ? 'success' : 'normal'"
+    variant="outlined"
+    density="comfortable"
+    data-test="connect-btn"
+    @click="showDialog = true"
+  >
+    {{ online ? "Connect" : "Offline" }}
+  </v-btn>
+
+  <TerminalDialog
+    v-model="showDialog"
+    :deviceUid
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import TerminalDialog from "./TerminalDialog.vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+defineProps<{
+  online: boolean;
+  deviceUid: string;
+}>();
+
+const showDialog = ref(false);
+</script>
+
+<style scoped lang="scss">
+
+</style>
