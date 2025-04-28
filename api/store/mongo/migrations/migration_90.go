5272f2d0590e0a74b5b211ea30eec073d679ecb4
Heitor Danilo <heitor.danilo@ossystems.com.br>
2025-04-28 16:46:59 -0300

    fix(cloud): set user's username to nil when creating with SAML origin

---
+++ api/store/mongo/migrations/migration_90.go
@@ -0,0 +1,37 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var migration90 = migrate.Migration{
+	Version:     90,
+	Description: "Convert the username's to nil when it's a blank string",
+	Up: migrate.MigrationFunc(func(ctx context.Context, db *mongo.Database) error {
+		logrus.WithFields(logrus.Fields{
+			"component": "migration",
+			"version":   90,
+			"action":    "Up",
+		}).Info("Applying migration")
+
+		_, err := db.Collection("users").UpdateMany(ctx, bson.M{"username": ""}, bson.M{"$set": bson.M{"username": nil}})
+
+		return err
+	}),
+	Down: migrate.MigrationFunc(func(ctx context.Context, db *mongo.Database) error {
+		logrus.WithFields(logrus.Fields{
+			"component": "migration",
+			"version":   90,
+			"action":    "Down",
+		}).Info("Cannot undo migration")
+
+		_, err := db.Collection("users").UpdateMany(ctx, bson.M{"username": nil}, bson.M{"$set": bson.M{"username": ""}})
+
+		return err
+	}),
+}
