7fd90898a02457546ffcde26d24c26c6c65ce027
Heitor Danilo <heitor.danilo@ossystems.com.br>
2025-04-28 09:50:22 -0300

    fix(api): set saml user's username to nil

---
+++ api/store/mongo/migrations/migration_99.go
@@ -0,0 +1,37 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var migration99 = migrate.Migration{
+	Version:     99,
+	Description: "Convert the username's to nil when it's a blank string",
+	Up: migrate.MigrationFunc(func(ctx context.Context, db *mongo.Database) error {
+		logrus.WithFields(logrus.Fields{
+			"component": "migration",
+			"version":   99,
+			"action":    "Up",
+		}).Info("Applying migration")
+
+		_, err := db.Collection("users").UpdateMany(ctx, bson.M{"username": ""}, bson.M{"$set": bson.M{"username": nil}})
+
+		return err
+	}),
+	Down: migrate.MigrationFunc(func(ctx context.Context, db *mongo.Database) error {
+		logrus.WithFields(logrus.Fields{
+			"component": "migration",
+			"version":   99,
+			"action":    "Down",
+		}).Info("Cannot undo migration")
+
+		_, err := db.Collection("users").UpdateMany(ctx, bson.M{"username": nil}, bson.M{"$set": bson.M{"username": ""}})
+
+		return err
+	}),
+}
