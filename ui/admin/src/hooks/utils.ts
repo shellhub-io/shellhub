b8646b98698c0094b86ef0880aa9da53025fda5b
luizhf42 <luizhf42@gmail.com>
2025-05-13 09:05:48 -0300

    refactor(ui): remove unused util

--- ui/admin/src/hooks/utils.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const isHTML = (str: string) => !(str || "")
-  .replace(/<([^>]+?)([^>]*?)>(.*?)<\/\1>/gi, "")
-  .replace(/(<([^>]+)>)/gi, "")
-  .trim();
