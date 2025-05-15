8a3f56deeeaedcc9caaf34fa3fc60b0bd3c99567
luizhf42 <luizhf42@gmail.com>
2025-05-15 10:09:08 -0300

    refactor(ui): improve notification store extensibility
    - makes the notification store less coupled to the pending devices/containers
    - adds a general, more extensible notification object
    - adds notification typing and adapter utils
    - refactors the component to use the new store approach

--- ui/src/components/AppBar/Notifications/NotificationList.vue
+++ ui/src/components/AppBar/Notifications/NotificationList.vue
@@ -6,15 +6,15 @@
     <v-list-item
       class="pr-0"
       v-for="notification in notifications"
-      :key="notification.uid"
+      :key="notification.id"
     >
       <template v-slot:prepend>
         <v-list-item-title>
           <router-link
-            :to="{ name: 'DeviceDetails', params: { id: notification.uid } }"
-            :data-test="notification.uid + '-field'"
+            :to="{ name: 'DeviceDetails', params: { id: notification.data.uid } }"
+            :data-test="notification.data.uid + '-field'"
           >
-            {{ notification.name }}
+            {{ notification.data.name }}
           </router-link>
         </v-list-item-title>
       </template>
@@ -22,13 +22,13 @@
       <template v-slot:append>
         <v-list-item-action class="ma-0">
           <DeviceActionButton
-            :uid="notification.uid"
-            :name="notification.name"
+            :uid="notification.data.uid"
+            :name="notification.data.name"
             variant="device"
             :notification-status="true"
             :show="true"
             action="accept"
-            :data-test="notification.uid + '-btn'"
+            :data-test="notification.data.uid + '-btn'"
             @update="$emit('update')"
           />
         </v-list-item-action>
@@ -39,11 +39,9 @@
 
 <script setup lang="ts">
 import DeviceActionButton from "@/components/Devices/DeviceActionButton.vue";
+import { INotification } from "@/interfaces/INotification";
 
 defineProps<{
-  notifications: Array<{
-    uid: string;
-    name: string;
-  }>,
+  notifications: Array<INotification>,
 }>();
 </script>
---
+++ ui/src/interfaces/INotification.ts
@@ -0,0 +1,15 @@
+import { IContainer } from "@/interfaces/IContainer";
+import { IDevice } from "@/interfaces/IDevice";
+
+export enum NotificationType {
+  DEVICE = "device",
+  CONTAINER = "container"
+}
+
+type NotificationData = IDevice | IContainer;
+
+export interface INotification {
+  id: string | number;
+  type: NotificationType;
+  data: NotificationData;
+}
