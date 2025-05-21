c2ea03507132c358636e3483050cd5cba3c11512
luizhf42 <luizhf42@gmail.com>
2025-05-21 12:49:01 -0300

    fix(ui): delete problematic global components declarations

--- ui/admin/src/global-components.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@vue/runtime-core" {
-  export interface GlobalComponents {
-    RouterLink: typeof import("vue-router")["RouterLink"];
-    RouterView: typeof import("vue-router")["RouterView"];
-  }
-}
