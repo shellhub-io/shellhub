988e8a289ab48a61317cdeb5b4fe7d29e7852800
luizhf42 <luizhf42@gmail.com>
2025-06-02 09:34:33 -0300

    test(ui): add stub and update snapshot
    - stub `visualViewport` globally since it doesn't work in test environment
    - update admin's License snapshot

--- ui/vitest/beforeAll.ts
+++ ui/vitest/beforeAll.ts
@@ -1,5 +1,5 @@
 import MockAdapter from "axios-mock-adapter";
-import { beforeAll } from "vitest";
+import { beforeAll, vi } from "vitest";
 import ResizeObserver from "resize-observer-polyfill";
 import { systemApi } from "../src/api/http";
 
@@ -18,6 +18,8 @@ const mockedSystemInfo = {
 
 const systemApiMock = new MockAdapter(systemApi.getAxios());
 systemApiMock.onGet("http://localhost:3000/info").reply(200, mockedSystemInfo);
+
+vi.stubGlobal("visualViewport", new EventTarget());
 
 global.CSS = { supports: () => false } as never;
 global.ResizeObserver = ResizeObserver;
