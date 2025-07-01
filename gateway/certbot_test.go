420fe07dbfdff0ff9340c701e311df520837a55b
Henry Barreto <henry.barreto@ossystems.com.br>
2025-07-01 12:14:53 -0300

    tests(gateway): add tests to certificate generation from DNS

--- gateway/certbot_test.go
+++ gateway/certbot_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"errors"
+	"os/exec"
 	"testing"
 
+	executorMock "github.com/shellhub-io/shellhub/gateway/mocks"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestCertBot_generateProviderCredentialsFile(t *testing.T) {
@@ -23,4 +27,117 @@ func TestCertBot_generateProviderCredentialsFile(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, "dns_digitalocean_token = test", string(buffer))
+}
+
+func TestCertBot_generateCertificateFromDNS(t *testing.T) {
+	tests := []struct {
+		name        string
+		config      Config
+		expected    error
+		expectCalls func(*executorMock.Executor)
+	}{
+		{
+			name: "failed to run the command",
+			config: Config{
+				Tunnels: &Tunnels{
+					Domain:   "localhost",
+					Provider: "digitalocean",
+					Token:    "test",
+				},
+			},
+			expectCalls: func(executorMock *executorMock.Executor) {
+				executorMock.On("Command", "certbot",
+					"certonly",
+					"--non-interactive",
+					"--agree-tos",
+					"--register-unsafely-without-email",
+					"--cert-name",
+					"*.localhost",
+					"--dns-digitalocean",
+					"--dns-digitalocean-credentials",
+					"/etc/shellhub-gateway/digitalocean.ini",
+					"-d",
+					"*.localhost",
+				).Return(exec.Command("")).Once()
+
+				executorMock.On("Run", mock.AnythingOfType("*exec.Cmd")).Return(errors.New("failed to run the command")).Once()
+			},
+			expected: errors.New("failed to run the command"),
+		},
+		{
+			name: "successful certificate generation",
+			config: Config{
+				Tunnels: &Tunnels{
+					Domain:   "localhost",
+					Provider: "digitalocean",
+					Token:    "test",
+				},
+			},
+			expectCalls: func(executorMock *executorMock.Executor) {
+				executorMock.On("Command", "certbot",
+					"certonly",
+					"--non-interactive",
+					"--agree-tos",
+					"--register-unsafely-without-email",
+					"--cert-name",
+					"*.localhost",
+					"--dns-digitalocean",
+					"--dns-digitalocean-credentials",
+					"/etc/shellhub-gateway/digitalocean.ini",
+					"-d",
+					"*.localhost",
+				).Return(exec.Command("")).Once()
+
+				executorMock.On("Run", mock.AnythingOfType("*exec.Cmd")).Return(nil).Once()
+			},
+			expected: nil,
+		},
+		{
+			name: "successful certificate generation in staging",
+			config: Config{
+				Tunnels: &Tunnels{
+					Domain:   "localhost",
+					Provider: "digitalocean",
+					Token:    "test",
+				},
+				Staging: true,
+			},
+			expectCalls: func(executorMock *executorMock.Executor) {
+				executorMock.On("Command", "certbot",
+					"certonly",
+					"--non-interactive",
+					"--agree-tos",
+					"--register-unsafely-without-email",
+					"--cert-name",
+					"*.localhost",
+					"--dns-digitalocean",
+					"--dns-digitalocean-credentials",
+					"/etc/shellhub-gateway/digitalocean.ini",
+					"-d",
+					"*.localhost",
+					"--staging",
+				).Return(exec.Command("")).Once()
+
+				executorMock.On("Run", mock.AnythingOfType("*exec.Cmd")).Return(nil).Once()
+			},
+			expected: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(tt *testing.T) {
+			executorMock := new(executorMock.Executor)
+
+			certbot := newCertBot(&tc.config)
+			certbot.fs = afero.NewMemMapFs()
+			certbot.ex = executorMock
+
+			tc.expectCalls(executorMock)
+
+			err := certbot.generateCertificateFromDNS()
+			assert.Equal(tt, tc.expected, err)
+
+			executorMock.AssertExpectations(t)
+		})
+	}
 }
