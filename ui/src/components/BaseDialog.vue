943c06e82347233b75bf22427b181b98fce9eb6c
luizhf42 <luizhf42@gmail.com>
2025-07-16 08:56:01 -0300

    feat(ui): add generic BaseDialog component

---
+++ ui/src/components/BaseDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-dialog
+    :fullscreen
+    :max-width
+  >
+    <slot />
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useDisplay } from "vuetify";
+
+const props = defineProps<{
+  /* Breakpoints
+  * - sm: 600px (default)
+  * - md: 960px
+  * - lg: 1280px
+  * - xl: 1920px
+  * - xxl: 2560px
+  */
+  breakpoint?: "sm" | "md" | "lg" | "xl" | "xxl"
+  forceFullscreen?: boolean
+}>();
+
+const { smAndDown, thresholds } = useDisplay();
+const fullscreen = computed(() => props.forceFullscreen || smAndDown.value);
+const maxWidth = computed(() => fullscreen.value ? undefined : thresholds.value[props.breakpoint || "sm"]);
+</script>
