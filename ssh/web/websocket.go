2b40b9c10476e1210d7135894b5cd089a86fb809
Henry Barreto <henry.barreto@ossystems.com.br>
2025-07-23 09:47:12 -0300

    refactor(ssh): change web terminal dimensions to uint32
    This commit updates the Dimensions from web terminal struct to use
    uint32 for Cols and Rows, ensuring that terminal dimensions are always
    non-negative.
    
    Using uint32 enhances type safety and eliminates the risk of negative
    values, which can occur when using int, especially on 32-bit systems
    where int can represent negative numbers.
    
    The getDimensions function and related calls to agent.RequestPty and
    agent.WindowChange have been updated accordingly, along with the
    relevant tests. This refactor improves clarity and robustness in
    handling terminal dimensions. It worth to notice that these functions do
    conversion from int to uint32 internally.

--- ssh/web/websocket.go
+++ ssh/web/websocket.go
@@ -16,7 +16,7 @@ func getToken(req *http.Request) (string, error) {
 	return token, nil
 }
 
-func getDimensions(req *http.Request) (int, int, error) {
+func getDimensions(req *http.Request) (uint32, uint32, error) {
 	toUint32 := func(text string) (uint64, error) {
 		integer, err := strconv.ParseUint(text, 10, 32)
 		if err != nil {
@@ -36,8 +36,8 @@ func getDimensions(req *http.Request) (int, int, error) {
 		return 0, 0, errors.Join(ErrGetDimensions, err)
 	}
 
-	// nolint: gosec // cols and rows are uint32, so we can safely convert them to int.
-	return int(cols), int(rows), nil
+	//nolint: gosec // cols and rows are uint32, so we can safely convert them.
+	return uint32(cols), uint32(rows), nil
 }
 
 func getIP(req *http.Request) (string, error) {
