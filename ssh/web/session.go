278944a7bb2dbef00ea08b2a79da21e877ab5d92
Henry Barreto <henry.barreto@ossystems.com.br>
2025-07-24 14:29:02 -0300

    fix(ssh): prevent panic when agent output is empty in web terminal
    The redirToWs function, responsible for piping data from the agent
    output to the web terminal, previously panicked when it encountered a
    return value of 0 from io.Reader. This behavior ignored the special case
    outlined in the io.Reader documentation, which states that a return
    value of 0 does not indicate EOF but rather signifies that no data was
    read, and we should expect for more data.
    
    ```https://pkg.go.dev/io#Reader```
    
    With this update, the function now correctly handles this scenario by
    continuing the reading loop instead of panicking, ensuring more robust
    behavior when no data is available. We also added a regression test to
    avoid this bahavior in the future.

--- ssh/web/session.go
+++ ssh/web/session.go
@@ -278,6 +278,15 @@ func redirToWs(rd io.Reader, ws *Conn) error {
 			return err
 		}
 
+		if nr == 0 {
+			// NOTE: "Callers should treat a return of 0 and nil as indicating that nothing happened; in particular it
+			// does not indicate EOF", in such a case, the caller should not interpret it as EOF, but instead wait for
+			// more data.
+			//
+			// https://pkg.go.dev/io#Reader
+			continue
+		}
+
 		buflen = start + nr
 		for end = buflen - 1; end >= 0; end-- {
 			if utf8.RuneStart(buf[end]) {
