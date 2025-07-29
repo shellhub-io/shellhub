43f149751a2fa73a7390b55c92c7028bd7874d74
luizhf42 <luizhf42@gmail.com>
2025-07-29 17:10:09 -0300

    refactor(ui): migrate API Keys store to Pinia
    - migrate store from Vuex to Pinia
    - update and clean tests
    - adapt components
    - improve logic and naming

--- ui/src/components/Team/ApiKeys/ApiKeyEdit.vue
+++ ui/src/components/Team/ApiKeys/ApiKeyEdit.vue
@@ -20,38 +20,30 @@
       <v-divider />
 
       <v-card-text class="mt-3">
-
-        <v-row>
-          <v-col>
-            <v-text-field
-              v-model="keyInput"
-              label="key name"
-              prepend-icon="mdi-key-outline"
-              :error-messages="keyInputError"
-              required
-              variant="underlined"
-              data-test="key-name-text"
-              messages="Please note that the new name must be unique
+        <v-text-field
+          v-model="keyName"
+          class="mb-5"
+          label="key name"
+          prepend-icon="mdi-key-outline"
+          :error-messages="keyNameError"
+          required
+          variant="underlined"
+          data-test="key-name-text"
+          messages="Please note that the new name must be unique
           and not already in use by another key."
-            />
+        />
 
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <RoleSelect
-              v-if="hasAuthorization"
-              v-model="selectedRole"
-              data-test="namespace-generate-role"
-            />
-          </v-col>
-        </v-row>
+        <RoleSelect
+          v-if="hasAuthorization"
+          v-model="selectedRole"
+          data-test="namespace-generate-role"
+        />
       </v-card-text>
 
       <v-card-actions>
         <v-btn variant="text" @click="showDialog = false" data-test="close-btn"> Close </v-btn>
         <v-spacer />
-        <v-btn color="success" variant="flat" data-test="edit-btn" @click="edit()">
+        <v-btn color="success" variant="flat" data-test="edit-btn" @click="edit()" :disabled="!!keyNameError">
           Edit key
         </v-btn>
       </v-card-actions>
@@ -60,16 +52,16 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import { useField } from "vee-validate";
 import * as yup from "yup";
 import axios, { AxiosError } from "axios";
-import { useStore } from "@/store";
 import handleError from "@/utils/handleError";
 import useSnackbar from "@/helpers/snackbar";
 import BaseDialog from "@/components/BaseDialog.vue";
 import RoleSelect from "@/components/Team/RoleSelect.vue";
 import { BasicRole } from "@/interfaces/INamespace";
+import useApiKeysStore from "@/store/modules/api_keys";
 
 defineOptions({
   inheritAttrs: false,
@@ -81,31 +73,34 @@ const props = defineProps<{
   hasAuthorization: boolean;
   disabled: boolean;
 }>();
+
 const emit = defineEmits(["update"]);
 const showDialog = ref(false);
-const store = useStore();
+const apiKeyStore = useApiKeysStore();
 const snackbar = useSnackbar();
-const keyGetter = computed(() => props.keyName);
+const selectedRole = ref<BasicRole>(props.keyRole as BasicRole);
+
 const {
-  value: keyInput,
-  errorMessage: keyInputError,
-  setErrors: setKeyInputError,
-} = useField<string | undefined>(
+  value: keyName,
+  errorMessage: keyNameError,
+  setErrors: setKeyNameError,
+} = useField<string>(
   "name",
   yup
     .string()
     .required()
     .min(3)
     .max(20)
-    .matches(/^(?!.*\s).*$/, "This field cannot contain any blankspaces"),
+    .matches(/^(?!.*\s).*$/, "This field cannot contain any blank spaces"),
   {
-    initialValue: keyGetter.value,
+    initialValue: props.keyName,
   },
 );
 
 const open = () => {
   showDialog.value = true;
-  keyInput.value = keyGetter.value;
+  keyName.value = props.keyName;
+  selectedRole.value = props.keyRole as BasicRole;
 };
 
 const update = () => {
@@ -113,35 +108,25 @@ const update = () => {
   showDialog.value = false;
 };
 
-const selectedRole = ref<BasicRole>(props.keyRole as BasicRole);
-
 const edit = async () => {
-  const equalName = props.keyName === keyInput.value;
-  const equalRole = props.keyRole === selectedRole.value;
-  const payload: { key: string; name?: string; role?: string } = { key: props.keyName };
-
-  if (!equalName) {
-    payload.name = keyInput.value;
-  }
-  if (!equalRole) {
-    payload.role = selectedRole.value;
-  }
-
   try {
-    await store.dispatch("apiKeys/editApiKey", payload);
+    await apiKeyStore.editApiKey({
+      key: props.keyName,
+      name: keyName.value === props.keyName ? undefined : keyName.value, // Only send name if it has changed
+      role: selectedRole.value,
+    });
+
     update();
     snackbar.showSuccess("Api Key edited successfully.");
   } catch (error: unknown) {
     snackbar.showError("Failed to edit Api Key.");
     if (axios.isAxiosError(error)) {
       const axiosError = error as AxiosError;
-      switch (axiosError.response?.status) {
-        case 409:
-          setKeyInputError("An API key with the same name already exists.");
-          break;
-        default:
-          setKeyInputError("An error occurred while editing your API key");
-          handleError(error);
+      if (axiosError.response?.status === 409) {
+        setKeyNameError("An API key with the same name already exists.");
+      } else {
+        setKeyNameError("An error occurred while editing your API key");
+        handleError(error);
       }
       return;
     }
@@ -149,5 +134,5 @@ const edit = async () => {
   }
 };
 
-defineExpose({ keyInputError });
+defineExpose({ keyNameError });
 </script>
