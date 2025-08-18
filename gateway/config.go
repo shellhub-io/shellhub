2f886c6bc0cb8fcfccd04a45a2cc238a38798351
Henry Barreto <henry.barreto@ossystems.com.br>
2025-08-18 14:37:24 -0300

    feat(gateway): add rate limit to api routes

--- gateway/config.go
+++ gateway/config.go
@@ -11,20 +11,24 @@ import (
 
 // GatewayConfig holds the configuration settings for the gateway.
 type GatewayConfig struct {
-	Env                     string      `env:"SHELLHUB_ENV"`
-	Domain                  string      `env:"SHELLHUB_DOMAIN,required" validate:"hostname"`
-	WebEndpoints            bool        `env:"SHELLHUB_WEB_ENDPOINTS,default=false"`
-	WebEndpointsDomain      string      `env:"SHELLHUB_WEB_ENDPOINTS_DOMAIN"`
-	WebEndpointsDNSProvider DNSProvider `env:"SHELLHUB_WEB_ENDPOINTS_DNS_PROVIDER,default=digitalocean"`
-	WebEndpointsDNSProviderToken string `env:"SHELLHUB_WEB_ENDPOINTS_DNS_PROVIDER_TOKEN"`
-	WorkerProcesses         string      `env:"WORKER_PROCESSES,default=auto"`
-	MaxWorkerOpenFiles      int         `env:"MAX_WORKER_OPEN_FILES,default=0"`
-	MaxWorkerConnections    int         `env:"MAX_WORKER_CONNECTIONS,default=16384"`
-	BacklogSize             int         `env:"BACKLOG_SIZE"`
-	EnableAutoSSL           bool        `env:"SHELLHUB_AUTO_SSL"`
-	EnableProxyProtocol     bool        `env:"SHELLHUB_PROXY"`
-	EnableEnterprise        bool        `env:"SHELLHUB_ENTERPRISE"`
-	EnableCloud             bool        `env:"SHELLHUB_CLOUD"`
+	Env                          string      `env:"SHELLHUB_ENV"`
+	Domain                       string      `env:"SHELLHUB_DOMAIN,required" validate:"hostname"`
+	WebEndpoints                 bool        `env:"SHELLHUB_WEB_ENDPOINTS,default=false"`
+	WebEndpointsDomain           string      `env:"SHELLHUB_WEB_ENDPOINTS_DOMAIN"`
+	WebEndpointsDNSProvider      DNSProvider `env:"SHELLHUB_WEB_ENDPOINTS_DNS_PROVIDER,default=digitalocean"`
+	WebEndpointsDNSProviderToken string      `env:"SHELLHUB_WEB_ENDPOINTS_DNS_PROVIDER_TOKEN"`
+	WorkerProcesses              string      `env:"WORKER_PROCESSES,default=auto"`
+	MaxWorkerOpenFiles           int         `env:"MAX_WORKER_OPEN_FILES,default=0"`
+	MaxWorkerConnections         int         `env:"MAX_WORKER_CONNECTIONS,default=16384"`
+	BacklogSize                  int         `env:"BACKLOG_SIZE"`
+	EnableAutoSSL                bool        `env:"SHELLHUB_AUTO_SSL"`
+	EnableProxyProtocol          bool        `env:"SHELLHUB_PROXY"`
+	EnableEnterprise             bool        `env:"SHELLHUB_ENTERPRISE"`
+	EnableCloud                  bool        `env:"SHELLHUB_CLOUD"`
+	APIRateLimit                 string      `env:"SHELLHUB_API_RATE_LIMIT,default=1000r/s"`
+	APIRateLimitZoneSize         string      `env:"SHELLHUB_API_RATE_LIMIT_ZONE_SIZE,default=10m"`
+	APIBurstSize                 string      `env:"SHELLHUB_API_BURST_SIZE,default=1"`
+	APIBurstDelay                string      `env:"SHELLHUB_API_BURST_DELAY,default=nodelay"`
 }
 
 var validate = validator.New()
