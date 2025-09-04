563c366ae6f679bd2e13fbbe5f0193cc9ada66ee
Henry Barreto <henry.barreto@ossystems.com.br>
2025-09-04 17:13:13 -0300

    feat(agent,pkg): load tunnel configuration from authentication data
    Updated the tunnel listening mechanism to utilize a configuration loaded
    from authentication data instead of the default configuration. This
    change enhances flexibility by allowing device-specific settings to be
    applied dynamically. The NewConfigFromMap function was added to create a
    new configuration from a map, ensuring that default values are used when
    specific keys are missing. Additionally, improved error handling during
    session creation to attempt a fallback to the default configuration if
    the custom one fails.

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -646,7 +646,7 @@ func (a *Agent) Listen(ctx context.Context) error {
 
 			a.listening <- true
 
-			if err := a.tunnel.Listen(conn, &tunnel.DefaultConfig); err != nil {
+			if err := a.tunnel.Listen(conn, tunnel.NewConfigFromMap(a.authData.Config)); err != nil {
 				logger.WithError(err).Error("Tunnel listener exited with error")
 			}
 
