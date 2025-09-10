8657def82de37ed4a21268bd7984f1e8b01aa897
luizhf42 <luizhf42@gmail.com>
2025-09-10 10:24:48 -0300

    feat(ui): separate reusable DeviceLink component

---
+++ ui/src/components/Devices/DeviceLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn
+    class="text-none border rounded bg-v-theme-background"
+    @click="redirectToDeviceDetails()"
+    prepend-icon="mdi-developer-board"
+    variant="plain"
+    density="default"
+    size="default"
+  >
+    {{ deviceName }}
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const { deviceUid, deviceName } = defineProps<{
+  deviceUid: string;
+  deviceName: string;
+}>();
+
+const router = useRouter();
+
+const redirectToDeviceDetails = () => {
+  router.push({ name: "DeviceDetails", params: { identifier: deviceUid } });
+};
+</script>
