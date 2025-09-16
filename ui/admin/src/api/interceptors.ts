25bd5025460eab497669e5a04cd10798e3bae157
luizhf42 <luizhf42@gmail.com>
2025-09-16 15:19:33 -0300

    refactor(ui): use main UI's Spinner store in admin
    - removes the admin store since they're exactly the same

--- ui/admin/src/api/interceptors.ts
+++ ui/admin/src/api/interceptors.ts
@@ -1,37 +1,27 @@
 import { AxiosError, AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from "axios";
-import { setActivePinia, createPinia } from "pinia";
-import useSpinnerStore from "@admin/store/modules/spinner";
 import useAuthStore from "@admin/store/modules/auth";
+import useSpinnerStore from "@/store/modules/spinner";
 import router from "../router/index";
 
-setActivePinia(createPinia());
-
 const onRequest = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-  const spinnerStore = useSpinnerStore();
-  spinnerStore.setStatus(true);
+  useSpinnerStore().status = true;
   return config;
 };
 
 const onRequestError = (error: AxiosError): Promise<AxiosError> => {
-  const spinnerStore = useSpinnerStore();
-
-  spinnerStore.setStatus(false);
+  useSpinnerStore().status = false;
   return Promise.reject(error);
 };
 
 const onResponse = (response: AxiosResponse): AxiosResponse => {
-  const spinnerStore = useSpinnerStore();
-
-  spinnerStore.setStatus(false);
+  useSpinnerStore().status = false;
   return response;
 };
 
 const onResponseError = async (error: AxiosError): Promise<AxiosError> => {
-  const spinnerStore = useSpinnerStore();
-  const authStore = useAuthStore();
-  spinnerStore.setStatus(false);
+  useSpinnerStore().status = false;
   if (error.response?.status === 401) {
-    await authStore.logout();
+    useAuthStore().logout();
     await router.push({ name: "login" });
   } else if (error.response?.status === 402) {
     await router.push({ name: "license" });
