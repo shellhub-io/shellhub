5671d7e7a63a53bad0a549a37b511482e8f3634a
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2025-09-19 16:02:59 -0300

    refactor(ui): migrate DeviceDelete to MessageDialog

--- ui/src/components/Devices/DeviceDelete.vue
+++ ui/src/components/Devices/DeviceDelete.vue
@@ -14,39 +14,22 @@
     </div>
   </v-list-item>
 
-  <BaseDialog v-model="showDialog" data-test="delete-device-dialog">
-    <v-card class="bg-v-theme-surface" data-test="device-delete-card">
-      <v-card-title class="text-h5 pa-5 bg-primary" data-test="dialog-title">
-        Are you sure?
-      </v-card-title>
-      <v-divider />
-
-      <v-card-text class="mt-4 mb-0 pb-1" data-test="dialog-text">
-        <p class="text-body-2 mb-2">You are about to remove this {{ variant }}.</p>
-
-        <p class="text-body-2 mb-2">
-          After confirming this action cannot be redone.
-        </p>
-      </v-card-text>
-
-      <v-card-actions>
-        <v-spacer />
-
-        <v-btn variant="text" @click="showDialog = false" data-test="close-btn">
-          Close
-        </v-btn>
-
-        <v-btn
-          color="red darken-1"
-          variant="text"
-          @click="removeDevice()"
-          data-test="confirm-btn"
-        >
-          Remove
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </BaseDialog>
+  <MessageDialog
+    v-model="showDialog"
+    @close="showDialog = false"
+    @confirm="removeDevice"
+    @cancel="showDialog = false"
+    title="Are you sure?"
+    :description="`You are about to remove this ${variant}. After confirming this action cannot be redone.`"
+    icon="mdi-alert"
+    icon-color="error"
+    confirm-text="Remove"
+    confirm-color="error"
+    cancel-text="Close"
+    confirm-data-test="confirm-btn"
+    cancel-data-test="close-btn"
+    data-test="delete-device-dialog"
+  />
 </template>
 
 <script setup lang="ts">
@@ -54,7 +37,7 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import handleError from "@/utils/handleError";
 import useSnackbar from "@/helpers/snackbar";
-import BaseDialog from "../BaseDialog.vue";
+import MessageDialog from "../MessageDialog.vue";
 import useDevicesStore from "@/store/modules/devices";
 
 const props = defineProps<{
