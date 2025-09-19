acdc4edc3af9d2adbda571a2b3948ffe3aab8cea
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2025-09-19 16:03:13 -0300

    refactor(ui): migrate DeviceAcceptWarning to MessageDialog

--- ui/src/components/Devices/DeviceAcceptWarning.vue
+++ ui/src/components/Devices/DeviceAcceptWarning.vue
@@ -1,36 +1,24 @@
 <template>
-  <BaseDialog
+  <MessageDialog
     v-if="canSubscribeToBilling"
     v-model="showDialog"
     @close="close"
-    transition="dialog-bottom-transition"
+    @cancel="close"
+    title="You already have a device using the same name"
+    :description="`${duplicatedDeviceName} name is already taken by another accepted device, please choose another name.`"
+    icon="mdi-alert"
+    icon-color="warning"
+    cancel-text="Close"
+    cancel-data-test="close-btn"
+    :show-footer="true"
     data-test="device-accept-warning-dialog"
-  >
-    <v-card class="bg-v-theme-surface" data-test="card-dialog">
-      <v-card-title class="pa-3 bg-primary" data-test="card-title">
-        You already have a device using the same name
-      </v-card-title>
-
-      <v-card-text class="mt-4 mb-3 pb-1">
-        <p class="mb-2" data-test="card-text">
-          <strong>{{ duplicatedDeviceName }} </strong> name is already taken by another accepted device,
-          please choose another name.
-        </p>
-      </v-card-text>
-
-      <v-card-actions>
-        <v-spacer />
-
-        <v-btn variant="text" data-test="close-btn" @click="close"> Close </v-btn>
-      </v-card-actions>
-    </v-card>
-  </BaseDialog>
+  />
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 import hasPermission from "@/utils/permission";
-import BaseDialog from "../BaseDialog.vue";
+import MessageDialog from "../MessageDialog.vue";
 import useDevicesStore from "@/store/modules/devices";
 
 const devicesStore = useDevicesStore();
