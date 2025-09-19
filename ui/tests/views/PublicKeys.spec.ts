610e0e05284194f9aed0abe075bb18eb098dab87
luizhf42 <luizhf42@gmail.com>
2025-09-19 18:10:19 -0300

    fix(ui): fix errors in tag-related tests
    - add missing mocks to the new tag's API route

--- ui/tests/views/PublicKeys.spec.ts
+++ ui/tests/views/PublicKeys.spec.ts
@@ -4,36 +4,39 @@ import { mount, VueWrapper } from "@vue/test-utils";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import MockAdapter from "axios-mock-adapter";
 import PublicKeys from "@/views/PublicKeys.vue";
-import { sshApi } from "@/api/http";
+import { sshApi, tagsApi } from "@/api/http";
 import { SnackbarPlugin } from "@/plugins/snackbar";
 import usePublicKeysStore from "@/store/modules/public_keys";
 
 type PublicKeysWrapper = VueWrapper<InstanceType<typeof PublicKeys>>;
+
+const mockPublicKeys = [{
+  data: "",
+  fingerprint: "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:01",
+  created_at: "2025-01-01T00:00:00.000Z",
+  tenant_id: "00000000-0000-4000-0000-000000000000",
+  name: "public-key-test",
+  username: ".*",
+  filter: {
+    hostname: ".*",
+  },
+}];
 
 describe("Public Keys", () => {
   let wrapper: PublicKeysWrapper;
   setActivePinia(createPinia());
   const publicKeysStore = usePublicKeysStore();
   const vuetify = createVuetify();
-
+  const mockTagsApi = new MockAdapter(tagsApi.getAxios());
   const mockSshApi = new MockAdapter(sshApi.getAxios());
-
-  const mockPublicKeys = [{
-    data: "",
-    fingerprint: "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:01",
-    created_at: "2025-01-01T00:00:00.000Z",
-    tenant_id: "00000000-0000-4000-0000-000000000000",
-    name: "public-key-test",
-    username: ".*",
-    filter: {
-      hostname: ".*",
-    },
-  }];
+  localStorage.setItem("tenant", "fake-tenant-data");
+  mockSshApi.onGet("http://localhost:3000/api/sshkeys/public-keys?page=1&per_page=10").reply(200, mockPublicKeys, { "x-total-count": 1 });
+  mockTagsApi
+    .onGet("http://localhost:3000/api/namespaces/fake-tenant-data/tags?filter=&page=1&per_page=10")
+    .reply(200, []);
+  publicKeysStore.publicKeys = mockPublicKeys;
 
   beforeEach(async () => {
-    mockSshApi.onGet("http://localhost:3000/api/sshkeys/public-keys?page=1&per_page=10").reply(200, mockPublicKeys, { "x-total-count": 1 });
-    publicKeysStore.publicKeys = mockPublicKeys;
-
     wrapper = mount(PublicKeys, {
       global: {
         plugins: [vuetify, SnackbarPlugin],
