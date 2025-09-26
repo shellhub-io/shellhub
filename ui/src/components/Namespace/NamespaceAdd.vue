679b9dcb58ccc0d3e4f267005f1d60694c8a0a24
luizhf42 <luizhf42@gmail.com>
2025-09-26 14:09:27 -0300

    refactor(ui): migrate `NamespaceAdd` to `FormDialog`

--- ui/src/components/Namespace/NamespaceAdd.vue
+++ ui/src/components/Namespace/NamespaceAdd.vue
@@ -1,87 +1,68 @@
 <template>
-  <BaseDialog v-model="showDialog" @close="close">
-    <v-card data-test="namespace-add-card" class="bg-v-theme-surface rounded" rounded>
+  <FormDialog
+    v-model="showDialog"
+    @close="close"
+    @confirm="addNamespace"
+    @cancel="close"
+    :title="isCommunityVersion ? 'Add a namespace using the CLI' : 'New Namespace'"
+    icon="mdi-folder-plus"
+    :confirm-text="isCommunityVersion ? '' : 'Submit'"
+    :confirm-disabled="isCommunityVersion || !fieldMeta.valid"
+    :confirm-loading="isLoading"
+    cancel-text="Close"
+    confirm-data-test="add-btn"
+    cancel-data-test="close-btn"
+    :footer-helper-text="isCommunityVersion ? 'Learn more on' : ''"
+    :footer-helper-link-text="isCommunityVersion ? 'ShellHub Administration Guide' : ''"
+    :footer-helper-link="isCommunityVersion ? 'https://docs.shellhub.io/self-hosted/administration' : ''"
+    data-test="namespace-add-card"
+  >
+    <v-card-text class="pa-6">
       <template v-if="!isCommunityVersion">
-        <v-card-title class="bg-primary d-flex justify-space-between align-center text-h5 pa-4">
-          New Namespace
-          <v-btn
-            icon="mdi-close"
-            variant="text"
-            @click="close"
-          />
-        </v-card-title>
-        <v-container>
-          <v-card-text class="pb-0">
-            <v-text-field
-              v-model="namespaceName"
-              label="Namespace"
-              :error-messages="namespaceNameError"
-              variant="outlined"
-              data-test="username-text"
-              :persistent-hint="true"
-            />
-          </v-card-text>
-          <v-card-text>
-            <v-row class="pl-3 pt-0">
-              <v-col>
-                <ul>
-                  <li>The namespace you choose here will be used for in the SSHID of your devices.</li>
-                  <li>The namespace can contain only lowercase alphanumeric characters and hyphens.</li>
-                  <li>It cannot begin or end with a hyphen ("-").</li>
-                  <li>The namespace must be a minimum of 3 characters and a maximum of 63 characters.</li>
-                  <li>The namespace cannot be changed after creation.</li>
-                </ul>
-              </v-col>
-            </v-row>
-          </v-card-text>
-          <v-divider />
-          <v-card-actions>
-            <v-spacer />
-            <v-btn data-test="close-btn" @click="close">Close</v-btn>
-            <v-btn color="primary" variant="outlined" data-test="add-btn" @click="addNamespace" :disabled="!fieldMeta.valid">Submit</v-btn>
-          </v-card-actions>
-        </v-container>
+        <v-text-field
+          v-model="namespaceName"
+          label="Namespace"
+          :error-messages="namespaceNameError"
+          hide-details="auto"
+          class="mt-1 mb-4"
+        />
+        <div class="text-body-2 text-justify">
+          <ul class="pl-4">
+            <li>The namespace you choose here will be used for in the SSHID of your devices.</li>
+            <li>The namespace can contain only lowercase alphanumeric characters and hyphens.</li>
+            <li>It cannot begin or end with a hyphen ("-").</li>
+            <li>The namespace must be a minimum of 3 characters and a maximum of 63 characters.</li>
+            <li>The namespace cannot be changed after creation.</li>
+          </ul>
+        </div>
       </template>
       <template v-else>
-        <v-card-title class="bg-primary">Add a namespace using the CLI</v-card-title>
-        <v-card-text class="mt-4 mb-0 pb-1 mb-4">
-          <p class="text-body-2">
-            In the Community Edition of ShellHub, namespaces must be added using the administration CLI.
-            For detailed instructions on how to add namespaces, please refer to the documentation at the ShellHub
-            Administration Guide.
-          </p>
-          <div id="cli-instructions" class="mt-3 text-body-2">
-            <p class="text-caption mb-0 mt-3" data-test="openContentSecond-text">
-              Check the
-              <a
-                :href="'https://docs.shellhub.io/self-hosted/administration'"
-                target="_blank"
-                rel="noopener noreferrer"
-              >ShellHub Administration Guide</a>
-              for more information.
-            </p>
-          </div>
-        </v-card-text>
+        <p class="text-body-2">
+          In the Community Edition of ShellHub, namespaces must be added using the administration CLI.
+          For detailed instructions on how to add namespaces, please refer to the documentation at the ShellHub
+          Administration Guide.
+        </p>
       </template>
-    </v-card>
-  </BaseDialog>
+    </v-card-text>
+  </FormDialog>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useField } from "vee-validate";
 import * as yup from "yup";
 import axios, { AxiosError } from "axios";
 import handleError from "@/utils/handleError";
 import useSnackbar from "@/helpers/snackbar";
 import { envVariables } from "@/envVariables";
-import BaseDialog from "../BaseDialog.vue";
+import FormDialog from "../FormDialog.vue";
 import useNamespacesStore from "@/store/modules/namespaces";
 
 const namespacesStore = useNamespacesStore();
 const snackbar = useSnackbar();
 const showDialog = defineModel({ default: false });
 const isCommunityVersion = computed(() => envVariables.isCommunity);
+const isLoading = ref(false);
 
 // Validation schema for namespace name
 const namespaceSchema = yup
@@ -125,6 +106,7 @@ const handleErrorAndNotify = (error: unknown) => {
 
 // Add a new namespace
 const addNamespace = async () => {
+  isLoading.value = true;
   try {
     const newNamespaceId = await namespacesStore.createNamespace(namespaceName.value);
     await changeNamespace(newNamespaceId);
@@ -145,6 +127,8 @@ const addNamespace = async () => {
     } else {
       handleErrorAndNotify(error);
     }
+  } finally {
+    isLoading.value = false;
   }
 };
 </script>
