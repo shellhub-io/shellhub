0f0eb16a5f9bff93dfbb304c5e64491591b63347
luizhf42 <luizhf42@gmail.com>
2025-10-01 10:34:31 -0300

    refactor(ui): migrate `ApiKeyDelete` to `MessageDialog`

--- ui/tests/components/Team/ApiKeys/ApiKeyDelete.spec.ts
+++ ui/tests/components/Team/ApiKeys/ApiKeyDelete.spec.ts
@@ -39,20 +39,11 @@ describe("Api Key Delete", () => {
     expect(wrapper.vm).toBeTruthy();
   });
 
-  it("Renders the component", () => {
-    expect(wrapper.html()).toMatchSnapshot();
-  });
-
-  it("Renders components", async () => {
-    expect(wrapper.find('[data-test="delete-icon"]').exists()).toBe(true);
-    expect(wrapper.find('[data-test="delete-main-btn-title"]').exists()).toBe(true);
-    await wrapper.findComponent('[data-test="delete-main-btn-title"]').trigger("click");
-    const dialog = new DOMWrapper(document.body);
+  it("Renders the component", async () => {
+    wrapper.vm.showDialog = true;
     await flushPromises();
-    expect(dialog.find('[data-test="title"]').exists()).toBe(true);
-    expect(dialog.find('[data-test="text"]').exists()).toBe(true);
-    expect(dialog.find('[data-test="close-btn"]').exists()).toBe(true);
-    expect(dialog.find('[data-test="delete-btn"]').exists()).toBe(true);
+    const dialog = new DOMWrapper(document.body);
+    expect(dialog.html()).toMatchSnapshot();
   });
 
   it("Successfully Delete Api Key", async () => {
