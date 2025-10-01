aede780babe4c09a910b7a11ca30cf5be083069e
Henry Barreto <henry.barreto@ossystems.com.br>
2025-10-01 13:20:31 -0300

    feat(agent): add env variable to permit logins with empty passwords

--- pkg/agent/pkg/osauth/utils.go
+++ pkg/agent/pkg/osauth/utils.go
@@ -1,5 +1,10 @@
 package osauth
 
+import (
+	"os"
+	"strings"
+)
+
 type User struct {
 	UID      uint32 // The user ID of the account.
 	GID      uint32 // The group ID of the account.
@@ -8,4 +13,14 @@ type User struct {
 	Name     string // The full name of the account owner.
 	HomeDir  string // The home directory path of the account.
 	Shell    string // The default login shell for the account.
+}
+
+// PermitEmptyPasswords checks if the environment variable
+// SHELLHUB_PERMIT_EMPTY_PASSWORDS is set to true. If so, it returns true,
+// allowing empty passwords. Otherwise, it returns false.
+func PermitEmptyPasswords() bool {
+	// TODO: Consider reading this configuration from the main application's function
+	// and passing it down to the osauth package.
+	// TODO: Consider caching the result to avoid repeated environment variable lookups.
+	return strings.EqualFold(os.Getenv("SHELLHUB_PERMIT_EMPTY_PASSWORDS"), "true")
 }
