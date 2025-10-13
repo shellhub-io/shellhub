73b8a2fdeff78ff64d5711145f8fb115db66b0e1
luizhf42 <luizhf42@gmail.com>
2025-10-13 16:01:26 -0300

    feat(ui): rework `WelcomeSecondScreen` UI and update tests

--- ui/tests/components/Welcome/WelcomeSecondScreen.spec.ts
+++ ui/tests/components/Welcome/WelcomeSecondScreen.spec.ts
@@ -1,46 +1,22 @@
 import { setActivePinia, createPinia } from "pinia";
-import { mount, VueWrapper } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import { createVuetify } from "vuetify";
-import { expect, describe, it, beforeEach, vi } from "vitest";
+import { expect, describe, it } from "vitest";
 import WelcomeSecondScreen from "@/components/Welcome/WelcomeSecondScreen.vue";
-import { SnackbarInjectionKey } from "@/plugins/snackbar";
-
-type WelcomeSecondScreenWrapper = VueWrapper<InstanceType<typeof WelcomeSecondScreen>>;
-
-const mockSnackbar = {
-  showInfo: vi.fn(),
-};
+import useAuthStore from "@/store/modules/auth";
+import { SnackbarPlugin } from "@/plugins/snackbar";
 
 describe("Welcome Second Screen", () => {
-  let wrapper: WelcomeSecondScreenWrapper;
   setActivePinia(createPinia());
-  const vuetify = createVuetify();
+  const authStore = useAuthStore();
+  authStore.tenantId = "fake-tenant-data";
 
-  beforeEach(async () => {
-    wrapper = mount(WelcomeSecondScreen, {
-      global: {
-        plugins: [vuetify],
-        provide: { [SnackbarInjectionKey]: mockSnackbar },
-      },
-      props: {
-        command: "",
-      },
-    });
-  });
+  const wrapper = mount(WelcomeSecondScreen, { global: { plugins: [createVuetify(), SnackbarPlugin] } });
 
-  it("Is a Vue instance", () => {
-    expect(wrapper.vm).toBeTruthy();
-  });
+  it("Renders the component", () => { expect(wrapper.html()).toMatchSnapshot(); });
 
-  it("Renders the component", () => {
-    expect(wrapper.html()).toMatchSnapshot();
-  });
-
-  it("Renders the components", async () => {
-    expect(wrapper.find('[data-test="welcome-second-title"]').exists()).toBe(true);
-    expect(wrapper.find('[data-test="welcome-second-text"]').exists()).toBe(true);
-    expect(wrapper.find('[data-test="welcome-second-run-title"]').exists()).toBe(true);
-    expect(wrapper.find('[data-test="command-field"]').exists()).toBe(true);
-    expect(wrapper.find('[data-test="welcome-second-link-docs"]').exists()).toBe(true);
+  it("Renders the correct command", async () => {
+    const expectedCommand = "curl -sSf http://localhost:3000/install.sh | TENANT_ID=fake-tenant-data SERVER_ADDRESS=http://localhost sh";
+    expect(wrapper.find('[data-test="command-field"] input').attributes("value")).toBe(expectedCommand);
   });
 });
