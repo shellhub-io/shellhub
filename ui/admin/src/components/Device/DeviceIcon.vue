8e743daa6cf2528ecc61db8a0146ed5ebf773bea
luizhf42 <luizhf42@gmail.com>
2025-10-17 11:41:11 -0300

    refactor(ui): adapt admin's Device components to main UI patterns

--- ui/admin/src/components/Device/DeviceIcon.vue
+++ ui/admin/src/components/Device/DeviceIcon.vue
@@ -2,50 +2,36 @@
   <i :class="iconName" style="font-size: 20px" />
 </template>
 
-<script lang="ts">
-import { computed, defineComponent } from "vue";
+<script setup lang="ts">
+import { computed } from "vue";
 
-export default defineComponent({
-  props: {
-    icon: {
-      type: String,
-      required: true,
-    },
-  },
-  setup(props) {
-    const deviceIcon = {
-      alpine: "fl-alpine",
-      arch: "fl-archlinux",
-      centos: "fl-centos",
-      coreos: "fl-coreos",
-      debian: "fl-debian",
-      devuan: "fl-devuan",
-      elementary: "fl-elementary",
-      fedora: "fl-fedora",
-      freebsd: "fl-freebsd",
-      gentoo: "fl-gentoo",
-      linuxmint: "fl-linuxmint",
-      mageia: "fl-mageia",
-      manjaro: "fl-manjaro",
-      mandriva: "fl-mandriva",
-      nixos: "fl-nixos",
-      opensuse: "fl-opensuse",
-      rhel: "fl-redhat",
-      sabayon: "fl-sabayon",
-      slackware: "fl-slackware",
-      ubuntu: "fl-ubuntu",
-      raspbian: "fl-raspberry-pi",
-      "ubuntu-core": "fl-ubuntu",
-      void: "fl-void",
-    };
+const { icon } = defineProps<{ icon: string }>();
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const iconName = computed(() => deviceIcon[props.icon]);
+const deviceIconMap: Record<string, string> = {
+  alpine: "fl-alpine",
+  arch: "fl-archlinux",
+  centos: "fl-centos",
+  coreos: "fl-coreos",
+  debian: "fl-debian",
+  devuan: "fl-devuan",
+  elementary: "fl-elementary",
+  fedora: "fl-fedora",
+  freebsd: "fl-freebsd",
+  gentoo: "fl-gentoo",
+  linuxmint: "fl-linuxmint",
+  mageia: "fl-mageia",
+  manjaro: "fl-manjaro",
+  mandriva: "fl-mandriva",
+  nixos: "fl-nixos",
+  opensuse: "fl-opensuse",
+  rhel: "fl-redhat",
+  sabayon: "fl-sabayon",
+  slackware: "fl-slackware",
+  ubuntu: "fl-ubuntu",
+  raspbian: "fl-raspberry-pi",
+  "ubuntu-core": "fl-ubuntu",
+  void: "fl-void",
+};
 
-    return {
-      iconName,
-    };
-  },
-});
+const iconName = computed(() => deviceIconMap[icon] || "");
 </script>
