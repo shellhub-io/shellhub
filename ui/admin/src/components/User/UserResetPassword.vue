c5f0588e9210d7c4d3caa7f12aa402349d239975
luizhf42 <luizhf42@gmail.com>
2025-10-20 17:40:31 -0300

    refactor(ui): migrate `UserResetPassword` to `WindowDialog`

--- ui/admin/src/components/User/UserResetPassword.vue
+++ ui/admin/src/components/User/UserResetPassword.vue
@@ -3,76 +3,72 @@
     <template v-slot:activator="{ props }">
       <v-icon
         tag="button"
-        dark
         v-bind="props"
         @click="showDialog = true"
         tabindex="0"
         data-test="open-dialog-icon"
-      >mdi-account-lock-open
-      </v-icon>
+        icon="mdi-account-lock-open"
+      />
     </template>
     <span>Enable Local Authentication</span>
   </v-tooltip>
 
-  <BaseDialog v-model="showDialog" @close="close">
-    <v-card>
-      <v-card-title class="text-h5 pb-2">Enable Local Authentication</v-card-title>
-      <v-divider />
+  <WindowDialog
+    v-model="showDialog"
+    title="Enable Local Authentication"
+    icon="mdi-account-lock-open"
+    @close="close"
+  >
+    <v-card-text class="pa-6">
       <v-window v-model="step">
-        <v-window-item value="step-1">
-          <v-card-text class="pb-0" data-test="step-1-description">
+        <v-window-item :value="1">
+          <p>
             This action will enable local authentication to this user and generate a new password.
-          </v-card-text>
-
-          <v-card-text data-test="step-1-confirmation">
             Do you want to enable?
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn @click="close" data-test="cancel-btn">Cancel</v-btn>
-            <v-spacer />
-            <v-btn color="primary" @click="proceedToSecondStep" data-test="enable-btn">Enable</v-btn>
-          </v-card-actions>
+          </p>
         </v-window-item>
-
-        <v-window-item value="step-2">
-          <v-card-text>
-            <v-alert
-              class="mb-2"
-              type="warning"
-              variant="tonal"
-              text="Users are strongly encouraged to change this password after their first successful authentication"
-              data-test="password-warning"
-            />
-            A new password has been generated for this user. Please copy it and share it securely:
-            <v-text-field
-              class="mt-4"
-              v-model="generatedPassword"
-              readonly
-              @click="copyText(generatedPassword)"
-              @keypress="copyText(generatedPassword)"
-              variant="outlined"
-              dense
-              prepend-inner-icon="mdi-key"
-              append-inner-icon="mdi-clipboard-text"
-              data-test="generated-password-field"
-            />
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn @click="close" data-test="close-btn">Close</v-btn>
-          </v-card-actions>
+        <v-window-item :value="2">
+          <v-alert
+            class="mb-4"
+            type="warning"
+            variant="tonal"
+            text="Users are strongly encouraged to change this password after their first successful authentication"
+            data-test="password-warning"
+          />
+          <p class="text-justify">A new password has been generated for this user. Please copy and share it securely:</p>
+          <CopyWarning :macro="generatedPassword" copied-item="Password">
+            <template #default="{ copyText }">
+              <v-text-field
+                class="mt-2"
+                v-model="generatedPassword"
+                readonly
+                @click="copyText(generatedPassword)"
+                @keypress.enter="copyText(generatedPassword)"
+                prepend-inner-icon="mdi-key"
+                append-inner-icon="mdi-content-copy"
+                data-test="generated-password-field"
+              />
+            </template>
+          </CopyWarning>
         </v-window-item>
       </v-window>
-    </v-card>
-  </BaseDialog>
+    </v-card-text>
+
+    <template #footer>
+      <v-spacer />
+      <v-btn v-if="step === 1" @click="close" data-test="cancel-btn">Cancel</v-btn>
+      <v-btn v-if="step === 1" color="primary" @click="proceedToSecondStep" data-test="enable-btn">Enable</v-btn>
+      <v-btn v-if="step === 2" @click="close" data-test="close-btn">Close</v-btn>
+    </template>
+  </WindowDialog>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import useUsersStore from "@admin/store/modules/users";
 import useSnackbar from "@/helpers/snackbar";
-import BaseDialog from "@/components/Dialogs/BaseDialog.vue";
+import WindowDialog from "@/components/Dialogs/WindowDialog.vue";
+import CopyWarning from "@/components/User/CopyWarning.vue";
 
 const props = defineProps<{ userId: string }>();
 
@@ -81,29 +77,20 @@ const emit = defineEmits(["update"]);
 const snackbar = useSnackbar();
 const usersStore = useUsersStore();
 const showDialog = ref(false);
-const step = ref("step-1");
+const step = ref(1);
 const generatedPassword = ref("");
 
 const close = () => {
   showDialog.value = false;
-  step.value = "step-1";
+  step.value = 1;
   emit("update");
-};
-
-const copyText = (value: string | undefined) => {
-  if (value) {
-    navigator.clipboard.writeText(value);
-    snackbar.showInfo("Tenant ID copied to clipboard.");
-  }
 };
 
 const proceedToSecondStep = async () => {
   try {
     generatedPassword.value = await usersStore.resetUserPassword(props.userId);
-    step.value = "step-2";
-  } catch (error) {
-    snackbar.showError("Failed to reset user password. Please try again.");
-  }
+    step.value = 2;
+  } catch (error) { snackbar.showError("Failed to reset user password. Please try again."); }
 };
 
 defineExpose({ showDialog, step });
