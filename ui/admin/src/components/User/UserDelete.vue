3410d2c17db728468e0cf773d8332e147fe27e94
luizhf42 <luizhf42@gmail.com>
2025-10-20 16:01:10 -0300

    refactor(ui): migrate `UserDelete` to `MessageDialog`

--- ui/admin/src/components/User/UserDelete.vue
+++ ui/admin/src/components/User/UserDelete.vue
@@ -1,30 +1,24 @@
 <template>
   <v-tooltip bottom anchor="bottom">
     <template v-slot:activator="{ props }">
-      <v-icon tag="a" dark v-bind="props" @click="showDialog = true">mdi-delete </v-icon>
+      <v-icon tag="a" dark v-bind="props" @click="showDialog = true" icon="mdi-delete" />
     </template>
     <span>Remove</span>
   </v-tooltip>
 
-  <BaseDialog v-model="showDialog">
-    <v-card>
-      <v-card-title class="lighten-2 text-center mt-2"> Are you sure? </v-card-title>
-      <v-divider />
-
-      <v-card-text class="mt-4 mb-3 pb-1">
-        You are about to remove this user and all its namespace associated data. This action will
-        also bill usage for namespace subscriptions if any and might take some time to finish.
-      </v-card-text>
-
-      <v-card-actions>
-        <v-spacer />
-
-        <v-btn text @click="showDialog = false"> Close </v-btn>
-
-        <v-btn color="red darken-1" text @click="remove()"> Remove </v-btn>
-      </v-card-actions>
-    </v-card>
-  </BaseDialog>
+  <MessageDialog
+    v-model="showDialog"
+    title="Are you sure?"
+    description="You are about to remove this user and all its namespace associated data.
+    This action will also bill usage for namespace subscriptions if any and might take some time to finish."
+    icon="mdi-alert-circle"
+    icon-color="error"
+    confirm-text="Remove"
+    confirm-color="error"
+    cancel-text="Close"
+    @confirm="remove"
+    @cancel="showDialog = false"
+  />
 </template>
 
 <script setup lang="ts">
@@ -32,7 +26,7 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import useUsersStore from "@admin/store/modules/users";
 import useSnackbar from "@/helpers/snackbar";
-import BaseDialog from "@/components/Dialogs/BaseDialog.vue";
+import MessageDialog from "@/components/Dialogs/MessageDialog.vue";
 import handleError from "@/utils/handleError";
 
 const props = defineProps<{
