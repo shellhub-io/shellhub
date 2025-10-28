5e8ee930db4ec402977af8656c6eb953499393cb
Henry Barreto <henry.barreto@ossystems.com.br>
2025-10-28 11:14:18 -0300

    refactor(openapi): adapt OpenAPI generation for unique spec

--- openapi/main.go
+++ openapi/main.go
@@ -5,38 +5,10 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
-
-	"github.com/shellhub-io/shellhub/pkg/envs"
-)
-
-const (
-	// CommunitySpecPath is the path to the Community edition OpenAPI spec.
-	CommunitySpecPath = "spec/community-openapi.yaml"
-	// EnterpriseSpecPath is the path to the Enterprise edition OpenAPI spec.
-	EnterpriseSpecPath = "spec/enterprise-openapi.yaml"
-	// CloudSpecPath is the path to the Cloud edition OpenAPI spec.
-	CloudSpecPath = "spec/cloud-openapi.yaml"
 )
 
 func main() {
-	var path string
-
-	// NOTE: Determine which OpenAPI spec to use based on the edition.
-	// Currently, the path are hardcoded, but in the future, this could be
-	// made configurable via command-line flags or environment variables.
-	switch {
-	case envs.IsCommunity():
-		path = CommunitySpecPath
-		log.Printf("info: Running in Community edition")
-	// WARN: The order of these checks matters. Cloud should be checked before Enterprise
-	// because Cloud is a superset of Enterprise.
-	case envs.IsCloud():
-		path = CloudSpecPath
-		log.Printf("info: Running in Cloud edition")
-	case envs.IsEnterprise():
-		path = EnterpriseSpecPath
-		log.Printf("info: Running in Enterprise edition")
-	}
+	path := "spec/openapi.yaml"
 
 	fmt.Printf("info: generating OpenAPI server from %s\n", path)
 
