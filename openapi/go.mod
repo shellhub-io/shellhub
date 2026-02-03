24876f63a4a5e543cc811952a373c61fef8f3ca0
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2026-02-03 18:12:53 -0300

    refactor(openapi): add dynamic spec serving by edition
    Dynamically serve OpenAPI specifications based on
    SHELLHUB_CLOUD/SHELLHUB_ENTERPRISE environment variables:
    - Community: spec/community-openapi.yaml
    - Enterprise: spec/enterprise-openapi.yaml
    - Cloud: spec/cloud-openapi.yaml
    
    Add replace directive in go.mod for local development.

--- openapi/go.mod
+++ openapi/go.mod
@@ -1,3 +1,9 @@
 module github.com/shellhub-io/shellhub/openapi
 
 go 1.24.9
+
+require github.com/shellhub-io/shellhub v0.21.5
+
+require github.com/sethvargo/go-envconfig v0.9.0 // indirect
+
+replace github.com/shellhub-io/shellhub => ../
--- openapi/main.go
+++ openapi/main.go
@@ -5,14 +5,33 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+
+	"github.com/shellhub-io/shellhub/pkg/envs"
 )
 
 func main() {
-	path := "spec/openapi.yaml"
+	// Determine which spec to use based on SHELLHUB_CLOUD and SHELLHUB_ENTERPRISE
+	isCloud := envs.IsCloud()
+	isEnterprise := envs.IsEnterprise()
 
-	fmt.Printf("info: generating OpenAPI server from %s\n", path)
+	var specPath string
+	var edition string
 
-	if err := exec.Command("redocly", "bundle", path, "-o", "static/openapi.json").Run(); err != nil { //nolint:gosec
+	switch {
+	case isCloud:
+		specPath = "spec/cloud-openapi.yaml"
+		edition = "cloud"
+	case isEnterprise:
+		specPath = "spec/enterprise-openapi.yaml"
+		edition = "enterprise"
+	default:
+		specPath = "spec/community-openapi.yaml"
+		edition = "community"
+	}
+
+	fmt.Printf("info: generating OpenAPI server from %s (edition: %s)\n", specPath, edition)
+
+	if err := exec.Command("redocly", "bundle", specPath, "-o", "static/openapi.json").Run(); err != nil { //nolint:gosec
 		log.Fatalf("error: failed to bundle the openapi spec: %v", err)
 	}
 
@@ -22,7 +41,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/openapi/", http.StripPrefix("/openapi/", fileServer))
 
-	log.Printf("info: OpenAPI server started\n")
+	log.Printf("info: OpenAPI server started (edition: %s)\n", edition)
 
 	if err := http.ListenAndServe(":8080", mux); err != nil { //nolint:gosec
 		log.Fatal(err)
