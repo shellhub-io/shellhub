8a61c975b0ce313248138e0310b048810f1f460e
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2026-03-12 17:40:58 -0300

    chore(api): squash PG migrations into single SQL file
    Replace 20 Go migration files with a single SQL migration using
    embed.FS + Discover(). This also fixes minor schema issues:
    - Remove redundant indexes (tunnels_address_idx, user_invitations_email_idx)
    - Add missing FKs on membership_invitations (user_id, invited_by)
    - Add missing FK on namespaces.owner_id (DEFERRABLE)
    - Fix active_sessions.session_id type to match sessions.id (varchar(128))
    - Fix membership_invitations.role to use membership_role enum
    - Make devices.status_updated_at nullable instead of Go zero-value default
    - Remove session_events.created_at DEFAULT now() (app sets it)

---
+++ api/store/pg/migrations/001_initial_schema.tx.down.sql
@@ -0,0 +1,93 @@
+DROP TABLE IF EXISTS membership_invitations;
+
+--bun:split
+
+DROP TABLE IF EXISTS user_invitations;
+
+--bun:split
+
+DROP TABLE IF EXISTS session_events;
+
+--bun:split
+
+DROP TABLE IF EXISTS active_sessions;
+
+--bun:split
+
+DROP TABLE IF EXISTS sessions;
+
+--bun:split
+
+DROP TABLE IF EXISTS systems;
+
+--bun:split
+
+DROP TABLE IF EXISTS public_key_tags;
+
+--bun:split
+
+DROP TABLE IF EXISTS device_tags;
+
+--bun:split
+
+DROP TABLE IF EXISTS tags;
+
+--bun:split
+
+DROP TABLE IF EXISTS public_keys;
+
+--bun:split
+
+DROP TABLE IF EXISTS api_keys;
+
+--bun:split
+
+DROP TABLE IF EXISTS private_keys;
+
+--bun:split
+
+DROP TABLE IF EXISTS devices;
+
+--bun:split
+
+DROP TABLE IF EXISTS memberships;
+
+--bun:split
+
+DROP TABLE IF EXISTS users;
+
+--bun:split
+
+DROP TABLE IF EXISTS namespaces;
+
+--bun:split
+
+DROP TYPE IF EXISTS user_status;
+
+--bun:split
+
+DROP TYPE IF EXISTS user_origin;
+
+--bun:split
+
+DROP TYPE IF EXISTS user_auth_method;
+
+--bun:split
+
+DROP TYPE IF EXISTS session_type;
+
+--bun:split
+
+DROP TYPE IF EXISTS namespace_scope;
+
+--bun:split
+
+DROP TYPE IF EXISTS membership_role;
+
+--bun:split
+
+DROP TYPE IF EXISTS membership_invitation_status;
+
+--bun:split
+
+DROP TYPE IF EXISTS device_status;
