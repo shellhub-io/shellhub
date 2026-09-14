88b99f722aa48e4ecbb1b98c1c4c40184766d0e9
Otavio Salvador <otavio@ossystems.com.br>
2026-09-14 14:31:05 -0300

    build(docs): lint MDX structure

---
+++ ui/apps/docs/.remarkrc.mjs
@@ -0,0 +1,21 @@
+import remarkFrontmatter from "remark-frontmatter";
+import remarkLintHeadingIncrement from "remark-lint-heading-increment";
+import remarkLintMdxJsxNoVoidChildren from "remark-lint-mdx-jsx-no-void-children";
+import remarkLintMdxJsxUniqueAttributeName from "remark-lint-mdx-jsx-unique-attribute-name";
+import remarkLintNoDuplicateHeadingsInSection from "remark-lint-no-duplicate-headings-in-section";
+import remarkLintNoEmptyUrl from "remark-lint-no-empty-url";
+import remarkMdx from "remark-mdx";
+import remarkPresetLintRecommended from "remark-preset-lint-recommended";
+
+export default {
+  plugins: [
+    remarkMdx,
+    [remarkFrontmatter, ["yaml"]],
+    remarkPresetLintRecommended,
+    remarkLintHeadingIncrement,
+    remarkLintNoDuplicateHeadingsInSection,
+    remarkLintNoEmptyUrl,
+    remarkLintMdxJsxUniqueAttributeName,
+    remarkLintMdxJsxNoVoidChildren,
+  ],
+};
